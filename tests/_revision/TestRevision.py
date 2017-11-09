78b279c620eecf6682bb96f619a99afbcea7d450
Doyle Rowland <andrew.rowland@reliaqual.com>
2017-11-08 23:22:19 -0500

    Moved TestRevision.py to subdirectory
    The test directory now mirrors the source directory.

--- tests/_revision/TestRevision.py
+++ tests/_revision/TestRevision.py
@@ -13,7 +13,7 @@ Controller algorithms and models.
 import sys
 from os.path import dirname
 
-sys.path.insert(0, dirname(dirname(__file__)) + "/rtk", )
+sys.path.insert(0, dirname(dirname(dirname(__file__))) + "/rtk", )
 
 import unittest
 from nose.plugins.attrib import attr
