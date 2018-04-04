2d70ca421b73879af16f2576a62a8fdb2753289c
Doyle Rowland <andrew.rowland@reliaqual.com>
2018-04-03 21:48:33 -0400

    Update RTKValidation test file.
    - Add validation record to test database.
    - Update RTKValidation test file to use pytest.
    
    Signed-off-by: Doyle Rowland <andrew.rowland@reliaqual.com>

--- tests/_dao/test_rtkvalidation.py
+++ tests/_dao/test_rtkvalidation.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python -O
 # -*- coding: utf-8 -*-
 #
-#       tests.unit._dao.TestRTKValidation.py is part of The RTK Project
+#       tests.dao.programdb.test_rtkvalidation.py is part of The RTK Project
 #
 # All rights reserved.
 """Test class for testing the RTKValidation module algorithms and models."""
 
 from datetime import date, timedelta
 
-import unittest
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from nose.plugins.attrib import attr
+import pytest
 
 from rtk.dao.RTKValidation import RTKValidation
 
@@ -21,141 +17,164 @@ __email__ = 'andrew.rowland@reliaqual.com'
 __organization__ = 'ReliaQual Associates, LLC'
 __copyright__ = 'Copyright 2017 Andrew "weibullguy" Rowland'
 
+ATTRIBUTES = {
+    'cost_minimum': 0.0,
+    'validation_id': 1,
+    'confidence': 95.0,
+    'task_specification': u'',
+    'date_start': date.today(),
+    'acceptable_variance': 0.0,
+    'task_type': u'',
+    'measurement_unit': u'',
+    'cost_average': 0.0,
+    'date_end': date.today() + timedelta(days=30),
+    'time_maximum': 0.0,
+    'description': 'Test Validation',
+    'time_variance': 0.0,
+    'acceptable_minimum': 0.0,
+    'cost_variance': 0.0,
+    'time_minimum': 0.0,
+    'acceptable_mean': 0.0,
+    'time_mean': 0.0,
+    'acceptable_maximum': 0.0,
+    'status': 0.0,
+    'cost_maximum': 0.0,
+    'time_average': 0.0,
+    'cost_mean': 0.0,
+    'revision_id': 1,
+    'cost_ll': 0.0,
+    'cost_ul': 0.0,
+    'time_ll': 0.0,
+    'time_ul': 0.0
+}
 
-class TestRTKValidation(unittest.TestCase):
-    """Class for testing the RTKValidation database table."""
 
-    _attributes = {
-        'cost_minimum': 0.0,
-        'validation_id': 1,
-        'confidence': 95.0,
-        'task_specification': u'',
-        'date_start': date.today(),
-        'acceptable_variance': 0.0,
-        'task_type': u'',
-        'measurement_unit': u'',
-        'cost_average': 0.0,
-        'date_end': date.today() + timedelta(days=30),
-        'time_maximum': 0.0,
-        'description': 'Test Validation',
-        'time_variance': 0.0,
-        'acceptable_minimum': 0.0,
-        'cost_variance': 0.0,
-        'time_minimum': 0.0,
-        'acceptable_mean': 0.0,
-        'time_mean': 0.0,
-        'acceptable_maximum': 0.0,
-        'status': 0.0,
-        'cost_maximum': 0.0,
-        'time_average': 0.0,
-        'cost_mean': 0.0,
-        'revision_id': 1,
-        'cost_ll': 0.0,
-        'cost_ul': 0.0,
-        'time_ll': 0.0,
-        'time_ul': 0.0
-    }
+@pytest.mark.integration
+@pytest.mark.database
+@pytest.mark.validation
+def test_rtkvalidation_create(test_dao):
+    """ __init__() should create an RTKValidation model. """
+    _session = test_dao.RTK_SESSION(
+        bind=test_dao.engine, autoflush=False, expire_on_commit=False)
+    DUT = _session.query(RTKValidation).first()
 
-    def setUp(self):
-        """Set up the test fixture for the RTKValidation class."""
-        engine = create_engine('sqlite:////tmp/TestDB.rtk', echo=False)
-        session = scoped_session(sessionmaker())
+    assert isinstance(DUT, RTKValidation)
 
-        session.remove()
-        session.configure(bind=engine, autoflush=False, expire_on_commit=False)
+    # Verify class attributes are properly initialized.
+    assert DUT.__tablename__ == 'rtk_validation'
+    assert DUT.revision_id == 1
+    assert DUT.validation_id == 1
+    assert DUT.acceptable_maximum == 0.0
+    assert DUT.acceptable_mean == 0.0
+    assert DUT.acceptable_minimum == 0.0
+    assert DUT.acceptable_variance == 0.0
+    assert DUT.confidence == 95.0
+    assert DUT.cost_average == 0.0
+    assert DUT.cost_ll == 0.0
+    assert DUT.cost_maximum == 0.0
+    assert DUT.cost_mean == 0.0
+    assert DUT.cost_minimum == 0.0
+    assert DUT.cost_ul == 0.0
+    assert DUT.cost_variance == 0.0
+    assert DUT.date_end == date.today() + timedelta(days=30)
+    assert DUT.date_start == date.today()
+    assert DUT.description == 'Test Validation'
+    assert DUT.measurement_unit == ''
+    assert DUT.status == 0.0
+    assert DUT.task_type == ''
+    assert DUT.task_specification == ''
+    assert DUT.time_average == 0.0
+    assert DUT.time_ll == 0.0
+    assert DUT.time_maximum == 0.0
+    assert DUT.time_mean == 0.0
+    assert DUT.time_minimum == 0.0
+    assert DUT.time_ul == 0.0
+    assert DUT.time_variance == 0.0
 
-        self.DUT = session.query(RTKValidation).first()
-        self.DUT.description = self._attributes['description']
 
-        session.commit()
+@pytest.mark.integration
+@pytest.mark.database
+@pytest.mark.validation
+def test_get_attributes(test_dao):
+    """ get_attributes() should return a dict of attribute values. """
+    _session = test_dao.RTK_SESSION(
+        bind=test_dao.engine, autoflush=False, expire_on_commit=False)
+    DUT = _session.query(RTKValidation).first()
 
-    @attr(all=True, unit=True)
-    def test00_rtkvalidation_create(self):
-        """(TestRTKValidation) __init__ should create an RTKValidation model."""
-        self.assertTrue(isinstance(self.DUT, RTKValidation))
+    assert DUT.get_attributes() == ATTRIBUTES
 
-        # Verify class attributes are properly initialized.
-        self.assertEqual(self.DUT.__tablename__, 'rtk_validation')
-        self.assertEqual(self.DUT.revision_id, 1)
-        self.assertEqual(self.DUT.validation_id, 1)
-        self.assertEqual(self.DUT.acceptable_maximum, 0.0)
-        self.assertEqual(self.DUT.acceptable_mean, 0.0)
-        self.assertEqual(self.DUT.acceptable_minimum, 0.0)
-        self.assertEqual(self.DUT.acceptable_variance, 0.0)
-        self.assertEqual(self.DUT.confidence, 95.0)
-        self.assertEqual(self.DUT.cost_average, 0.0)
-        self.assertEqual(self.DUT.cost_ll, 0.0)
-        self.assertEqual(self.DUT.cost_maximum, 0.0)
-        self.assertEqual(self.DUT.cost_mean, 0.0)
-        self.assertEqual(self.DUT.cost_minimum, 0.0)
-        self.assertEqual(self.DUT.cost_ul, 0.0)
-        self.assertEqual(self.DUT.cost_variance, 0.0)
-        self.assertEqual(self.DUT.date_end, date.today() + timedelta(days=30))
-        self.assertEqual(self.DUT.date_start, date.today())
-        self.assertEqual(self.DUT.description, 'Test Validation')
-        self.assertEqual(self.DUT.measurement_unit, '')
-        self.assertEqual(self.DUT.status, 0.0)
-        self.assertEqual(self.DUT.task_type, '')
-        self.assertEqual(self.DUT.task_specification, '')
-        self.assertEqual(self.DUT.time_average, 0.0)
-        self.assertEqual(self.DUT.time_ll, 0.0)
-        self.assertEqual(self.DUT.time_maximum, 0.0)
-        self.assertEqual(self.DUT.time_mean, 0.0)
-        self.assertEqual(self.DUT.time_minimum, 0.0)
-        self.assertEqual(self.DUT.time_ul, 0.0)
-        self.assertEqual(self.DUT.time_variance, 0.0)
 
-    @attr(all=True, unit=True)
-    def test01_get_attributes(self):
-        """(TestRTKValidation) get_attributes should return a dict of attribute values."""
-        self.assertEqual(self.DUT.get_attributes(), self._attributes)
+@pytest.mark.integration
+@pytest.mark.database
+@pytest.mark.validation
+def test_set_attributes(test_dao):
+    """ set_attributes() should return a zero error code on success. """
+    _session = test_dao.RTK_SESSION(
+        bind=test_dao.engine, autoflush=False, expire_on_commit=False)
+    DUT = _session.query(RTKValidation).first()
 
-    @attr(all=True, unit=True)
-    def test02a_set_attributes(self):
-        """(TestRTKValidation) set_attributes should return a zero error code on success."""
-        _error_code, _msg = self.DUT.set_attributes(self._attributes)
+    _error_code, _msg = DUT.set_attributes(ATTRIBUTES)
 
-        self.assertEqual(_error_code, 0)
-        self.assertEqual(_msg, "RTK SUCCESS: Updating RTKValidation {0:d} "
-                         "attributes.".format(self.DUT.validation_id))
+    assert _error_code == 0
+    assert _msg == ("RTK SUCCESS: Updating RTKValidation {0:d} "
+                    "attributes.".format(DUT.validation_id))
 
-    @attr(all=True, unit=True)
-    def test02b_set_attributes_missing_key(self):
-        """(TestRTKValidation) set_attributes should return a 40 error code when passed a dict with a missing key."""
-        self._attributes.pop('status')
 
-        _error_code, _msg = self.DUT.set_attributes(self._attributes)
+@pytest.mark.integration
+@pytest.mark.database
+@pytest.mark.validation
+def test_set_attributes_missing_key(test_dao):
+    """ set_attributes() should return a 40 error code when passed a dict with a missing key. """
+    _session = test_dao.RTK_SESSION(
+        bind=test_dao.engine, autoflush=False, expire_on_commit=False)
+    DUT = _session.query(RTKValidation).first()
+    ATTRIBUTES.pop('status')
 
-        self.assertEqual(_error_code, 40)
-        self.assertEqual(_msg, "RTK ERROR: Missing attribute 'status' in "
-                         "attribute dictionary passed to "
-                         "RTKValidation.set_attributes().")
+    _error_code, _msg = DUT.set_attributes(ATTRIBUTES)
 
-        self._attributes['status'] = 0.0
+    assert _error_code == 40
+    assert _msg == ("RTK ERROR: Missing attribute 'status' in attribute "
+                    "dictionary passed to RTKValidation.set_attributes().")
 
-    @attr(all=True, unit=True)
-    def test03a_calculate_task_time(self):
-        """(TestRTKValidation) calculate returns False on successfully calculating tasks times."""
-        self.DUT.time_minimum = 25.2
-        self.DUT.time_average = 36.8
-        self.DUT.time_maximum = 44.1
+    ATTRIBUTES['status'] = 0.0
 
-        self.assertFalse(self.DUT.calculate_task_time())
-        self.assertAlmostEqual(self.DUT.time_ll, 29.90944678)
-        self.assertAlmostEqual(self.DUT.time_mean, 36.08333333)
-        self.assertAlmostEqual(self.DUT.time_ul, 42.2572199)
-        self.assertAlmostEqual(self.DUT.time_variance, 9.9225)
 
-    @attr(all=True, unit=True)
-    def test03b_calculate_task_cost(self):
-        """(TestRTKValidation) calculate returns False on successfully calculating tasks costs."""
-        self.DUT.cost_minimum = 252.00
-        self.DUT.cost_average = 368.00
-        self.DUT.cost_maximum = 441.00
-        self.DUT.confidence = 0.95
+@pytest.mark.integration
+@pytest.mark.database
+@pytest.mark.validation
+def test_calculate_task_time(test_dao):
+    """ calculate() returns False on successfully calculating tasks times. """
+    _session = test_dao.RTK_SESSION(
+        bind=test_dao.engine, autoflush=False, expire_on_commit=False)
+    DUT = _session.query(RTKValidation).first()
 
-        self.assertFalse(self.DUT.calculate_task_cost())
-        self.assertAlmostEqual(self.DUT.cost_ll, 299.09446782)
-        self.assertAlmostEqual(self.DUT.cost_mean, 360.83333333)
-        self.assertAlmostEqual(self.DUT.cost_ul, 422.5721988)
-        self.assertAlmostEqual(self.DUT.cost_variance, 992.25)
+    DUT.time_minimum = 25.2
+    DUT.time_average = 36.8
+    DUT.time_maximum = 44.1
+
+    assert not DUT.calculate_task_time()
+    assert DUT.time_ll == pytest.approx(29.90944678)
+    assert DUT.time_mean == pytest.approx(36.08333333)
+    assert DUT.time_ul == pytest.approx(42.2572199)
+    assert DUT.time_variance == pytest.approx(9.9225)
+
+
+@pytest.mark.integration
+@pytest.mark.database
+@pytest.mark.validation
+def test_calculate_task_cost(test_dao):
+    """ calculate() returns False on successfully calculating tasks costs. """
+    _session = test_dao.RTK_SESSION(
+        bind=test_dao.engine, autoflush=False, expire_on_commit=False)
+    DUT = _session.query(RTKValidation).first()
+
+    DUT.cost_minimum = 252.00
+    DUT.cost_average = 368.00
+    DUT.cost_maximum = 441.00
+    DUT.confidence = 0.95
+
+    assert not DUT.calculate_task_cost()
+    assert DUT.cost_ll == pytest.approx(299.09446782)
+    assert DUT.cost_mean == pytest.approx(360.83333333)
+    assert DUT.cost_ul == pytest.approx(422.5721988)
+    assert DUT.cost_variance == pytest.approx(992.25)
