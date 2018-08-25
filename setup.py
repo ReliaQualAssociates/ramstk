a10400df09db332dd746703ff5f3ec03a66f186f
Doyle Rowland <doyle.rowland@reliaqual.com>
2018-08-24 20:18:44 -0400

    style: (setup.py) fix PEP8 and pylint errors
    Signed-off-by: Doyle Rowland <doyle.rowland@reliaqual.com>

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-#!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
+"""``RAMSTK`` lives on `GitHub <https://github.com/weibullguy/ramstk>`_."""
 import os
 import sys
-import subprocess
 import glob
-
-import distutils.cmd
-import distutils.log
 
 from setuptools import find_packages, setup
 from setuptools.command.install import install as _install
 from setuptools.command.sdist import sdist
 
 # Read the contents of your README file
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
+HERE = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(HERE, 'README.md')) as f:
     __long_description__ = f.read()
 
 __appname__ = 'RAMSTK'
@@ -22,53 +18,54 @@ __version__ = '1.0.0.dev0'
 __author__ = "Doyle 'weibullguy' Rowland"
 __email__ = "andrew.rowland@reliaqual.com"
 __trove__ = [
-             'Development Status :: 4 - Beta',
-             'Environment :: Win32 (MS Windows)',
-             'Environment :: X11 Applications :: GTK',
-             'Intended Audience :: Other Audience',
-             'License :: OSI Approved :: BSD License',
-             'Programming Language :: Python :: 2.7',
-             'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)'
-        ]
+    'Development Status :: 4 - Beta',
+    'Environment :: Win32 (MS Windows)',
+    'Environment :: X11 Applications :: GTK',
+    'Intended Audience :: Other Audience',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)'
+    ]
 
 
 if not sys.version_info[0] == 2:
     sys.exit("Sorry, Python 3 is not supported (yet)")
 
 # Lists of required packages for RTK.
-install_requires = [
+INSTALL_REQUIRES = [
     'defusedxml', 'lifelines', 'lxml', 'matplotlib==1.4.3', 'numpy', 'pandas',
     'PyPubSub==3.3.0', 'scipy', 'sortedcontainers', 'SQLAlchemy',
     'SQLAlchemy-Utils', 'statsmodels', 'treelib', 'xlrd', 'xlwt'
 ]
-tests_require = [
+TEST_REQUIRES = [
     'pytest', 'pytest-cov', 'coverage==4.0.3', 'codacy-coverage',
     'python-coveralls'
 ]
 
 # Build lists of data files to install.
-layout_files = []
-directories = glob.glob('data/layouts/')
-for directory in directories:
+LAYOUT_FILES = []
+for directory in glob.glob('data/layouts/'):
     files = glob.glob(directory+'*')
-    layout_files.append(files)
+    LAYOUT_FILES.append(files)
 
-icon16_files = []
-directories = glob.glob('data/icons/16x16/')
-for directory in directories:
+ICON16_FILES = []
+for directory in glob.glob('data/icons/16x16/'):
     files = glob.glob(directory+'*')
-    icon16_files.append(files)
+    ICON16_FILES.append(files)
 
-icon32_files = []
-directories = glob.glob('data/icons/32x32/')
-for directory in directories:
+ICON32_FILES = []
+for directory in glob.glob('data/icons/32x32/'):
     files = glob.glob(directory+'*')
-    icon32_files.append(files)
+    ICON32_FILES.append(files)
 
 
 class Install(_install):
-    def pre_install_script(self):
-        import os, shutil
+    """Custom install class for RAMSTK."""
+
+    @staticmethod
+    def pre_install_script():
+        """Execute before install."""
+        import shutil
 
         _builddir = os.path.abspath('.') + '/build'
         if os.path.isdir(_builddir):
@@ -80,13 +77,16 @@ class Install(_install):
                         os.unlink(_file_path)
                     elif os.path.isdir(_file_path):
                         shutil.rmtree(_file_path)
-                except Exception as e:
-                    print(e)
+                except Exception as _error:
+                    print(_error)
 
-    def post_install_script(self):
+    @staticmethod
+    def post_install_script():
+        """Execute after install."""
         pass
 
     def run(self):
+        """Run the install."""
         self.pre_install_script()
 
         _install.run(self)
@@ -95,12 +95,14 @@ class Install(_install):
 
 
 class Sdist(sdist):
-    """Custom ``sdist`` command to ensure that mo files are always created. """
+    """Custom ``sdist`` command to ensure that mo files are always created."""
 
     def run(self):
+        """Run custom sdist command."""
         try:
             self.run_command('compile_catalog')
-        except Exception as e:
+        except Exception as _error:
+            print(_error)
             print('No messages catalogs found.')
 
         sdist.run(self)
@@ -120,9 +122,9 @@ if __name__ == '__main__':
         license='BSD-3',
         url='https://github.com/weibullguy/rtk',
         python_requires='>=2.7, <4',
-        install_requires=install_requires,
+        install_requires=INSTALL_REQUIRES,
         setup_requires=['pytest_runner', 'Babel'],
-        tests_require=tests_require,
+        tests_require=TEST_REQUIRES,
         keywords='''reliability availability maintainability safety RAMS
         engineering quality''',
         scripts=[],
@@ -137,10 +139,10 @@ if __name__ == '__main__':
                     ('share/pixmaps', ['data/icons/RTK.png']),
                     ('share/RTK', ['data/RTK.conf']),
                     ('share/RTK', ['data/Site.conf']),
-                    ('share/RTK/layouts', layout_files[0]),
-                    ('share/RTK/icons/16x16', icon16_files[0]),
-                    ('share/RTK/icons/32x32', icon32_files[0])
-                    ],
+                    ('share/RTK/layouts', LAYOUT_FILES[0]),
+                    ('share/RTK/icons/16x16', ICON16_FILES[0]),
+                    ('share/RTK/icons/32x32', ICON32_FILES[0])
+                   ],
         package_data={},
         dependency_links=[],
         zip_safe=True,
