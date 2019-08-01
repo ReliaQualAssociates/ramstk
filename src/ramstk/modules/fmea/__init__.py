6cdd041577a2cc617e73648f8d22f2f95d21b430
Doyle Rowland <doyle.rowland@reliaqual.com>
2019-07-31 21:58:35 -0400

    chore(FMEA): remove old FMEA module package

--- src/ramstk/modules/fmea/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Model import ActionDataModel as dtmAction
-from .Model import CauseDataModel as dtmCause
-from .Model import ControlDataModel as dtmControl
-from .Model import FMEADataModel as dtmFMEA
-from .Model import MechanismDataModel as dtmMechanism
-from .Model import ModeDataModel as dtmMode
-from .Controller import FMEADataController as dtcFMEA
