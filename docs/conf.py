1b0589f2a46e168fdcfc4012d0170b239b0c8327
Doyle Rowland <doyle.rowland@reliaqual.com>
2020-08-31 09:54:07 -0400

    chore(*): add github actions

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 """Configuration for Sphinx."""
 
 # RAMSTK Package Imports
-# import sphinx_py3doc_enhanced_theme
+import sphinx_py3doc_enhanced_theme
 
 # -- Project information -----------------------------------------------------
 project = u'RAMSTK'
@@ -58,8 +58,9 @@ pygments_style = None
 # -- Options for HTML output -------------------------------------------------
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "bizstyle"
-#html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path(), "."]
+# html_theme = "bizstyle"
+html_theme = "sphinx_py3doc_enhanced_theme"
+html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path(), "."]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
