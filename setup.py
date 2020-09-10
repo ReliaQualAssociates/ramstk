f9129627876120ab51403b9a5a8dc76a234ccb4d
Doyle Rowland <doyle.rowland@reliaqual.com>
2020-09-10 17:54:06 -0400

    chore(*): add cinfig files

--- setup.py
+++ setup.py
@@ -1,43 +1,47 @@
 # -*- coding: utf-8 -*-
-"""``RAMSTK`` lives on `GitHub <https://github.com/weibullguy/ramstk>`_."""
-import os
-import sys
-import glob
+"""``RAMSTK`` lives on `GitHub <https://github.com/ReliaQualAssociates/ramstk>`_."""
 
+# Standard Library Imports
+import glob
+import os
+
+# Third Party Imports
 from setuptools import find_packages, setup
 from setuptools.command.install import install as _install
 from setuptools.command.sdist import sdist
+from setuptools.command.test import test
 
-# Read the contents of your README file
 HERE = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(HERE, 'README.md')) as f:
+# Read the contents of the VERSION file.
+with open(os.path.join(HERE, 'VERSION'), encoding='utf-8') as f:
+    RAMSTK_VERSION = f.read()
+
+# Read the contents of the README file.
+with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
     __long_description__ = f.read()
 
 __appname__ = 'RAMSTK'
-__version__ = '1.0.1'
+__version__ = RAMSTK_VERSION
 __author__ = "Doyle 'weibullguy' Rowland"
 __email__ = "doyle.rowland@reliaqual.com"
 __trove__ = [
-    'Development Status :: 4 - Beta', 'Environment :: Win32 (MS Windows)',
+    'Development Status :: 4 - Beta',
+    'Environment :: Win32 (MS Windows)',
     'Environment :: X11 Applications :: GTK',
     'Intended Audience :: Other Audience',
     'License :: OSI Approved :: BSD License',
     'Programming Language :: Python :: 2.7',
-    'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)'
+    'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
 ]
-
-if not sys.version_info[0] == 2:
-    sys.exit("Sorry, Python 3 is not supported yet.")
 
 # Lists of required packages for RAMSTK.
 INSTALL_REQUIRES = [
-    'defusedxml', 'lifelines>=0.13,<0.15', 'matplotlib==1.4.3', 'numpy', 'pandas',
-    'PyPubSub==3.3.0', 'scipy', 'sortedcontainers==1.5.9', 'SQLAlchemy',
-    'SQLAlchemy-Utils', 'statsmodels', 'treelib', 'xlrd', 'xlwt'
+    'defusedxml', 'lifelines<15.0', 'matplotlib', 'numpy', 'openpyxl',
+    'pandas', 'pycairo', 'PyGObject>=3.27', 'PyPubSub', 'scipy',
+    'sortedcontainers', 'SQLAlchemy>=1.3.0', 'SQLAlchemy-Utils', 'statsmodels',
+    'sympy', 'treelib>=1.5.3', 'xlrd', 'xlsxwriter', 'xlwt'
 ]
-TEST_REQUIRES = [
-    'pytest', 'pytest-cov', 'coverage', 'coveralls', 'codacy-coverage'
-]
+TEST_REQUIRES = ['pytest', 'pytest-cov', 'coveralls', 'codacy-coverage']
 
 # Build lists of data files to install.
 LAYOUT_FILES = []
@@ -55,27 +59,38 @@ for directory in glob.glob('data/icons/32x32/'):
     files = glob.glob(directory + '*')
     ICON32_FILES.append(files)
 
+# Codes to allow colored outputs.
+RESET = '\033[0m'
+BOLD = '\033[01m'
+FG_RED = '\033[31m'
+FG_GREEN = '\033[32m'
+FG_YELLOW = '\033[93m'
+BG_BLACK = '\033[40m'
+BG_RED = '\033[41m'
+BG_GREEN = '\033[42m'
+
 
 class Install(_install):
     """Custom install class for RAMSTK."""
-
     @staticmethod
-    def pre_install_script():
+    def pre_install_script() -> None:
         """Execute before install."""
         _eggfile = os.path.abspath('.') + '/src/RAMSTK.egg-info'
-        print("Removing old egg...")
+        print("{0:s}Removing old rotten RAMSTK egg...{1:s}".format(
+            FG_YELLOW, RESET))
         try:
             if os.path.isfile(_eggfile):
                 os.unlink(_eggfile)
-        except Exception as _error:
+        except Exception as _error:  # pylint: disable=broad-except
             print(_error)
 
     @staticmethod
-    def post_install_script():
+    def post_install_script() -> None:
         """Execute after install."""
-        pass
+        print("{0:s}{1:s}Your shiny new RAMSTK-{2:s} is "
+              "installed!!{3:s}".format(FG_GREEN, BOLD, __version__, RESET))
 
-    def run(self):
+    def run(self) -> None:
         """Run the install."""
         self.pre_install_script()
 
@@ -86,63 +101,61 @@ class Install(_install):
 
 class Sdist(sdist):
     """Custom ``sdist`` command to ensure that mo files are always created."""
-
-    def run(self):
+    def run(self) -> None:
         """Run custom sdist command."""
         try:
             self.run_command('compile_catalog')
-        except Exception as _error:
+        except Exception as _error:  # pylint: disable=broad-except
             print(_error)
             print('No messages catalogs found.')
 
         sdist.run(self)
 
 
+class MyTest(test):
+    """Custom ``test`` command to tell the user to use make test."""
+    def run(self) -> None:
+        """Run the custom test command."""
+        print("{0:s}{1:s}Running tests using setup.py is not supported.  "
+              "Please execute tests using the Makefile.  Issue make help for "
+              "options.{2:s}".format(FG_RED, BOLD, RESET))
+
+
 if __name__ == '__main__':
-    setup(
-        name=__appname__,
-        version=__version__,
-        description='''The RAMS ToolKit (RAMSTK) is a suite of tools
+    setup(name=__appname__,
+          version=__version__,
+          description='''The RAMS ToolKit (RAMSTK) is a suite of tools
         for performing and documenting reliability, availability,
         maintainability, and safety (RAMS) analyses''',
-        long_description=__long_description__,
-        long_description_content_type='text/markdown',
-        author=__author__,
-        author_email=__email__,
-        license='BSD-3',
-        url='https://github.com/weibullguy/ramstk',
-        python_requires='>=2.7, <4',
-        install_requires=INSTALL_REQUIRES,
-        setup_requires=['pytest_runner', 'Babel'],
-        tests_require=TEST_REQUIRES,
-        keywords='''reliability availability maintainability safety RAMS
+          long_description=__long_description__,
+          long_description_content_type='text/markdown',
+          author=__author__,
+          author_email=__email__,
+          license='BSD-3',
+          url='https://github.com/ReliaQualAssociates/ramstk',
+          python_requires='>=2.7, <4',
+          install_requires=INSTALL_REQUIRES,
+          setup_requires=['pytest_runner', 'Babel'],
+          tests_require=TEST_REQUIRES,
+          keywords='''reliability availability maintainability safety RAMS
         engineering quality''',
-        scripts=[],
-        packages=find_packages('src', exclude=['tests']),
-        package_dir={'': 'src'},
-        py_modules=[
-            'ramstk.Configuration', 'ramstk.RAMSTK', 'ramstk.Utilities'
-        ],
-        classifiers=__trove__,
-        entry_points={
-            'console_scripts': ['ramstk = ramstk.RAMSTK:main'],
-        },
-        data_files=[('share/applications',
-                     ['data/RAMSTK.desktop']), ('share/pixmaps',
-                                                ['data/icons/RAMSTK.png']),
-                    ('share/RAMSTK', ['data/RAMSTK.conf']), ('share/RAMSTK', [
-                        'data/Site.conf'
-                    ]), ('share/RAMSTK',
-                         ['data/ramstk_common.rtk']), ('share/RAMSTK/layouts',
-                                                       LAYOUT_FILES[0]),
-                    ('share/RAMSTK/icons/16x16',
-                     ICON16_FILES[0]), ('share/RAMSTK/icons/32x32',
-                                        ICON32_FILES[0])],
-        package_data={},
-        dependency_links=[],
-        zip_safe=True,
-        cmdclass={
-            'install': Install,
-            'sdist': Sdist
-        },
-    )
+          scripts=[],
+          packages=find_packages('src', exclude=['tests']),
+          package_dir={'': 'src'},
+          py_modules=[
+              'ramstk.configuration', 'ramstk.exceptions', 'ramstk.logger',
+              'ramstk.ramstk', 'ramstk.utilities'
+          ],
+          classifiers=__trove__,
+          entry_points={
+              'console_scripts': ['ramstk = ramstk.__main__:the_one_ring'],
+              'gui_scripts': ['ramstk = ramstk.__main__:the_one_ring']
+          },
+          package_data={},
+          dependency_links=[],
+          zip_safe=True,
+          cmdclass={
+              'install': Install,
+              'sdist': Sdist,
+              'test': MyTest
+          })
