e8eb648646a2f2ab6b298ec2817457f0ef3fabc3
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-01-14 11:06:22 -0500

    Remove program preferences from Options data manager

--- src/ramstk/controllers/options/datamanager.py
+++ src/ramstk/controllers/options/datamanager.py
@@ -17,14 +17,13 @@ from pubsub import pub
 from ramstk.controllers import RAMSTKDataManager
 from ramstk.exceptions import DataAccessError
 from ramstk.models.commondb import RAMSTKSiteInfo
-from ramstk.models.programdb import RAMSTKProgramInfo
 
 
 class DataManager(RAMSTKDataManager):
     """Contain the attributes and methods of the Options data manager.
 
-    This class manages the user-configurable Preferences and Options
-    data from the Site and Program databases.
+    This class manages the admin-configurable options and data from the
+    Site database.
     """
 
     # Define private dict class attributes.
@@ -48,22 +47,18 @@ class DataManager(RAMSTKDataManager):
         # Initialize private dictionary attributes.
         self._pkey: Dict[str, List[str]] = {
             'siteinfo': ['site_id'],
-            'programinfo': ['revision_id']
         }
 
         # Initialize private list attributes.
 
         # Initialize private scalar attributes.
-        self._parent_id: int = 0
+        self._site_id: int = 0
 
         # Initialize public dictionary attributes.
 
         # Initialize public list attributes.
 
         # Initialize public scalar attributes.
-        self.common_dao = kwargs['common_dao']
-        self.site_configuration = kwargs['site_configuration']
-        self.user_configuration = kwargs['user_configuration']
 
         # Subscribe to PyPubSub messages.
         pub.subscribe(super().do_get_attributes,
@@ -72,7 +67,6 @@ class DataManager(RAMSTKDataManager):
                       'request_set_option_attributes')
 
         pub.subscribe(self.do_get_tree, 'request_get_options_tree')
-        pub.subscribe(self.do_select_all, 'selected_revision')
         pub.subscribe(self.do_update, 'request_update_option')
 
     def do_get_tree(self) -> None:
@@ -91,27 +85,21 @@ class DataManager(RAMSTKDataManager):
         :return: None
         :rtype: None
         """
-        self._revision_id = attributes['revision_id']
+        self._site_id = attributes['site_id']
 
         for _node in self.tree.children(self.tree.root):
             self.tree.remove_node(_node.identifier)
 
         # noinspection PyUnresolvedReferences
-        for _option in self.common_dao.session.query(RAMSTKSiteInfo).all():
+        for _option in self.dao.do_select_all(RAMSTKSiteInfo,
+                                              key=['site_id'],
+                                              value=[self._site_id],
+                                              order=RAMSTKSiteInfo.site_id):
 
             self.tree.create_node(tag='siteinfo',
-                                  identifier='siteinfo',
+                                  identifier=_option.site_id,
                                   parent=self._root,
                                   data={'siteinfo': _option})
-
-        # noinspection PyUnresolvedReferences
-        for _option in self.dao.session.query(RAMSTKProgramInfo).filter(
-                RAMSTKProgramInfo.revision_id == self._revision_id).all():
-
-            self.tree.create_node(tag='programinfo',
-                                  identifier='programinfo',
-                                  parent=self._root,
-                                  data={'programinfo': _option})
 
         pub.sendMessage(
             'succeed_retrieve_options',
@@ -126,42 +114,32 @@ class DataManager(RAMSTKDataManager):
         :rtype: None
         """
         try:
-            if node_id == 'siteinfo':
-                # noinspection PyUnresolvedReferences
-                self.common_dao.session.add(
-                    self.tree.get_node(node_id).data[node_id])
-                self.common_dao.do_update()
-                pub.sendMessage(
-                    'succeed_update_options',
-                    node_id=node_id,
-                )
-            elif node_id == 'programinfo':
-                # noinspection PyUnresolvedReferences
-                self.dao.session.add(self.tree.get_node(node_id).data[node_id])
-                self.dao.do_update()
-                pub.sendMessage(
-                    'succeed_update_options',
-                    node_id=node_id,
-                )
-            else:
-                _method_name: str = inspect.currentframe(  # type: ignore
-                ).f_code.co_name
-                _error_msg = ('{1}: Attempted to save non-existent Option '
-                              'type {0}.').format(str(node_id), _method_name)
-                pub.sendMessage(
-                    'do_log_debug',
-                    logger_name='DEBUG',
-                    message=_error_msg,
-                )
-                pub.sendMessage(
-                    'fail_update_options',
-                    error_message=_error_msg,
-                )
+            self.dao.do_update(self.tree.get_node(node_id).data['siteinfo'])
+
+            pub.sendMessage(
+                'succeed_update_options',
+                node_id=node_id,
+            )
+        except AttributeError:
+            _method_name: str = inspect.currentframe(  # type: ignore
+            ).f_code.co_name
+            _error_msg = (
+                '{1}: Attempted to save non-existent Site ID {0}.').format(
+                    str(node_id), _method_name)
+            pub.sendMessage(
+                'do_log_debug',
+                logger_name='DEBUG',
+                message=_error_msg,
+            )
+            pub.sendMessage(
+                'fail_update_options',
+                error_message=_error_msg,
+            )
         except KeyError:
             _method_name: str = inspect.currentframe(  # type: ignore
             ).f_code.co_name
-            _error_msg = ('{1}: No data package found for Option {0}.').format(
-                str(node_id), _method_name)
+            _error_msg = ('{1}: No data package found for Site {0} '
+                          'options.').format(str(node_id), _method_name)
             pub.sendMessage(
                 'do_log_debug',
                 logger_name='DEBUG',
@@ -175,7 +153,7 @@ class DataManager(RAMSTKDataManager):
             _method_name: str = inspect.currentframe(  # type: ignore
             ).f_code.co_name
             _error_msg = ('{1}: The value for one or more attributes for '
-                          'Options {0} was the wrong type.').format(
+                          'Site {0} options was the wrong type.').format(
                               str(node_id), _method_name)
             pub.sendMessage(
                 'do_log_debug',
