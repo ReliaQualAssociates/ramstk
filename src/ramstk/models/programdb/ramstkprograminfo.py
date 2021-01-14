60f9810696f61718e377bd68a30ea35081886166
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-01-13 19:19:46 -0500

    Add active module fields for all modules

--- src/ramstk/models/programdb/ramstkprograminfo.py
+++ src/ramstk/models/programdb/ramstkprograminfo.py
@@ -4,7 +4,7 @@
 #       Project
 #
 # All rights reserved.
-# Copyright 2007 - 2019 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+# Copyright 2007 - 2020 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
 """RAMSTKProgramInfo Table Module."""
 
 # Standard Library Imports
@@ -28,20 +28,24 @@ class RAMSTKProgramInfo(RAMSTK_BASE, RAMSTKBaseTable):
         'function_active': 1,
         'requirement_active': 1,
         'hardware_active': 1,
-        'software_active': 1,
-        'vandv_active': 1,
-        'fmea_active': 1,
-        'testing_active': 1,
-        'fraca_active': 1,
-        'survival_active': 1,
+        'software_active': 0,
         'rcm_active': 0,
+        'testing_active': 0,
+        'incident_active': 0,
+        'survival_active': 0,
+        'vandv_active': 1,
+        'hazard_active': 1,
+        'stakeholder_active': 1,
+        'allocation_active': 1,
+        'similar_item_active': 1,
+        'fmea_active': 1,
+        'pof_active': 1,
         'rbd_active': 0,
         'fta_active': 0,
         'created_on': date.today(),
         'created_by': '',
         'last_saved': date.today(),
-        'last_saved_by': '',
-        'method': 'STANDARD'
+        'last_saved_by': ''
     }
     __tablename__ = 'ramstk_program_info'
     __table_args__ = {'extend_existing': True}
@@ -63,24 +67,39 @@ class RAMSTKProgramInfo(RAMSTK_BASE, RAMSTKBaseTable):
     software_active = Column('fld_software_active',
                              Integer,
                              default=__defaults__['software_active'])
-    vandv_active = Column('fld_vandv_active',
-                          Integer,
-                          default=__defaults__['vandv_active'])
-    fmea_active = Column('fld_fmea_active',
-                         Integer,
-                         default=__defaults__['fmea_active'])
-    testing_active = Column('fld_testing_active',
-                            Integer,
-                            default=__defaults__['testing_active'])
-    fraca_active = Column('fld_fraca_active',
-                          Integer,
-                          default=__defaults__['fraca_active'])
-    survival_active = Column('fld_survival_active',
-                             Integer,
-                             default=__defaults__['survival_active'])
     rcm_active = Column('fld_rcm_active',
                         Integer,
                         default=__defaults__['rcm_active'])
+    testing_active = Column('fld_testing_active',
+                            Integer,
+                            default=__defaults__['testing_active'])
+    incident_active = Column('fld_incident_active',
+                             Integer,
+                             default=__defaults__['incident_active'])
+    survival_active = Column('fld_survival_active',
+                             Integer,
+                             default=__defaults__['survival_active'])
+    vandv_active = Column('fld_vandv_active',
+                          Integer,
+                          default=__defaults__['vandv_active'])
+    hazard_active = Column('fld_hazard_active',
+                           Integer,
+                           default=__defaults__['hazard_active'])
+    stakeholder_active = Column('fld_stakeholder_active',
+                                Integer,
+                                default=__defaults__['stakeholder_active'])
+    allocation_active = Column('fld_allocation_active',
+                               Integer,
+                               default=__defaults__['allocation_active'])
+    similar_item_active = Column('fld_similar_item_active',
+                                 Integer,
+                                 default=__defaults__['similar_item_active'])
+    fmea_active = Column('fld_fmea_active',
+                         Integer,
+                         default=__defaults__['fmea_active'])
+    pof_active = Column('fld_pof_active',
+                        Integer,
+                        default=__defaults__['pof_active'])
     rbd_active = Column('fld_rbd_active',
                         Integer,
                         default=__defaults__['rbd_active'])
@@ -99,7 +118,6 @@ class RAMSTKProgramInfo(RAMSTK_BASE, RAMSTKBaseTable):
     last_saved_by = Column('fld_last_saved_by',
                            String(512),
                            default=__defaults__['last_saved_by'])
-    method = Column('fld_method', String(512), default=__defaults__['method'])
 
     def get_attributes(self):
         """Retrieve current values of RAMSTKProgramInfo data model attributes.
@@ -116,20 +134,24 @@ class RAMSTKProgramInfo(RAMSTK_BASE, RAMSTKBaseTable):
             'function_active': self.function_active,
             'requirement_active': self.requirement_active,
             'hardware_active': self.hardware_active,
+            'software_active': self.software_active,
+            'rcm_active': self.rcm_active,
+            'testing_active': self.testing_active,
+            'incident_active': self.incident_active,
+            'survival_active': self.survival_active,
             'vandv_active': self.vandv_active,
             'fmea_active': self.fmea_active,
-            'software_active': self.software_active,
-            'testing_active': self.testing_active,
-            'fraca_active': self.fraca_active,
-            'survival_active': self.survival_active,
-            'rcm_active': self.rcm_active,
+            'pof_active': self.pof_active,
+            'hazard_active': self.hazard_active,
+            'stakeholder_active': self.stakeholder_active,
+            'allocation_active': self.allocation_active,
+            'similar_item_active': self.similar_item_active,
             'rbd_active': self.rbd_active,
             'fta_active': self.fta_active,
             'created_on': self.created_on,
             'created_by': self.created_by,
             'last_saved': self.last_saved,
             'last_saved_by': self.last_saved_by,
-            'method': self.method,
         }
 
         return _attributes
--- tests/models/programdb/test_ramstkprograminfo.py
+++ tests/models/programdb/test_ramstkprograminfo.py
@@ -7,7 +7,8 @@
 #
 # All rights reserved.
 # Copyright 2007 - 2019 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""Test class for testing the RAMSTKProgramInfo module algorithms and models."""
+"""Test class for testing the RAMSTKProgramInfo module algorithms and
+models."""
 
 # Standard Library Imports
 from datetime import date
@@ -21,21 +22,25 @@ from ramstk.models.programdb import RAMSTKProgramInfo
 ATTRIBUTES = {
     'created_by': '',
     'created_on': date.today(),
-    'fmea_active': 1,
-    'fraca_active': 1,
-    'fta_active': 0,
-    'function_active': 1,
-    'hardware_active': 1,
     'last_saved': date.today(),
     'last_saved_by': '',
-    'method': 'STANDARD',
-    'rbd_active': 0,
-    'rcm_active': 0,
+    'function_active': 1,
     'requirement_active': 1,
-    'software_active': 1,
-    'survival_active': 1,
-    'testing_active': 1,
-    'vandv_active': 1
+    'hardware_active': 1,
+    'software_active': 0,
+    'rcm_active': 0,
+    'testing_active': 0,
+    'incident_active': 0,
+    'survival_active': 0,
+    'vandv_active': 1,
+    'hazard_active': 1,
+    'stakeholder_active': 1,
+    'allocation_active': 1,
+    'similar_item_active': 1,
+    'fmea_active': 1,
+    'pof_active': 1,
+    'rbd_active': 0,
+    'fta_active': 0,
 }
 
 
@@ -44,7 +49,7 @@ class TestRAMSTKProgramInfo():
     """Class for testing the RAMSTKProgramInfo model."""
     @pytest.mark.integration
     def test_ramstkprograminfo_create(self, test_program_dao):
-        """ __init__() should create an RAMSTKProgramInfo model. """
+        """__init__() should create an RAMSTKProgramInfo model."""
         DUT = test_program_dao.session.query(RAMSTKProgramInfo).first()
 
         assert isinstance(DUT, RAMSTKProgramInfo)
@@ -52,69 +57,79 @@ class TestRAMSTKProgramInfo():
         # Verify class attributes are properly initialized.
         assert DUT.__tablename__ == 'ramstk_program_info'
         assert DUT.revision_id == 1
-        assert DUT.function_active == 0
+        assert DUT.function_active == 1
         assert DUT.requirement_active == 1
-        assert DUT.hardware_active == 0
-        assert DUT.vandv_active == 0
+        assert DUT.hardware_active == 1
+        assert DUT.software_active == 0
+        assert DUT.rcm_active == 0
+        assert DUT.testing_active == 0
+        assert DUT.incident_active == 0
+        assert DUT.survival_active == 0
+        assert DUT.vandv_active == 1
+        assert DUT.hazard_active == 1
+        assert DUT.stakeholder_active == 1
+        assert DUT.allocation_active == 1
+        assert DUT.similar_item_active == 1
         assert DUT.fmea_active == 1
-        assert DUT.software_active == 1
-        assert DUT.testing_active == 1
-        assert DUT.fraca_active == 1
-        assert DUT.survival_active == 1
-        assert DUT.rcm_active == 1
+        assert DUT.pof_active == 1
         assert DUT.rbd_active == 0
         assert DUT.fta_active == 0
-        assert DUT.created_on == date(2019, 7, 21)
+        assert DUT.created_on == date.today()
         assert DUT.created_by == ''
-        assert DUT.last_saved == date(2019, 7, 21)
+        assert DUT.last_saved == date.today()
         assert DUT.last_saved_by == ''
-        assert DUT.method == 'STANDARD'
 
     @pytest.mark.integration
     def test_get_attributes(self, test_program_dao):
-        """ get_attributes() should return a dict of attribute values. """
+        """get_attributes() should return a dict of attribute values."""
         DUT = test_program_dao.session.query(RAMSTKProgramInfo).first()
 
         _attributes = DUT.get_attributes()
         assert _attributes['revision_id'] == 1
-        assert _attributes['function_active'] == 0
+        assert _attributes['function_active'] == 1
         assert _attributes['requirement_active'] == 1
-        assert _attributes['hardware_active'] == 0
-        assert _attributes['vandv_active'] == 0
+        assert _attributes['hardware_active'] == 1
+        assert _attributes['software_active'] == 0
+        assert _attributes['rcm_active'] == 0
+        assert _attributes['testing_active'] == 0
+        assert _attributes['incident_active'] == 0
+        assert _attributes['survival_active'] == 0
+        assert _attributes['vandv_active'] == 1
+        assert _attributes['hazard_active'] == 1
+        assert _attributes['stakeholder_active'] == 1
+        assert _attributes['allocation_active'] == 1
+        assert _attributes['similar_item_active'] == 1
         assert _attributes['fmea_active'] == 1
-        assert _attributes['software_active'] == 1
-        assert _attributes['testing_active'] == 1
-        assert _attributes['fraca_active'] == 1
-        assert _attributes['survival_active'] == 1
-        assert _attributes['rcm_active'] == 1
+        assert _attributes['pof_active'] == 1
         assert _attributes['rbd_active'] == 0
         assert _attributes['fta_active'] == 0
-        assert _attributes['created_on'] == date(2019, 7, 21)
+        assert _attributes['created_on'] == date.today()
         assert _attributes['created_by'] == ''
-        assert _attributes['last_saved'] == date(2019, 7, 21)
+        assert _attributes['last_saved'] == date.today()
         assert _attributes['last_saved_by'] == ''
-        assert _attributes['method'] == 'STANDARD'
 
     @pytest.mark.integration
     def test_set_attributes(self, test_program_dao):
-        """ set_attributes() should return a zero error code on success. """
+        """set_attributes() should return a zero error code on success."""
         DUT = test_program_dao.session.query(RAMSTKProgramInfo).first()
 
         assert DUT.set_attributes(ATTRIBUTES) is None
 
     @pytest.mark.integration
     def test_set_attributes_none_value(self, test_program_dao):
-        """set_attributes() should set an attribute to it's default value when the attribute is passed with a None value."""
+        """set_attributes() should set an attribute to it's default value when
+        the attribute is passed with a None value."""
         DUT = test_program_dao.session.query(RAMSTKProgramInfo).first()
 
-        ATTRIBUTES['method'] = None
+        ATTRIBUTES['pof_active'] = None
 
         assert DUT.set_attributes(ATTRIBUTES) is None
-        assert DUT.get_attributes()['method'] == 'STANDARD'
+        assert DUT.get_attributes()['pof_active'] == 1
 
     @pytest.mark.integration
     def test_set_attributes_unknown_attributes(self, test_program_dao):
-        """set_attributes() should raise an AttributeError when passed an unknown attribute."""
+        """set_attributes() should raise an AttributeError when passed an
+        unknown attribute."""
         DUT = test_program_dao.session.query(RAMSTKProgramInfo).first()
 
         with pytest.raises(AttributeError):
