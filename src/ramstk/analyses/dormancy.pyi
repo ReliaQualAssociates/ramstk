dd7bb02f8034de2dd435c7e9a4d809f9dc06cc39
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-01-27 08:38:02 -0500

    Fix typing for hw_info argument

---
+++ src/ramstk/analyses/dormancy.pyi
@@ -0,0 +1,9 @@
+# Standard Library Imports
+from typing import Any, List, Union
+
+DORMANT_HR_MULT: Any
+
+
+def do_calculate_dormant_hazard_rate(hw_info: List[Union[int, float]],
+                                     env_info: List[int]) -> float:
+    ...
