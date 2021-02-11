98fe999f378d8d86ed1f3eaac38835611f65813f
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-02-11 11:40:19 -0500

    Convert ramstkhazardanalysis tests to unit tests

--- tests/models/programdb/ramstkhazardanalysis_unit_test.py
+++ tests/models/programdb/ramstkhazardanalysis_unit_test.py
@@ -2,17 +2,75 @@
 # type: ignore
 # -*- coding: utf-8 -*-
 #
-#       tests.dao.programdb.test_ramstkhazardanalysis.py is part of The RAMSTK
-#       Project
+#       tests.dao.programdb.ramstkhazardanalysis_unit_test.py is part of The
+#       RAMSTK Project
 #
 # All rights reserved.
-"""Test class for testing RAMSTKHazardAnalysis module algorithms and models."""
+# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+"""Class for testing RAMSTKHazardAnalysis module algorithms and models."""
 
 # Third Party Imports
+# noinspection PyPackageRequirements
 import pytest
+# noinspection PyUnresolvedReferences
+from mocks import MockDAO
 
 # RAMSTK Package Imports
 from ramstk.models.programdb import RAMSTKHazardAnalysis
+
+
+@pytest.fixture
+def mock_program_dao(monkeypatch):
+    _hazard_1 = RAMSTKHazardAnalysis()
+    _hazard_1.revision_id = 1
+    _hazard_1.function_id = 1
+    _hazard_1.hazard_id = 1
+    _hazard_1.user_blob_3 = ''
+    _hazard_1.user_blob_2 = ''
+    _hazard_1.user_blob_1 = ''
+    _hazard_1.system_severity = 'Medium'
+    _hazard_1.result_2 = 0.0
+    _hazard_1.result_3 = 0.0
+    _hazard_1.assembly_probability = 'Level A - Frequent'
+    _hazard_1.system_probability = 'Level A - Frequent'
+    _hazard_1.system_probability_f = 'Level A - Frequent'
+    _hazard_1.assembly_hri = 20
+    _hazard_1.system_hri = 20
+    _hazard_1.system_effect = ''
+    _hazard_1.user_int_1 = 0
+    _hazard_1.user_float_3 = 0.0
+    _hazard_1.result_4 = 0.0
+    _hazard_1.user_float_1 = 0.0
+    _hazard_1.potential_hazard = ''
+    _hazard_1.remarks = ''
+    _hazard_1.system_hri_f = 20
+    _hazard_1.result_5 = 0.0
+    _hazard_1.assembly_severity = 'Medium'
+    _hazard_1.assembly_probability_f = 'Level A - Frequent'
+    _hazard_1.assembly_hri_f = 4
+    _hazard_1.assembly_effect = ''
+    _hazard_1.function_4 = ''
+    _hazard_1.potential_cause = ''
+    _hazard_1.system_mitigation = ''
+    _hazard_1.function_3 = ''
+    _hazard_1.function_2 = ''
+    _hazard_1.function_1 = ''
+    _hazard_1.user_int_3 = 0
+    _hazard_1.user_int_2 = 0
+    _hazard_1.assembly_severity_f = 'Medium'
+    _hazard_1.system_severity_f = 'Medium'
+    _hazard_1.assembly_mitigation = ''
+    _hazard_1.function_5 = ''
+    _hazard_1.result_1 = 0.0
+    _hazard_1.user_float_2 = 0.0
+
+    DAO = MockDAO()
+    DAO.table = [
+        _hazard_1,
+    ]
+
+    yield DAO
+
 
 ATTRIBUTES = {
     'user_blob_3': '',
@@ -56,17 +114,15 @@ ATTRIBUTES = {
 }
 
 
-@pytest.mark.usefixtures('test_program_dao')
-class TestRAMSTKHazardAnalysis():
+@pytest.mark.usefixtures('mock_program_dao')
+class TestRAMSTKHazardAnalysis:
     """Class for testing the RAMSTKHazardAnalysis model."""
-    @pytest.mark.integration
-    def test_ramstkallocation_create(self, test_program_dao):
+    @pytest.mark.unit
+    def test_ramstkallocation_create(self, mock_program_dao):
         """__init__() should create an RAMSTKHazardAnalysis model."""
-        DUT = test_program_dao.session.query(RAMSTKHazardAnalysis).first()
+        DUT = mock_program_dao.do_select_all(RAMSTKHazardAnalysis)[0]
 
         assert isinstance(DUT, RAMSTKHazardAnalysis)
-
-        # Verify class attributes are properly initialized.
         assert DUT.__tablename__ == 'ramstk_hazard_analysis'
         assert DUT.revision_id == 1
         assert DUT.function_id == 1
@@ -110,10 +166,10 @@ class TestRAMSTKHazardAnalysis():
         assert DUT.user_int_2 == 0
         assert DUT.user_int_3 == 0
 
-    @pytest.mark.integration
-    def test_get_attributes(self, test_program_dao):
+    @pytest.mark.unit
+    def test_get_attributes(self, mock_program_dao):
         """get_attributes() should return a dict of attribute values."""
-        DUT = test_program_dao.session.query(RAMSTKHazardAnalysis).first()
+        DUT = mock_program_dao.do_select_all(RAMSTKHazardAnalysis)[0]
 
         _attributes = DUT.get_attributes()
 
@@ -161,27 +217,29 @@ class TestRAMSTKHazardAnalysis():
         assert _attributes['user_int_2'] == 0
         assert _attributes['user_int_3'] == 0
 
-    @pytest.mark.integration
-    def test_set_attributes(self, test_program_dao):
+    @pytest.mark.unit
+    def test_set_attributes(self, mock_program_dao):
         """set_attributes() should return a zero error code on success."""
-        DUT = test_program_dao.session.query(RAMSTKHazardAnalysis).first()
+        DUT = mock_program_dao.do_select_all(RAMSTKHazardAnalysis)[0]
 
         assert DUT.set_attributes(ATTRIBUTES) is None
 
-    @pytest.mark.integration
-    def test_set_attributes_none_value(self, test_program_dao):
-        """set_attributes() should set an attribute to it's default value when the attribute is passed with a None value."""
-        DUT = test_program_dao.session.query(RAMSTKHazardAnalysis).first()
+    @pytest.mark.unit
+    def test_set_attributes_none_value(self, mock_program_dao):
+        """set_attributes() should set an attribute to it's default value when
+        the attribute is passed with a None value."""
+        DUT = mock_program_dao.do_select_all(RAMSTKHazardAnalysis)[0]
 
         ATTRIBUTES['remarks'] = None
 
         assert DUT.set_attributes(ATTRIBUTES) is None
         assert DUT.get_attributes()['remarks'] == ''
 
-    @pytest.mark.integration
-    def test_set_attributes_unknown_attributes(self, test_program_dao):
-        """set_attributes() should raise an AttributeError when passed an unknown attribute."""
-        DUT = test_program_dao.session.query(RAMSTKHazardAnalysis).first()
+    @pytest.mark.unit
+    def test_set_attributes_unknown_attributes(self, mock_program_dao):
+        """set_attributes() should raise an AttributeError when passed an
+        unknown attribute."""
+        DUT = mock_program_dao.do_select_all(RAMSTKHazardAnalysis)[0]
 
         with pytest.raises(AttributeError):
             DUT.set_attributes({'shibboly-bibbly-boo': 0.9998})
