01bbd0964de449984d67cf068a7f83281312f726
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-06-18 09:48:11 -0400

    test: add mock RAMSTKSiteInfo table for unit tests

--- tests/controllers/options/options_unit_test.py
+++ tests/controllers/options/options_unit_test.py
@@ -14,7 +14,7 @@ from datetime import date, timedelta
 
 # Third Party Imports
 import pytest
-from mocks import MockDAO
+from mocks import MockDAO, MockRAMSTKSiteInfo
 from pubsub import pub
 from treelib import Tree
 
@@ -26,10 +26,10 @@ from ramstk.models.commondb import RAMSTKSiteInfo
 
 @pytest.fixture
 def mock_common_dao(monkeypatch):
-    _site_1 = RAMSTKSiteInfo()
+    _site_1 = MockRAMSTKSiteInfo()
     _site_1.site_id = 1
-    _site_1.site_name = 'DEMO SITE'
-    _site_1.product_key = 'DEMO'
+    _site_1.site_name = "DEMO SITE"
+    _site_1.product_key = "DEMO"
     _site_1.expire_on = date.today() + timedelta(30)
     _site_1.function_enabled = 1
     _site_1.requirement_enabled = 1
@@ -59,6 +59,7 @@ def mock_common_dao(monkeypatch):
 
 class TestCreateControllers:
     """Class for controller initialization test suite."""
+
     @pytest.mark.unit
     def test_data_manager_create(self, mock_common_dao):
         """__init__() should return a Options data manager."""
@@ -68,25 +69,24 @@ class TestCreateControllers:
         assert isinstance(DUT.tree, Tree)
         assert isinstance(DUT.dao, BaseDatabase)
         assert DUT._pkey == {
-            'siteinfo': ['site_id'],
+            "siteinfo": ["site_id"],
         }
-        assert DUT._tag == 'options'
+        assert DUT._tag == "options"
         assert DUT._root == 0
         assert DUT._site_id == 0
 
-        assert pub.isSubscribed(DUT.do_update, 'request_update_option')
-        assert pub.isSubscribed(DUT.do_get_attributes,
-                                'request_get_option_attributes')
-        assert pub.isSubscribed(DUT.do_get_tree, 'request_get_options_tree')
-        assert pub.isSubscribed(DUT.do_set_attributes,
-                                'request_set_option_attributes')
+        assert pub.isSubscribed(DUT.do_update, "request_update_option")
+        assert pub.isSubscribed(DUT.do_get_attributes, "request_get_option_attributes")
+        assert pub.isSubscribed(DUT.do_get_tree, "request_get_options_tree")
+        assert pub.isSubscribed(DUT.do_set_attributes, "request_set_option_attributes")
 
 
 class TestSelectMethods:
     """Class for testing data manager select_all() and select() methods."""
+
     def on_succeed_select_all(self, tree):
         assert isinstance(tree, Tree)
-        assert isinstance(tree.get_node(1).data['siteinfo'], RAMSTKSiteInfo)
+        assert isinstance(tree.get_node(1).data["siteinfo"], MockRAMSTKSiteInfo)
         # There should be a root node with no data package and a node with
         # the one RAMSTKSiteInfo record.
         assert len(tree.all_nodes()) == 2
@@ -96,26 +96,26 @@ class TestSelectMethods:
     def test_do_select_all(self, mock_common_dao):
         """do_select_all() should return a Tree() object populated with
         RAMSTKProgramInfo and RAMSTKSiteInfo instances on success."""
-        pub.subscribe(self.on_succeed_select_all, 'succeed_retrieve_options')
+        pub.subscribe(self.on_succeed_select_all, "succeed_retrieve_options")
 
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
+        DUT.do_select_all({"site_id": 1})
 
-        pub.unsubscribe(self.on_succeed_select_all, 'succeed_retrieve_options')
+        pub.unsubscribe(self.on_succeed_select_all, "succeed_retrieve_options")
 
     @pytest.mark.unit
     def test_do_select_all_populated_tree(self, mock_common_dao):
         """do_select_all() should clear nodes from an existing Options tree."""
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
+        DUT.do_select_all({"site_id": 1})
 
-        pub.subscribe(self.on_succeed_select_all, 'succeed_retrieve_options')
+        pub.subscribe(self.on_succeed_select_all, "succeed_retrieve_options")
 
-        DUT.do_select_all({'site_id': 1})
+        DUT.do_select_all({"site_id": 1})
 
-        pub.unsubscribe(self.on_succeed_select_all, 'succeed_retrieve_options')
+        pub.unsubscribe(self.on_succeed_select_all, "succeed_retrieve_options")
 
     @pytest.mark.unit
     def test_do_select(self, mock_common_dao):
@@ -123,14 +123,14 @@ class TestSelectMethods:
         success."""
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
+        DUT.do_select_all({"site_id": 1})
 
-        _options = DUT.do_select(1, table='siteinfo')
+        _options = DUT.do_select(1, table="siteinfo")
 
-        assert isinstance(_options, RAMSTKSiteInfo)
+        assert isinstance(_options, MockRAMSTKSiteInfo)
         assert _options.site_id == 1
-        assert _options.site_name == 'DEMO SITE'
-        assert _options.product_key == 'DEMO'
+        assert _options.site_name == "DEMO SITE"
+        assert _options.product_key == "DEMO"
         assert _options.expire_on == date.today() + timedelta(30)
         assert _options.function_enabled == 1
         assert _options.requirement_enabled == 1
@@ -156,10 +156,10 @@ class TestSelectMethods:
         requested."""
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
+        DUT.do_select_all({"site_id": 1})
 
         with pytest.raises(KeyError):
-            DUT.do_select(1, table='scibbidy-bibbidy-doo')
+            DUT.do_select(1, table="scibbidy-bibbidy-doo")
 
     @pytest.mark.unit
     def test_do_select_non_existent_id(self, mock_common_dao):
@@ -167,52 +167,53 @@ class TestSelectMethods:
         requested."""
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
+        DUT.do_select_all({"site_id": 1})
 
-        assert DUT.do_select(100, table='siteinfo') is None
+        assert DUT.do_select(100, table="siteinfo") is None
 
 
 class TestGetterSetter:
     """Class for testing methods that get or set."""
+
     def on_succeed_get_attributes(self, attributes):
         assert isinstance(attributes, dict)
-        assert attributes['function_enabled'] == 1
-        assert attributes['requirement_enabled'] == 1
-        assert attributes['hardware_enabled'] == 1
-        assert attributes['software_enabled'] == 0
-        assert attributes['rcm_enabled'] == 0
-        assert attributes['testing_enabled'] == 0
-        assert attributes['incident_enabled'] == 0
-        assert attributes['survival_enabled'] == 0
-        assert attributes['vandv_enabled'] == 1
-        assert attributes['hazard_enabled'] == 1
-        assert attributes['stakeholder_enabled'] == 1
-        assert attributes['allocation_enabled'] == 1
-        assert attributes['similar_item_enabled'] == 1
-        assert attributes['fmea_enabled'] == 1
-        assert attributes['pof_enabled'] == 1
-        assert attributes['rbd_enabled'] == 0
+        assert attributes["function_enabled"] == 1
+        assert attributes["requirement_enabled"] == 1
+        assert attributes["hardware_enabled"] == 1
+        assert attributes["software_enabled"] == 0
+        assert attributes["rcm_enabled"] == 0
+        assert attributes["testing_enabled"] == 0
+        assert attributes["incident_enabled"] == 0
+        assert attributes["survival_enabled"] == 0
+        assert attributes["vandv_enabled"] == 1
+        assert attributes["hazard_enabled"] == 1
+        assert attributes["stakeholder_enabled"] == 1
+        assert attributes["allocation_enabled"] == 1
+        assert attributes["similar_item_enabled"] == 1
+        assert attributes["fmea_enabled"] == 1
+        assert attributes["pof_enabled"] == 1
+        assert attributes["rbd_enabled"] == 0
         print("\033[36m\nsucceed_get_siteinfo_attributes topic was broadcast.")
 
     def on_succeed_get_data_manager_tree(self, tree):
         assert isinstance(tree, Tree)
-        assert isinstance(tree.get_node(1).data['siteinfo'], RAMSTKSiteInfo)
+        assert isinstance(tree.get_node(1).data["siteinfo"], MockRAMSTKSiteInfo)
         print("\033[36m\nsucceed_get_options_tree topic was broadcast")
 
     @pytest.mark.unit
     def test_do_get_attributes(self, mock_common_dao):
         """do_get_attributes() should return a dict of site information
         attributes on success."""
-        pub.subscribe(self.on_succeed_get_attributes,
-                      'succeed_get_siteinfo_attributes')
+        pub.subscribe(self.on_succeed_get_attributes, "succeed_get_siteinfo_attributes")
 
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
-        DUT.do_get_attributes(1, 'siteinfo')
+        DUT.do_select_all({"site_id": 1})
+        DUT.do_get_attributes(1, "siteinfo")
 
-        pub.unsubscribe(self.on_succeed_get_attributes,
-                        'succeed_get_siteinfo_attributes')
+        pub.unsubscribe(
+            self.on_succeed_get_attributes, "succeed_get_siteinfo_attributes"
+        )
 
     @pytest.mark.unit
     def test_do_set_attributes(self, mock_common_dao):
@@ -220,74 +221,77 @@ class TestGetterSetter:
         site information attributes."""
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
+        DUT.do_select_all({"site_id": 1})
 
-        pub.sendMessage('request_set_option_attributes',
-                        node_id=[1, ''],
-                        package={'function_enabled': 1})
-        pub.sendMessage('request_set_option_attributes',
-                        node_id=[1, ''],
-                        package={'requirement_enabled': 1})
-        assert DUT.do_select(1, table='siteinfo').function_enabled == 1
-        assert DUT.do_select(1, table='siteinfo').requirement_enabled == 1
+        pub.sendMessage(
+            "request_set_option_attributes",
+            node_id=[1, ""],
+            package={"function_enabled": 1},
+        )
+        pub.sendMessage(
+            "request_set_option_attributes",
+            node_id=[1, ""],
+            package={"requirement_enabled": 1},
+        )
+        assert DUT.do_select(1, table="siteinfo").function_enabled == 1
+        assert DUT.do_select(1, table="siteinfo").requirement_enabled == 1
 
-        pub.unsubscribe(DUT.do_set_attributes, 'request_set_option_attributes')
+        pub.unsubscribe(DUT.do_set_attributes, "request_set_option_attributes")
 
     @pytest.mark.unit
     def test_on_get_data_manager_tree(self, mock_common_dao):
         """on_get_tree() should return the Options treelib Tree."""
-        pub.subscribe(self.on_succeed_get_data_manager_tree,
-                      'succeed_get_options_tree')
+        pub.subscribe(self.on_succeed_get_data_manager_tree, "succeed_get_options_tree")
 
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
+        DUT.do_select_all({"site_id": 1})
         DUT.do_get_tree()
 
-        pub.unsubscribe(self.on_succeed_get_data_manager_tree,
-                        'succeed_get_options_tree')
+        pub.unsubscribe(
+            self.on_succeed_get_data_manager_tree, "succeed_get_options_tree"
+        )
 
 
 class TestUpdateMethods:
     """Class for testing update() and update_all() methods."""
+
     def on_fail_update_non_existent_id(self, error_message):
-        assert error_message == ('do_update: Attempted to save non-existent '
-                                 'Site ID skullduggery.')
+        assert error_message == (
+            "do_update: Attempted to save non-existent " "Site ID skullduggery."
+        )
         print("\033[35m\nfail_update_options topic was broadcast")
 
     def on_fail_update_no_data_package(self, error_message):
-        assert error_message == ('do_update: No data package found for Site 1 '
-                                 'options.')
+        assert error_message == (
+            "do_update: No data package found for Site 1 " "options."
+        )
         print("\033[35m\nfail_update_options topic was broadcast")
 
     @pytest.mark.unit
     def test_do_update_non_existent_id(self, mock_common_dao):
         """do_update() should return a non-zero error code when passed a
         Options ID that doesn't exist."""
-        pub.subscribe(self.on_fail_update_non_existent_id,
-                      'fail_update_options')
+        pub.subscribe(self.on_fail_update_non_existent_id, "fail_update_options")
 
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
-        DUT.do_update('skullduggery', table='siteinfo')
+        DUT.do_select_all({"site_id": 1})
+        DUT.do_update("skullduggery", table="siteinfo")
 
-        pub.unsubscribe(self.on_fail_update_non_existent_id,
-                        'fail_update_options')
+        pub.unsubscribe(self.on_fail_update_non_existent_id, "fail_update_options")
 
     @pytest.mark.unit
     def test_do_update_no_data_package(self, mock_common_dao):
         """do_update() should return a non-zero error code when passed a
         Options ID that doesn't exist."""
-        pub.subscribe(self.on_fail_update_no_data_package,
-                      'fail_update_options')
+        pub.subscribe(self.on_fail_update_no_data_package, "fail_update_options")
 
         DUT = dmOptions()
         DUT.do_connect(mock_common_dao)
-        DUT.do_select_all({'site_id': 1})
-        DUT.tree.get_node(1).data.pop('siteinfo')
+        DUT.do_select_all({"site_id": 1})
+        DUT.tree.get_node(1).data.pop("siteinfo")
 
-        DUT.do_update(1, table='siteinfo')
+        DUT.do_update(1, table="siteinfo")
 
-        pub.unsubscribe(self.on_fail_update_no_data_package,
-                        'fail_update_options')
+        pub.unsubscribe(self.on_fail_update_no_data_package, "fail_update_options")
