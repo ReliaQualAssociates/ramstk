d00b1827336a170949f429592df5d032bbe4309b
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-06-18 08:55:00 -0400

    test: add mock RAMSTKFunction table for unit tests

--- tests/mocks/__init__.py
+++ tests/mocks/__init__.py
@@ -8,4 +8,9 @@
 # Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
 
 # RAMSTK Local Imports
+from .mock_ramstkallocation import MockRAMSTKAllocation
+from .mock_ramstkenvironment import MockRAMSTKEnvironment
+from .mock_ramstkfailuredefinition import MockRAMSTKFailureDefinition
+from .mock_ramstkfunction import MockRAMSTKFunction
+from .mock_tables import MockRAMSTKBaseTable
 from .MockDAO import MockDAO
