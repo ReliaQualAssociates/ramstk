295d9bffc73571ce95c09c21127f2004676b37c6
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-06-26 12:52:51 -0400

    test: add fixtures for test managers

--- tests/controllers/failure_definition/failure_definition_integration_test.py
+++ tests/controllers/failure_definition/failure_definition_integration_test.py
@@ -18,7 +18,33 @@ from treelib import Tree
 from ramstk.controllers import dmFailureDefinition
 
 
-@pytest.mark.usefixtures("test_program_dao")
+@pytest.fixture(scope="class")
+def test_datamanager(test_program_dao):
+    """Get a data manager instance for each test class."""
+    # Create the device under test (dut) and connect to the database.
+    dut = dmFailureDefinition()
+    dut.do_connect(test_program_dao)
+    dut.do_select_all(attributes={"revision_id": 1})
+
+    yield dut
+
+    # Unsubscribe from pypubsub topics.
+    pub.unsubscribe(dut.do_get_attributes, "request_get_failure_definition_attributes")
+    pub.unsubscribe(dut.do_set_attributes, "request_set_failure_definition_attributes")
+    pub.unsubscribe(dut.do_set_attributes, "lvw_editing_failure_definition")
+    pub.unsubscribe(dut.do_update, "request_update_failure_definition")
+    pub.unsubscribe(dut.do_get_tree, "request_get_failure_definition_tree")
+    pub.unsubscribe(dut.do_select_all, "selected_revision")
+    pub.unsubscribe(dut._do_delete, "request_delete_failure_definitions")
+    pub.unsubscribe(
+        dut._do_insert_failure_definition, "request_insert_failure_definitions"
+    )
+
+    # Delete the device under test.
+    del dut
+
+
+@pytest.mark.usefixtures("test_datamanager")
 class TestInsertMethods:
     """Class to test data controller insert methods using actual database."""
 
@@ -30,24 +56,21 @@ class TestInsertMethods:
         print("\033[35m\nfail_insert_function topic was broadcast.")
 
     @pytest.mark.integration
-    def test_do_insert_no_revision(self, test_program_dao):
+    def test_do_insert_no_revision(self, test_datamanager):
         """do_insert() should send the fail_insert_failure_definition message
         when attempting to insert a new failure definition with a non-existent
         revision ID."""
         pub.subscribe(self.on_fail_insert_no_revision, "fail_insert_failure_definition")
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(test_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT._revision_id = 40
-        DUT._do_insert_failure_definition()
+        test_datamanager._revision_id = 40
+        test_datamanager._do_insert_failure_definition()
 
         pub.unsubscribe(
             self.on_fail_insert_no_revision, "fail_insert_failure_definition"
         )
 
 
-@pytest.mark.usefixtures("test_program_dao")
+@pytest.mark.usefixtures("test_datamanager")
 class TestUpdateMethods:
     """Class to test data controller update methods using actual database."""
 
@@ -63,61 +86,49 @@ class TestUpdateMethods:
         print("\033[35m\nfail_update_failure_definition topic was broadcast")
 
     @pytest.mark.integration
-    def test_do_update(self, test_program_dao):
+    def test_do_update(self, test_datamanager):
         """do_update() should send the succeed_update_failure_definition on
         success."""
         pub.subscribe(self.on_succeed_update, "succeed_update_failure_definition")
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(test_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-
-        _failure_definition = DUT.do_select(1, table="failure_definition")
+        _failure_definition = test_datamanager.do_select(1, table="failure_definition")
         _failure_definition.definition = "Big test definition"
 
-        DUT.do_update(1, table="failure_definition")
-        _failure_definition = DUT.do_select(1, table="failure_definition")
+        test_datamanager.do_update(1, table="failure_definition")
+        _failure_definition = test_datamanager.do_select(1, table="failure_definition")
 
         assert _failure_definition.definition == "Big test definition"
 
         pub.unsubscribe(self.on_succeed_update, "succeed_update_failure_definition")
 
     @pytest.mark.integration
-    def test_do_update_all(self, test_program_dao):
+    def test_do_update_all(self, test_datamanager):
         """do_update_all failure_definition() should return None on success."""
-        DUT = dmFailureDefinition()
-        DUT.do_connect(test_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-
-        _failure_definition = DUT.do_select(1, table="failure_definition")
+        _failure_definition = test_datamanager.do_select(1, table="failure_definition")
         _failure_definition.definition = "Big test definition #1"
-        _failure_definition = DUT.do_select(2, table="failure_definition")
+        _failure_definition = test_datamanager.do_select(2, table="failure_definition")
         _failure_definition.definition = "Big test definition #2"
 
         pub.sendMessage("request_update_all_failure_definitions")
 
-        _failure_definition = DUT.do_select(1, table="failure_definition")
+        _failure_definition = test_datamanager.do_select(1, table="failure_definition")
         assert _failure_definition.definition == "Big test definition #1"
 
-        _failure_definition = DUT.do_select(2, table="failure_definition")
+        _failure_definition = test_datamanager.do_select(2, table="failure_definition")
         assert _failure_definition.definition == "Big test definition #2"
 
     @pytest.mark.integration
-    def test_do_update_wrong_data_type(self, test_program_dao):
+    def test_do_update_wrong_data_type(self, test_datamanager):
         """do_update() should send the succeed_update_failure_definition on
         success."""
         pub.subscribe(
             self.on_fail_update_wrong_data_type, "fail_update_failure_definition"
         )
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(test_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT.tree.get_node(1).data["failure_definition"].definition = {
+        test_datamanager.tree.get_node(1).data["failure_definition"].definition = {
             1: "Big test definition",
         }
-
-        DUT.do_update(1, table="failure_definition")
+        test_datamanager.do_update(1, table="failure_definition")
 
         pub.unsubscribe(
             self.on_fail_update_wrong_data_type, "fail_update_failure_definition"
--- tests/controllers/failure_definition/failure_definition_unit_test.py
+++ tests/controllers/failure_definition/failure_definition_unit_test.py
@@ -44,6 +44,31 @@ def mock_program_dao(monkeypatch):
     yield DAO
 
 
+@pytest.fixture(scope="function")
+def test_datamanager(mock_program_dao):
+    """Get a data manager instance for each test function."""
+    # Create the device under test (dut) and connect to the database.
+    dut = dmFailureDefinition()
+    dut.do_connect(mock_program_dao)
+
+    yield dut
+
+    # Unsubscribe from pypubsub topics.
+    pub.unsubscribe(dut.do_get_attributes, "request_get_failure_definition_attributes")
+    pub.unsubscribe(dut.do_set_attributes, "request_set_failure_definition_attributes")
+    pub.unsubscribe(dut.do_set_attributes, "lvw_editing_failure_definition")
+    pub.unsubscribe(dut.do_update, "request_update_failure_definition")
+    pub.unsubscribe(dut.do_get_tree, "request_get_failure_definition_tree")
+    pub.unsubscribe(dut.do_select_all, "selected_revision")
+    pub.unsubscribe(dut._do_delete, "request_delete_failure_definitions")
+    pub.unsubscribe(
+        dut._do_insert_failure_definition, "request_insert_failure_definitions"
+    )
+
+    # Delete the device under test.
+    del dut
+
+
 class TestCreateControllers:
     """Class for controller initialization test suite."""
 
@@ -77,72 +102,66 @@ class TestCreateControllers:
         )
 
 
+@pytest.mark.usefixtures("test_datamanager")
 class TestSelectMethods:
     """Class for testing data manager select_all() and select() methods."""
 
     @pytest.mark.unit
-    def test_do_select_all(self, mock_program_dao):
+    def test_do_select_all(self, test_datamanager):
         """do_select_all() should return a Tree() object populated with
         RAMSTKFailureDefinition instances on success."""
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
 
-        assert isinstance(DUT.tree, Tree)
+        assert isinstance(test_datamanager.tree, Tree)
         assert isinstance(
-            DUT.tree.get_node(1).data["failure_definition"], MockRAMSTKFailureDefinition
+            test_datamanager.tree.get_node(1).data["failure_definition"],
+            MockRAMSTKFailureDefinition,
         )
 
     @pytest.mark.unit
-    def test_do_select_all_populated_tree(self, mock_program_dao):
+    def test_do_select_all_populated_tree(self, test_datamanager):
         """do_select_all() should return a Tree() object populated with
         RAMSTKFailureDefinition instances on success when there is already a
         tree of definitions."""
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
 
-        assert isinstance(DUT.tree, Tree)
+        assert isinstance(test_datamanager.tree, Tree)
         assert isinstance(
-            DUT.tree.get_node(1).data["failure_definition"], MockRAMSTKFailureDefinition
+            test_datamanager.tree.get_node(1).data["failure_definition"],
+            MockRAMSTKFailureDefinition,
         )
 
     @pytest.mark.unit
-    def test_do_select(self, mock_program_dao):
+    def test_do_select(self, test_datamanager):
         """do_select() should return an instance of RAMSTKFailureDefinition on
         success."""
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
 
-        _failure_definition = DUT.do_select(1, table="failure_definition")
+        _failure_definition = test_datamanager.do_select(1, table="failure_definition")
 
         assert isinstance(_failure_definition, MockRAMSTKFailureDefinition)
         assert _failure_definition.definition == "Mock Failure Definition 1"
 
     @pytest.mark.unit
-    def test_do_select_unknown_table(self, mock_program_dao):
+    def test_do_select_unknown_table(self, test_datamanager):
         """do_select() should raise a KeyError when an unknown table name is
         requested."""
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
 
         with pytest.raises(KeyError):
-            DUT.do_select(1, table="scibbidy-bibbidy-doo")
+            test_datamanager.do_select(1, table="scibbidy-bibbidy-doo")
 
     @pytest.mark.unit
-    def test_do_select_non_existent_id(self, mock_program_dao):
+    def test_do_select_non_existent_id(self, test_datamanager):
         """do_select() should return None when a non-existent Revision ID is
         requested."""
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
 
-        assert DUT.do_select(100, table="failure_definition") is None
+        assert test_datamanager.do_select(100, table="failure_definition") is None
 
 
+@pytest.mark.usefixtures("test_datamanager")
 class TestDeleteMethods:
     """Class for testing the data manager delete() method."""
 
@@ -157,39 +176,36 @@ class TestDeleteMethods:
         print("\033[35m\nfail_delete_failure_definition topic was broadcast.")
 
     @pytest.mark.unit
-    def test_do_delete(self, mock_program_dao):
+    def test_do_delete(self, test_datamanager):
         """_do_delete_failure_definition() should send the success message
         after successfully deleting a definition."""
         pub.subscribe(self.on_succeed_delete, "succeed_delete_failure_definition")
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT._do_delete(1)
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager._do_delete(1)
 
         with pytest.raises(AttributeError):
-            __ = DUT.tree.get_node(1).data["failure_definition"]
+            test_datamanager.tree.get_node(1).data["failure_definition"]
 
         pub.unsubscribe(self.on_succeed_delete, "succeed_delete_failure_definition")
 
     @pytest.mark.unit
-    def test_do_delete_non_existent_id(self, mock_program_dao):
+    def test_do_delete_non_existent_id(self, test_datamanager):
         """_do_delete_failure_definition() should send the fail message when
         attempting to delete a non-existent failure definition."""
         pub.subscribe(
             self.on_fail_delete_non_existent_id, "fail_delete_failure_definition"
         )
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT._do_delete(10)
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager._do_delete(10)
 
         pub.unsubscribe(
             self.on_fail_delete_non_existent_id, "fail_delete_failure_definition"
         )
 
 
+@pytest.mark.usefixtures("test_datamanager")
 class TestGetterSetter:
     """Class for testing methods that get or set."""
 
@@ -209,55 +225,49 @@ class TestGetterSetter:
         print("\033[36m\nsucceed_get_failure_definition_tree topic was broadcast")
 
     @pytest.mark.unit
-    def test_do_get_attributes(self, mock_program_dao):
+    def test_do_get_attributes(self, test_datamanager):
         """_do_get_attributes() should return a dict of failure definition
         records on success."""
         pub.subscribe(
             self.on_succeed_get_attributes, "succeed_get_failure_definition_attributes"
         )
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT.do_get_attributes(1, "failure_definition")
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_get_attributes(1, "failure_definition")
 
         pub.unsubscribe(
             self.on_succeed_get_attributes, "succeed_get_failure_definition_attributes"
         )
 
     @pytest.mark.unit
-    def test_do_set_attributes(self, mock_program_dao):
+    def test_do_set_attributes(self, test_datamanager):
         """do_set_attributes() should send the success message."""
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-
-        DUT.do_set_attributes(
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_set_attributes(
             node_id=[1, 1, ""], package={"definition": "Test Description"}
         )
 
         assert (
-            DUT.do_select(1, table="failure_definition").definition
+            test_datamanager.do_select(1, table="failure_definition").definition
             == "Test Description"
         )
 
     @pytest.mark.unit
-    def test_on_get_data_manager_tree(self, mock_program_dao):
+    def test_on_get_data_manager_tree(self, test_datamanager):
         """on_get_tree() should return the failure definition treelib Tree."""
         pub.subscribe(
             self.on_succeed_get_data_manager_tree, "succeed_get_failure_definition_tree"
         )
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT.do_get_tree()
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_get_tree()
 
         pub.unsubscribe(
             self.on_succeed_get_data_manager_tree, "succeed_get_failure_definition_tree"
         )
 
 
+@pytest.mark.usefixtures("test_datamanager")
 class TestInsertMethods:
     """Class for testing the data manager insert() method."""
 
@@ -268,20 +278,19 @@ class TestInsertMethods:
         print("\033[36m\nsucceed_insert_failure_definition topic was broadcast")
 
     @pytest.mark.unit
-    def test_do_insert_sibling(self, mock_program_dao):
+    def test_do_insert_sibling(self, test_datamanager):
         """do_insert() should send the success message after successfully
         inserting a new failure definition."""
         pub.subscribe(
             self.on_succeed_insert_sibling, "succeed_insert_failure_definition"
         )
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT._do_insert_failure_definition()
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager._do_insert_failure_definition()
 
         assert isinstance(
-            DUT.tree.get_node(1).data["failure_definition"], MockRAMSTKFailureDefinition
+            test_datamanager.tree.get_node(1).data["failure_definition"],
+            MockRAMSTKFailureDefinition,
         )
 
         pub.unsubscribe(
@@ -289,6 +298,7 @@ class TestInsertMethods:
         )
 
 
+@pytest.mark.usefixtures("test_datamanager")
 class TestUpdateMethods:
     """Class for testing update() and update_all() methods."""
 
@@ -306,36 +316,31 @@ class TestUpdateMethods:
         print("\033[35m\nfail_update_failure_definition topic was broadcast")
 
     @pytest.mark.unit
-    def test_do_update_non_existent_id(self, mock_program_dao):
+    def test_do_update_non_existent_id(self, test_datamanager):
         """do_update_failure_definition() should broadcast the fail message
         when attempting to save a non-existent ID."""
         pub.subscribe(
             self.on_fail_update_non_existent_id, "fail_update_failure_definition"
         )
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT.do_update(100, table="failure_definitions")
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.do_update(100, table="failure_definitions")
 
         pub.unsubscribe(
             self.on_fail_update_non_existent_id, "fail_update_failure_definition"
         )
 
     @pytest.mark.unit
-    def test_do_update_no_data_package(self, mock_program_dao):
+    def test_do_update_no_data_package(self, test_datamanager):
         """do_update() should return a non-zero error code when passed a
         Function ID that has no data package."""
         pub.subscribe(
             self.on_fail_update_no_data_package, "fail_update_failure_definition"
         )
 
-        DUT = dmFailureDefinition()
-        DUT.do_connect(mock_program_dao)
-        DUT.do_select_all(attributes={"revision_id": 1})
-        DUT.tree.get_node(1).data.pop("failure_definition")
-
-        DUT.do_update(1, table="failure_definition")
+        test_datamanager.do_select_all(attributes={"revision_id": 1})
+        test_datamanager.tree.get_node(1).data.pop("failure_definition")
+        test_datamanager.do_update(1, table="failure_definition")
 
         pub.unsubscribe(
             self.on_fail_update_no_data_package, "fail_update_failure_definition"
