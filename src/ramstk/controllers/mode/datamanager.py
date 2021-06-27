759ec4b93f7f327b8977f8440ada6f759e367a80
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-06-27 17:22:43 -0400

    refactor: move Mode do_update() to metaclass

---
+++ src/ramstk/controllers/mode/datamanager.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+#
+#       ramstk.controllers.mode.datamanager.py is part of The RAMSTK Project
+#
+# All rights reserved.
+# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+"""Mode Package Data Controller."""
+
+# Standard Library Imports
+import inspect
+from typing import Any, Dict
+
+# Third Party Imports
+from pubsub import pub
+from treelib.exceptions import NodeIDAbsentError
+
+# RAMSTK Package Imports
+from ramstk.controllers import RAMSTKDataManager
+from ramstk.exceptions import DataAccessError
+from ramstk.models.programdb import RAMSTKMode
+
+
+class DataManager(RAMSTKDataManager):
+    """Contain the attributes and methods of the Mode data manager."""
+
+    _tag = "modes"
+    _root = 0
+
+    def __init__(self, **kwargs: Dict[str, Any]) -> None:
+        """Initialize a Mode data manager instance."""
+        super().__init__(**kwargs)
+
+        # Initialize private dictionary attributes.
+        self._pkey = {
+            "mode": ["revision_id", "hardware_id", "mode_id"],
+        }
+
+        # Initialize private list attributes.
+
+        # Initialize private scalar attributes.
+
+        # Initialize public dictionary attributes.
+
+        # Initialize public list attributes.
+
+        # Initialize public scalar attributes.
+
+        # Subscribe to PyPubSub messages.
+        pub.subscribe(super().do_get_attributes, "request_get_mode_attributes")
+        pub.subscribe(super().do_set_attributes, "request_set_mode_attributes")
+        pub.subscribe(super().do_set_attributes, "wvw_editing_mode")
+        pub.subscribe(super().do_update, "request_update_mode")
+
+        pub.subscribe(self.do_select_all, "selected_revision")
+        pub.subscribe(self.do_get_tree, "request_get_mode_tree")
+
+        pub.subscribe(self._do_delete, "request_delete_mode")
+        pub.subscribe(self._do_insert_mode, "request_insert_mode")
+
+    def do_get_tree(self) -> None:
+        """Retrieve the Mode treelib Tree.
+
+        :return: None
+        :rtype: None
+        """
+        pub.sendMessage(
+            "succeed_get_mode_tree",
+            tree=self.tree,
+        )
+
+    def do_select_all(self, attributes: Dict[str, Any]) -> None:
+        """Retrieve all the Mode data from the RAMSTK Program database.
+
+        :param attributes: the attributes dict for the selected
+            function or hardware item.
+        :return: None
+        :rtype: None
+        """
+        self._revision_id = attributes["revision_id"]
+        self._parent_id = attributes["hardware_id"]
+
+        for _node in self.tree.children(self.tree.root):
+            self.tree.remove_node(_node.identifier)
+
+        for _mode in self.dao.do_select_all(
+            RAMSTKMode,
+            key=["revision_id", "hardware_id"],
+            value=[self._revision_id, self._parent_id],
+        ):
+            self.tree.create_node(
+                tag="mode",
+                identifier=_mode.mode_id,
+                parent=self._root,
+                data={"mode": _mode},
+            )
+            self.last_id = _mode.mode_id
+
+        pub.sendMessage(
+            "succeed_retrieve_modes",
+            tree=self.tree,
+        )
+
+    def _do_delete(self, node_id: int) -> None:
+        """Remove a Mode element.
+
+        :param node_id: the node (Mode element) ID to be removed from the
+            RAMSTK Program database.
+        :return: None
+        :rtype: None
+        """
+        try:
+            _table = list(self.tree.get_node(node_id).data.keys())[0]
+
+            super().do_delete(node_id, _table)
+
+            self.tree.remove_node(node_id)
+
+            pub.sendMessage(
+                "succeed_delete_mode",
+                tree=self.tree,
+            )
+        except (AttributeError, DataAccessError, NodeIDAbsentError):
+            _method_name: str = inspect.currentframe().f_code.co_name  # type: ignore
+            _error_msg: str = (
+                "{1}: Attempted to delete non-existent mode ID {0}."
+            ).format(str(node_id), _method_name)
+            pub.sendMessage(
+                "do_log_debug",
+                logger_name="DEBUG",
+                message=_error_msg,
+            )
+            pub.sendMessage(
+                "fail_delete_mode",
+                error_message=_error_msg,
+            )
+
+    def _do_insert_mode(self) -> None:
+        """Add a new failure mode.
+
+        :return: None
+        :rtype: None
+        """
+        try:
+            _last_id: int = self.dao.get_last_id("ramstk_mission", "mission_id")
+            _mode = RAMSTKMode()
+            _mode.revision_id = self._revision_id
+            _mode.hardware_id = self._parent_id
+            _mode.mode_id = _last_id + 1
+
+            self.dao.do_insert(_mode)
+
+            self.tree.create_node(
+                tag="mode",
+                identifier=_mode.mode_id,
+                parent=self._root,
+                data={"mode": _mode},
+            )
+
+            self.last_id = max(self.last_id, _mode.mode_id)
+
+            pub.sendMessage(
+                "succeed_insert_mode",
+                node_id=_mode.mode_id,
+                tree=self.tree,
+            )
+        except DataAccessError as _error:
+            pub.sendMessage(
+                "do_log_debug",
+                logger_name="DEBUG",
+                message=_error.msg,
+            )
+            pub.sendMessage(
+                "fail_insert_mode",
+                error_message=_error.msg,
+            )
--- tests/models/programdb/ramstkmode_unit_test.py
+++ tests/models/programdb/ramstkmode_unit_test.py
@@ -2,21 +2,67 @@
 # type: ignore
 # -*- coding: utf-8 -*-
 #
-#       tests.models.programdb.TestRAMSTKMode.py is part of The RAMSTK Project
+#       tests.models.programdb.ramstkmode_unit_test.py is part of The RAMSTK
+#       Project
 #
 # All rights reserved.
-# Copyright 2007 - 2019 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""Test class for testing the RAMSTKMode module algorithms and models."""
+# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+"""Class for testing the RAMSTKMode module algorithms and models."""
 
 # Third Party Imports
+# noinspection PyPackageRequirements
 import pytest
+
+# noinspection PyUnresolvedReferences
+from mocks import MockDAO
 
 # RAMSTK Package Imports
 from ramstk.models.programdb import RAMSTKMode
 
+
+@pytest.fixture
+def mock_program_dao(monkeypatch):
+    _mode_1 = RAMSTKMode()
+    _mode_1.revision_id = 1
+    _mode_1.hardware_id = 1
+    _mode_1.mode_id = 1
+    _mode_1.effect_local = ""
+    _mode_1.mission = "Default Mission"
+    _mode_1.other_indications = ""
+    _mode_1.mode_criticality = 0.0
+    _mode_1.single_point = 0
+    _mode_1.design_provisions = ""
+    _mode_1.type_id = 0
+    _mode_1.rpn_severity_new = 1
+    _mode_1.effect_next = ""
+    _mode_1.detection_method = ""
+    _mode_1.operator_actions = ""
+    _mode_1.critical_item = 0
+    _mode_1.hazard_rate_source = ""
+    _mode_1.severity_class = ""
+    _mode_1.description = "Test Failure Mode #1"
+    _mode_1.mission_phase = ""
+    _mode_1.mode_probability = ""
+    _mode_1.remarks = ""
+    _mode_1.mode_ratio = 0.0
+    _mode_1.mode_hazard_rate = 0.0
+    _mode_1.rpn_severity = 1
+    _mode_1.isolation_method = ""
+    _mode_1.effect_end = ""
+    _mode_1.mode_op_time = 0.0
+    _mode_1.effect_probability = 0.0
+
+    DAO = MockDAO()
+    DAO.table = [
+        _mode_1,
+    ]
+
+    yield DAO
+
+
 ATTRIBUTES = {
     "effect_local": "",
-    "mission": "Default Mission",
+    "mission": "Big Mission",
     "other_indications": "",
     "mode_criticality": 0.0,
     "single_point": 0,
@@ -29,7 +75,7 @@ ATTRIBUTES = {
     "critical_item": 0,
     "hazard_rate_source": "",
     "severity_class": "",
-    "description": "Test Functional Failure Mode #1",
+    "description": "Big Failure Mode",
     "mission_phase": "",
     "mode_probability": "",
     "remarks": "",
@@ -43,14 +89,14 @@ ATTRIBUTES = {
 }
 
 
-@pytest.mark.usefixtures("test_program_dao")
+@pytest.mark.usefixtures("mock_program_dao")
 class TestRAMSTKMode:
     """Class for testing the RAMSTKMode model."""
 
-    @pytest.mark.integration
-    def test_ramstkmode_create(self, test_program_dao):
-        """ __init__() should create an RAMSTKMode model. """
-        DUT = test_program_dao.session.query(RAMSTKMode).first()
+    @pytest.mark.unit
+    def test_ramstkmode_create(self, mock_program_dao):
+        """__init__() should create an RAMSTKMode model."""
+        DUT = mock_program_dao.do_select_all(RAMSTKMode)[0]
 
         assert isinstance(DUT, RAMSTKMode)
 
@@ -58,89 +104,94 @@ class TestRAMSTKMode:
         assert DUT.__tablename__ == "ramstk_mode"
         assert DUT.revision_id == 1
         assert DUT.hardware_id == 1
-        assert DUT.mode_id == 4
+        assert DUT.mode_id == 1
         assert DUT.critical_item == 0
-        assert DUT.description == "System Test Failure Mode #1"
+        assert DUT.description == "Test Failure Mode #1"
         assert DUT.design_provisions == ""
         assert DUT.detection_method == ""
         assert DUT.effect_end == ""
         assert DUT.effect_local == ""
         assert DUT.effect_next == ""
-        assert DUT.effect_probability == 1.0
+        assert DUT.effect_probability == 0.0
         assert DUT.hazard_rate_source == ""
         assert DUT.isolation_method == ""
         assert DUT.mission == "Default Mission"
         assert DUT.mission_phase == ""
         assert DUT.mode_criticality == 0.0
         assert DUT.mode_hazard_rate == 0.0
-        assert DUT.mode_op_time == 10.0
+        assert DUT.mode_op_time == 0.0
         assert DUT.mode_probability == ""
-        assert DUT.mode_ratio == 0.5
+        assert DUT.mode_ratio == 0.0
         assert DUT.operator_actions == ""
         assert DUT.other_indications == ""
         assert DUT.remarks == ""
         assert DUT.rpn_severity == 1
         assert DUT.rpn_severity_new == 1
-        assert DUT.severity_class == "IV"
+        assert DUT.severity_class == ""
         assert DUT.single_point == 0
         assert DUT.type_id == 0
 
-    @pytest.mark.integration
-    def test_get_attributes(self, test_program_dao):
-        """ get_attributes() should return a dict of attribute name:value pairs. """
-        DUT = test_program_dao.session.query(RAMSTKMode).first()
+    @pytest.mark.unit
+    def test_get_attributes(self, mock_program_dao):
+        """get_attributes() should return a dict of attribute name:value
+        pairs."""
+        DUT = mock_program_dao.do_select_all(RAMSTKMode)[0]
 
         _attributes = DUT.get_attributes()
         assert _attributes["revision_id"] == 1
         assert _attributes["hardware_id"] == 1
-        assert _attributes["mode_id"] == 4
+        assert _attributes["mode_id"] == 1
         assert _attributes["critical_item"] == 0
-        assert _attributes["description"] == "System Test Failure Mode #1"
+        assert _attributes["description"] == "Test Failure Mode #1"
         assert _attributes["design_provisions"] == ""
         assert _attributes["detection_method"] == ""
         assert _attributes["effect_end"] == ""
         assert _attributes["effect_local"] == ""
         assert _attributes["effect_next"] == ""
-        assert _attributes["effect_probability"] == 1.0
+        assert _attributes["effect_probability"] == 0.0
         assert _attributes["hazard_rate_source"] == ""
         assert _attributes["isolation_method"] == ""
         assert _attributes["mission"] == "Default Mission"
         assert _attributes["mission_phase"] == ""
         assert _attributes["mode_criticality"] == 0.0
         assert _attributes["mode_hazard_rate"] == 0.0
-        assert _attributes["mode_op_time"] == 10.0
+        assert _attributes["mode_op_time"] == 0.0
         assert _attributes["mode_probability"] == ""
-        assert _attributes["mode_ratio"] == 0.5
+        assert _attributes["mode_ratio"] == 0.0
         assert _attributes["operator_actions"] == ""
         assert _attributes["other_indications"] == ""
         assert _attributes["remarks"] == ""
         assert _attributes["rpn_severity"] == 1
         assert _attributes["rpn_severity_new"] == 1
-        assert _attributes["severity_class"] == "IV"
+        assert _attributes["severity_class"] == ""
         assert _attributes["single_point"] == 0
         assert _attributes["type_id"] == 0
 
-    @pytest.mark.integration
-    def test_set_attributes(self, test_program_dao):
-        """ set_attributes() should return a zero error code on success. """
-        DUT = test_program_dao.session.query(RAMSTKMode).first()
+    @pytest.mark.unit
+    def test_set_attributes(self, mock_program_dao):
+        """set_attributes() should return a zero error code on success."""
+        DUT = mock_program_dao.do_select_all(RAMSTKMode)[0]
 
         assert DUT.set_attributes(ATTRIBUTES) is None
+        assert DUT.mission == "Big Mission"
+        assert DUT.description == "Big Failure Mode"
 
-    @pytest.mark.integration
-    def test_set_attributes_none_value(self, test_program_dao):
-        """set_attributes() should set an attribute to it's default value when the attribute is passed with a None value."""
-        DUT = test_program_dao.session.query(RAMSTKMode).first()
+    @pytest.mark.unit
+    def test_set_attributes_set_default(self, mock_program_dao):
+        """set_attributes() should set an attribute to it's default value when
+        the attribute is passed with a None value."""
+        DUT = mock_program_dao.do_select_all(RAMSTKMode)[0]
 
         ATTRIBUTES["description"] = None
 
         assert DUT.set_attributes(ATTRIBUTES) is None
         assert DUT.get_attributes()["description"] == ""
 
-    @pytest.mark.integration
-    def test_set_attributes_unknown_attributes(self, test_program_dao):
-        """set_attributes() should raise an AttributeError when passed an unknown attribute."""
-        DUT = test_program_dao.session.query(RAMSTKMode).first()
+    @pytest.mark.unit
+    def test_set_attributes_unknown_attributes(self, mock_program_dao):
+        """set_attributes() should raise an AttributeError when passed an
+        unknown attribute."""
+        DUT = mock_program_dao.do_select_all(RAMSTKMode)[0]
 
         with pytest.raises(AttributeError):
             DUT.set_attributes({"shibboly-bibbly-boo": 0.9998})
