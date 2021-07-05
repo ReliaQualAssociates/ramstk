c07f5f04c4b2d314f14ba72f46ff956efeb230b3
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-07-05 11:35:45 -0400

    test: add fixtures for Revision test datamanager (#531)

--- tests/controllers/revision/revision_unit_test.py
+++ tests/controllers/revision/revision_unit_test.py
@@ -99,6 +99,14 @@ def test_datamanager(mock_program_dao):
     yield dut
 
     # Unsubscribe from pypubsub topics.
+    pub.unsubscribe(dut.do_get_attributes, "request_get_revision_attributes")
+    pub.unsubscribe(dut.do_set_attributes, "request_set_revision_attributes")
+    pub.unsubscribe(dut.do_set_attributes, "wvw_editing_revision")
+    pub.unsubscribe(dut.do_update, "request_update_revision")
+    pub.unsubscribe(dut.do_get_tree, "request_get_revision_tree")
+    pub.unsubscribe(dut.do_select_all, "request_retrieve_revisions")
+    pub.unsubscribe(dut._do_delete, "request_delete_revision")
+    pub.unsubscribe(dut._do_insert_revision, "request_insert_revision")
 
     # Delete the device under test.
     del dut
@@ -130,6 +138,16 @@ class TestCreateControllers:
         )
         assert pub.isSubscribed(DUT._do_delete, "request_delete_revision")
         assert pub.isSubscribed(DUT._do_insert_revision, "request_insert_revision")
+
+        # Unsubscribe from pypubsub topics.
+        pub.unsubscribe(DUT.do_get_attributes, "request_get_revision_attributes")
+        pub.unsubscribe(DUT.do_set_attributes, "request_set_revision_attributes")
+        pub.unsubscribe(DUT.do_set_attributes, "wvw_editing_revision")
+        pub.unsubscribe(DUT.do_update, "request_update_revision")
+        pub.unsubscribe(DUT.do_get_tree, "request_get_revision_tree")
+        pub.unsubscribe(DUT.do_select_all, "request_retrieve_revisions")
+        pub.unsubscribe(DUT._do_delete, "request_delete_revision")
+        pub.unsubscribe(DUT._do_insert_revision, "request_insert_revision")
 
 
 @pytest.mark.usefixtures("test_datamanager")
@@ -185,122 +203,13 @@ class TestSelectMethods:
 
 
 @pytest.mark.usefixtures("test_datamanager")
-class TestDeleteMethods:
-    """Class for testing the data manager delete() method."""
-
-    def on_succeed_delete(self, tree):
-        assert isinstance(tree, Tree)
-        print("\033[36m\nsucceed_delete_revision topic was broadcast.")
-
-    def on_fail_delete_non_existent_id(self, error_message):
-        assert error_message == (
-            "_do_delete: Attempted to delete non-existent revision ID 300."
-        )
-        print("\033[35m\nfail_delete_revision topic was broadcast.")
-
-    @pytest.mark.unit
-    def test_do_delete(self, test_datamanager):
-        """_do_delete() should send the success message with the treelib
-        Tree."""
-        pub.subscribe(self.on_succeed_delete, "succeed_delete_revision")
-
-        test_datamanager.do_select_all()
-        test_datamanager._do_delete(test_datamanager.last_id)
-
-        assert test_datamanager.last_id == 1
-
-        pub.unsubscribe(self.on_succeed_delete, "succeed_delete_revision")
-
-    @pytest.mark.unit
-    def test_do_delete_non_existent_id(self, test_datamanager):
-        """_do_delete() should send the fail message when attempting to delete
-        a non-existent revision."""
-        pub.subscribe(self.on_fail_delete_non_existent_id, "fail_delete_revision")
-
-        test_datamanager.do_select_all()
-        test_datamanager._do_delete(300)
-
-        pub.unsubscribe(self.on_fail_delete_non_existent_id, "fail_delete_revision")
-
-
-@pytest.mark.usefixtures("test_datamanager")
-class TestGetterSetter:
-    """Class for testing methods that get or set."""
-
-    def on_succeed_get_attributes(self, attributes):
-        assert isinstance(attributes, dict)
-        assert attributes["revision_id"] == 1
-        assert attributes["name"] == "Original Revision"
-        assert attributes["program_time"] == 2562
-        print("\033[36m\nsucceed_get_revision_attributes topic was broadcast")
-
-    def on_succeed_get_data_manager_tree(self, tree):
-        assert isinstance(tree, Tree)
-        assert isinstance(tree.get_node(1).data["revision"], MockRAMSTKRevision)
-        print("\033[36m\nsucceed_get_revision_tree topic was broadcast")
-
-    @pytest.mark.unit
-    def test_do_get_attributes(self, test_datamanager):
-        """_do_get_attributes() should return a dict of revision attributes on
-        success."""
-        pub.subscribe(self.on_succeed_get_attributes, "succeed_get_revision_attributes")
-
-        test_datamanager.do_select_all()
-        test_datamanager.do_get_attributes(1, "revision")
-
-        pub.unsubscribe(
-            self.on_succeed_get_attributes, "succeed_get_revision_attributes"
-        )
-
-    @pytest.mark.unit
-    def test_do_set_attributes(self, test_datamanager):
-        """do_set_attributes() should send the success message."""
-        test_datamanager.do_select_all()
-        test_datamanager.do_set_attributes(
-            node_id=[
-                1,
-            ],
-            package={"revision_code": "-"},
-        )
-
-        assert test_datamanager.do_select(1, table="revision").revision_code == "-"
-
-    @pytest.mark.unit
-    def test_on_get_data_manager_tree(self, test_datamanager):
-        """on_get_tree() should return the revision treelib Tree."""
-        pub.subscribe(
-            self.on_succeed_get_data_manager_tree, "succeed_get_revision_tree"
-        )
-
-        test_datamanager.do_select_all()
-        test_datamanager.do_get_tree()
-
-        pub.unsubscribe(
-            self.on_succeed_get_data_manager_tree, "succeed_get_revision_tree"
-        )
-
-
-@pytest.mark.usefixtures("test_datamanager")
 class TestInsertMethods:
     """Class for testing the data manager insert() method."""
-
-    def on_succeed_insert_sibling(self, node_id, tree):
-        assert node_id == 3
-        assert isinstance(tree, Tree)
-        print("\033[36m\nsucceed_insert_revision topic was broadcast")
-
-    def on_fail_insert_no_database(self, error_message):
-        assert error_message == (
-            "_do_insert_revision: Failed to insert " "revision into program database."
-        )
-        print("\033[35m\nfail_insert_revision topic was broadcast.")
 
     @pytest.mark.unit
     def test_do_insert_sibling(self, test_datamanager):
         """_do_insert_revision() should send the success message after
         successfully inserting a new revision."""
-        pub.subscribe(self.on_succeed_insert_sibling, "succeed_insert_revision")
-
         test_datamanager.do_select_all()
         test_datamanager._do_insert_revision()
 
@@ -310,54 +219,17 @@ class TestInsertMethods:
         assert test_datamanager.tree.get_node(3).data["revision"].revision_id == 3
         assert test_datamanager.tree.get_node(3).data["revision"].name == "New Revision"
 
-        pub.unsubscribe(self.on_succeed_insert_sibling, "succeed_insert_revision")
-
-    @pytest.mark.unit
-    def test_do_insert_no_database(self):
-        """_do_insert_revision() should send the success message after
-        successfully inserting a new revision."""
-        pub.subscribe(self.on_fail_insert_no_database, "fail_insert_revision")
-
-        DUT = dmRevision()
-        DUT._do_insert_revision()
-
-        pub.unsubscribe(self.on_fail_insert_no_database, "fail_insert_revision")
-
 
 @pytest.mark.usefixtures("test_datamanager")
-class TestUpdateMethods:
-    """Class for testing update() and update_all() methods."""
-
-    def on_fail_update_non_existent_id(self, error_message):
-        assert error_message == (
-            "do_update: Attempted to save non-existent revision with revision "
-            "ID 100."
-        )
-        print("\033[35m\nfail_update_revision topic was broadcast")
-
-    def on_fail_update_no_data_package(self, error_message):
-        assert error_message == ("do_update: No data package found for revision ID 1.")
-        print("\033[35m\nfail_update_revision topic was broadcast")
+class TestDeleteMethods:
+    """Class for testing the data manager delete() method."""
 
     @pytest.mark.unit
-    def test_do_update_non_existent_id(self, test_datamanager):
-        """do_update() should return a non-zero error code when passed a
-        revision ID that doesn't exist."""
-        pub.subscribe(self.on_fail_update_non_existent_id, "fail_update_revision")
-
+    def test_do_delete(self, test_datamanager):
+        """_do_delete() should send the success message with the treelib
+        Tree."""
         test_datamanager.do_select_all()
-        test_datamanager.do_update(100, table="revision")
+        test_datamanager._do_delete(2)
 
-        pub.unsubscribe(self.on_fail_update_non_existent_id, "fail_update_revision")
-
-    @pytest.mark.unit
-    def test_do_update_no_data_package(self, test_datamanager):
-        """do_update() should return a non-zero error code when passed a
-        Function ID that has no data package."""
-        pub.subscribe(self.on_fail_update_no_data_package, "fail_update_revision")
-
-        test_datamanager.do_select_all()
-        test_datamanager.tree.get_node(1).data.pop("revision")
-        test_datamanager.do_update(1, table="revision")
-
-        pub.unsubscribe(self.on_fail_update_no_data_package, "fail_update_revision")
+        assert test_datamanager.last_id == 1
+        assert test_datamanager.tree.get_node(2) is None
