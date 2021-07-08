665b68f0d022b06dda2fa6593614a76de445ab51
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-07-08 08:59:34 -0400

    refactor: move Stakeholder do_update to metaclass

--- src/ramstk/controllers/stakeholder/datamanager.py
+++ src/ramstk/controllers/stakeholder/datamanager.py
@@ -3,7 +3,7 @@
 #       ramstk.controllers.stakeholder.py is part of The RAMSTK Project
 #
 # All rights reserved.
-# Copyright 2007 - 2020 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
 """Stakeholder Package Data Model."""
 
 # Standard Library Imports
@@ -27,14 +27,14 @@ class DataManager(RAMSTKDataManager):
     and RAMSTKStakeholder data models.
     """
 
-    _tag = 'stakeholders'
+    _tag = "stakeholders"
 
     def __init__(self, **kwargs: Dict[Any, Any]) -> None:
         """Initialize a Stakeholder data manager instance."""
         super().__init__(**kwargs)
 
         # Initialize private dictionary attributes.
-        self._pkey = {'stakeholder': ['revision_id', 'stakeholder_id']}
+        self._pkey = {"stakeholder": ["revision_id", "stakeholder_id"]}
 
         # Initialize private list attributes.
 
@@ -47,20 +47,16 @@ class DataManager(RAMSTKDataManager):
         # Initialize public scalar attributes.
 
         # Subscribe to PyPubSub messages.
-        pub.subscribe(super().do_get_attributes,
-                      'request_get_stakeholder_attributes')
-        pub.subscribe(super().do_set_attributes,
-                      'request_set_stakeholder_attributes')
-        pub.subscribe(super().do_set_attributes, 'lvw_editing_stakeholder')
-        pub.subscribe(super().do_update_all, 'request_update_all_stakeholders')
+        pub.subscribe(super().do_get_attributes, "request_get_stakeholder_attributes")
+        pub.subscribe(super().do_set_attributes, "request_set_stakeholder_attributes")
+        pub.subscribe(super().do_set_attributes, "lvw_editing_stakeholder")
+        pub.subscribe(super().do_update, "request_update_stakeholders")
 
-        pub.subscribe(self.do_get_tree, 'request_get_stakeholder_tree')
-        pub.subscribe(self.do_select_all, 'selected_revision')
-        pub.subscribe(self.do_update, 'request_update_stakeholders')
+        pub.subscribe(self.do_get_tree, "request_get_stakeholder_tree")
+        pub.subscribe(self.do_select_all, "selected_revision")
 
-        pub.subscribe(self._do_delete, 'request_delete_stakeholder')
-        pub.subscribe(self._do_insert_stakeholder,
-                      'request_insert_stakeholder')
+        pub.subscribe(self._do_delete, "request_delete_stakeholder")
+        pub.subscribe(self._do_insert_stakeholder, "request_insert_stakeholder")
 
     def do_get_tree(self) -> None:
         """Retrieve the stakeholder treelib Tree.
@@ -69,7 +65,7 @@ class DataManager(RAMSTKDataManager):
         :rtype: None
         """
         pub.sendMessage(
-            'succeed_get_stakeholder_tree',
+            "succeed_get_stakeholder_tree",
             tree=self.tree,
         )
 
@@ -80,90 +76,32 @@ class DataManager(RAMSTKDataManager):
         :return: None
         :rtype: None
         """
-        self._revision_id = attributes['revision_id']
+        self._revision_id = attributes["revision_id"]
 
         for _node in self.tree.children(self.tree.root):
             self.tree.remove_node(_node.identifier)
 
         for _stakeholder in self.dao.do_select_all(
-                RAMSTKStakeholder,
-                key=['revision_id'],
-                value=[self._revision_id],
-                order=RAMSTKStakeholder.stakeholder_id):
-            _data_package = {'stakeholder': _stakeholder}
+            RAMSTKStakeholder,
+            key=["revision_id"],
+            value=[self._revision_id],
+            order=RAMSTKStakeholder.stakeholder_id,
+        ):
+            _data_package = {"stakeholder": _stakeholder}
 
-            self.tree.create_node(tag='stakeholder',
-                                  identifier=_stakeholder.stakeholder_id,
-                                  parent=self._root,
-                                  data=_data_package)
+            self.tree.create_node(
+                tag="stakeholder",
+                identifier=_stakeholder.stakeholder_id,
+                parent=self._root,
+                data=_data_package,
+            )
 
         self.last_id = max(self.tree.nodes.keys())
 
         pub.sendMessage(
-            'succeed_retrieve_stakeholders',
+            "succeed_retrieve_stakeholders",
             tree=self.tree,
         )
-
-    def do_update(self, node_id: int) -> None:
-        """Update record associated with node ID in RAMSTK Program database.
-
-        :param node_id: the node (stakeholder) ID of the stakeholder to save.
-        :return: None
-        :rtype: None
-        """
-        try:
-            self.dao.do_update(self.tree.get_node(node_id).data['stakeholder'])
-
-            pub.sendMessage(
-                'succeed_update_stakeholders',
-                tree=self.tree,
-            )
-        except AttributeError:
-            _method_name: str = inspect.currentframe(  # type: ignore
-            ).f_code.co_name
-            _error_msg: str = (
-                '{1}: Attempted to save non-existent stakeholder input with '
-                'stakeholder input ID {0}.').format(str(node_id), _method_name)
-            pub.sendMessage(
-                'do_log_debug',
-                logger_name='DEBUG',
-                message=_error_msg,
-            )
-            pub.sendMessage(
-                'fail_update_stakeholders',
-                error_message=_error_msg,
-            )
-        except KeyError:
-            _method_name: str = inspect.currentframe(  # type: ignore
-            ).f_code.co_name
-            _error_msg = (
-                '{1}: No data package found for stakeholder input ID {0}.'
-            ).format(str(node_id), _method_name)
-            pub.sendMessage(
-                'do_log_debug',
-                logger_name='DEBUG',
-                message=_error_msg,
-            )
-            pub.sendMessage(
-                'fail_update_stakeholders',
-                error_message=_error_msg,
-            )
-        except TypeError:
-            if node_id != 0:
-                _method_name: str = inspect.currentframe(  # type: ignore
-                ).f_code.co_name
-                _error_msg = ('{1}: The value for one or more attributes for '
-                              'stakeholder input ID {0} was the wrong '
-                              'type.').format(str(node_id), _method_name)
-                pub.sendMessage(
-                    'do_log_debug',
-                    logger_name='DEBUG',
-                    message=_error_msg,
-                )
-                pub.sendMessage(
-                    'fail_update_stakeholders',
-                    error_message=_error_msg,
-                )
 
     def _do_delete(self, node_id: int) -> None:
         """Remove a stakeholder.
@@ -174,28 +112,27 @@ class DataManager(RAMSTKDataManager):
         :rtype: None
         """
         try:
-            super().do_delete(node_id, 'stakeholder')
+            super().do_delete(node_id, "stakeholder")
 
             self.tree.remove_node(node_id)
             self.last_id = max(self.tree.nodes.keys())
 
             pub.sendMessage(
-                'succeed_delete_stakeholder',
+                "succeed_delete_stakeholder",
                 tree=self.tree,
             )
         except (AttributeError, DataAccessError, NodeIDAbsentError):
-            _method_name: str = inspect.currentframe(  # type: ignore
-            ).f_code.co_name
+            _method_name: str = inspect.currentframe().f_code.co_name  # type: ignore
             _error_msg: str = (
-                '{1}: Attempted to delete non-existent stakeholder input ID '
-                '{0}.').format(str(node_id), _method_name)
+                "{1}: Attempted to delete non-existent stakeholder input ID " "{0}."
+            ).format(str(node_id), _method_name)
             pub.sendMessage(
-                'do_log_debug',
-                logger_name='DEBUG',
+                "do_log_debug",
+                logger_name="DEBUG",
                 message=_error_msg,
             )
             pub.sendMessage(
-                'fail_delete_stakeholder',
+                "fail_delete_stakeholder",
                 error_message=_error_msg,
             )
 
@@ -209,25 +146,27 @@ class DataManager(RAMSTKDataManager):
             _stakeholder = RAMSTKStakeholder()
             _stakeholder.revision_id = self._revision_id
             _stakeholder.stakeholder_id = self.last_id + 1
-            _stakeholder.description = 'New Stakeholder Input'
+            _stakeholder.description = "New Stakeholder Input"
 
             self.dao.do_insert(_stakeholder)
 
             self.last_id = _stakeholder.stakeholder_id
-            self.tree.create_node(tag='stakeholder',
-                                  identifier=self.last_id,
-                                  parent=0,
-                                  data={'stakeholder': _stakeholder})
+            self.tree.create_node(
+                tag="stakeholder",
+                identifier=self.last_id,
+                parent=0,
+                data={"stakeholder": _stakeholder},
+            )
 
             pub.sendMessage(
-                'succeed_insert_stakeholder',
+                "succeed_insert_stakeholder",
                 node_id=self.last_id,
                 tree=self.tree,
             )
         except DataAccessError as _error:
             pub.sendMessage(
-                'do_log_debug',
-                logger_name='DEBUG',
+                "do_log_debug",
+                logger_name="DEBUG",
                 message=_error.msg,
             )
             pub.sendMessage(
---
+++ tests/models/programdb/ramstkprogramstatus_unit_test.py
@@ -0,0 +1,114 @@
+# pylint: skip-file
+# type: ignore
+# -*- coding: utf-8 -*-
+#
+#       tests.models.programdb.ramstkprogramstatus_unit_test.py is part of The
+#       RAMSTK Project
+#
+# All rights reserved.
+# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+"""Class for testing RAMSTKProgramStatus module algorithms and models."""
+
+# Standard Library Imports
+from datetime import date, timedelta
+
+# Third Party Imports
+# noinspection PyPackageRequirements
+import pytest
+
+# noinspection PyUnresolvedReferences
+from mocks import MockDAO
+
+# RAMSTK Package Imports
+from ramstk.models.programdb import RAMSTKProgramStatus
+
+
+@pytest.fixture
+def mock_program_dao(monkeypatch):
+    _status_1 = RAMSTKProgramStatus()
+    _status_1.revision_id = 1
+    _status_1.status_id = 1
+    _status_1.cost_remaining = 500.0
+    _status_1.date_status = date.today()
+    _status_1.time_remaining = 10.0
+
+    _status_2 = RAMSTKProgramStatus()
+    _status_2.revision_id = 1
+    _status_2.status_id = 1
+    _status_2.cost_remaining = 550.0
+    _status_2.date_status = date.today() - timedelta(1)
+    _status_2.time_remaining = 18.0
+
+    DAO = MockDAO()
+    DAO.table = [
+        _status_1,
+    ]
+
+    yield DAO
+
+
+ATTRIBUTES = {
+    "cost_remaining": 0.0,
+    "date_status": date.today() + timedelta(7),
+    "time_remaining": 0.0,
+}
+
+
+@pytest.mark.usefixtures("mock_program_dao")
+class TestRAMSTKProgramStatus:
+    """Class for testing the RAMSTKProgramStatus model."""
+
+    @pytest.mark.unit
+    def test_ramstkprogramstatus_create(self, mock_program_dao):
+        """__init__() should create an RAMSTKProgramStatus model."""
+        DUT = mock_program_dao.do_select_all(RAMSTKProgramStatus)[0]
+
+        assert isinstance(DUT, RAMSTKProgramStatus)
+        assert DUT.__tablename__ == "ramstk_program_status"
+        assert DUT.revision_id == 1
+        assert DUT.status_id == 1
+        assert DUT.cost_remaining == 500.0
+        assert DUT.date_status == date.today()
+        assert DUT.time_remaining == 10.0
+
+    @pytest.mark.unit
+    def test_get_attributes(self, mock_program_dao):
+        """get_attributes() should return a dict of attribute values."""
+        DUT = mock_program_dao.do_select_all(RAMSTKProgramStatus)[0]
+
+        _attributes = DUT.get_attributes()
+        assert _attributes["revision_id"] == 1
+        assert _attributes["status_id"] == 1
+        assert _attributes["cost_remaining"] == 500.0
+        assert _attributes["date_status"] == date.today()
+        assert _attributes["time_remaining"] == 10.0
+
+    @pytest.mark.unit
+    def test_set_attributes(self, mock_program_dao):
+        """set_attributes() should return a zero error code on success."""
+        DUT = mock_program_dao.do_select_all(RAMSTKProgramStatus)[0]
+
+        assert DUT.set_attributes(ATTRIBUTES) is None
+        assert DUT.cost_remaining == 0.0
+        assert DUT.date_status == date.today() + timedelta(7)
+        assert DUT.time_remaining == 0.0
+
+    @pytest.mark.unit
+    def test_set_attributes_none_value(self, mock_program_dao):
+        """set_attributes() should set an attribute to it's default value when
+        the attribute is passed with a None value."""
+        DUT = mock_program_dao.do_select_all(RAMSTKProgramStatus)[0]
+
+        ATTRIBUTES["time_remaining"] = None
+
+        assert DUT.set_attributes(ATTRIBUTES) is None
+        assert DUT.get_attributes()["time_remaining"] == 0.0
+
+    @pytest.mark.unit
+    def test_set_attributes_unknown_attributes(self, mock_program_dao):
+        """set_attributes() should raise an AttributeError when passed an
+        unknown attribute."""
+        DUT = mock_program_dao.do_select_all(RAMSTKProgramStatus)[0]
+
+        with pytest.raises(AttributeError):
+            DUT.set_attributes({"shibboly-bibbly-boo": 0.9998})
