aee2e37e1cd2035db153e60bf5795c557b092a5b
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-07-10 11:41:33 -0400

    test: cleanup validation test suite (#539)
    * test: add RAMSTKValidation mock tables for test
    * test: add fixtures for Validation test managers

--- tests/mocks/__init__.py
+++ tests/mocks/__init__.py
@@ -32,4 +32,5 @@ from .mock_ramstksimilaritem import MockRAMSTKSimilarItem
 from .mock_ramstksiteinfo import MockRAMSTKSiteInfo
 from .mock_ramstkstakeholder import MockRAMSTKStakeholder
 from .mock_ramstktestmethod import MockRAMSTKTestMethod
+from .mock_ramstkvalidation import MockRAMSTKValidation
 from .MockDAO import MockDAO
