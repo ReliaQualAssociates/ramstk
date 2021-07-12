3ebc9fb7c787b8b957f47ad722a661ddfde150c8
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-07-11 23:54:39 -0400

    test: add fixtures for test datamanager

--- src/ramstk/controllers/environment/datamanager.py
+++ src/ramstk/controllers/environment/datamanager.py
@@ -111,6 +111,7 @@ class DataManager(RAMSTKDataManager):
             super().do_delete(node_id, "environment")
 
             self.tree.remove_node(node_id)
+            self.last_id = max(self.tree.nodes.keys())
 
             pub.sendMessage(
                 "succeed_delete_environment",
