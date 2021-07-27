72e818ea02ba080973aae6bf066a596a38107082
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-07-27 16:41:14 -0400

    refactor: remove do_get_tree() from Program Status (#628)

--- tests/controllers/program_status/program_status_unit_test.py
+++ tests/controllers/program_status/program_status_unit_test.py
@@ -2,11 +2,11 @@
 # type: ignore
 # -*- coding: utf-8 -*-
 #
-#       tests.controllers.program_status.program_status_unit_test.py is part
-#       of The RAMSTK Project
+#       tests.controllers.program_status.program_status_unit_test.py is part of The
+#       RAMSTK Project
 #
 # All rights reserved.
-# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
 """Test class for testing Program Status module algorithms and models."""
 
 # Standard Library Imports
