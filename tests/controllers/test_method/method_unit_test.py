016abde38085d869cce159f84d35e584b16ec5b9
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-07-28 21:50:13 -0400

    refactor: remove do_get_tree from Test Method

--- tests/controllers/test_method/method_unit_test.py
+++ tests/controllers/test_method/method_unit_test.py
@@ -6,7 +6,7 @@
 #       Project
 #
 # All rights reserved.
-# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
 """Class for testing Test Method algorithms and models."""
 
 # Third Party Imports
