9400531c82f9c2e64d72cd392723d999863d8652
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-07-28 22:21:50 -0400

    Refactor/issue 608 (#634)
    * refactor: remove do_get_tree from Test Method

--- src/ramstk/controllers/test_method/datamanager.py
+++ src/ramstk/controllers/test_method/datamanager.py
@@ -3,7 +3,7 @@
 #       ramstk.controllers.test_method.datamanager.py is part of The RAMSTK Project
 #
 # All rights reserved.
-# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
 """Test Method Package Data Controller."""
 
 # Standard Library Imports
@@ -66,21 +66,9 @@ class DataManager(RAMSTKDataManager):
         pub.subscribe(super().do_update, "request_update_test_method")
 
         pub.subscribe(self.do_select_all, "selected_load")
-        pub.subscribe(self.do_get_tree, "request_get_test_method_tree")
 
         pub.subscribe(self._do_delete, "request_delete_test_method")
         pub.subscribe(self._do_insert_test_method, "request_insert_test_method")
-
-    def do_get_tree(self) -> None:
-        """Retrieve the Test Method treelib Tree.
-
-        :return: None
-        :rtype: None
-        """
-        pub.sendMessage(
-            "succeed_get_test_method_tree",
-            tree=self.tree,
-        )
 
     def do_select_all(self, attributes: Dict[str, Any]) -> None:
         """Retrieve all the Test Method data from the RAMSTK Program database.
