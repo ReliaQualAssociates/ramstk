11ee587fda00b3067bcdc82df0075362cd878aa9
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-02 20:57:30 -0400

    refactor: remove _do_insert_<> from OpLoad

--- tests/controllers/opload/opload_unit_test.py
+++ tests/controllers/opload/opload_unit_test.py
@@ -55,6 +55,17 @@ def mock_program_dao(monkeypatch):
 
 
 @pytest.fixture(scope="function")
+def test_attributes():
+    yield {
+        "revision_id": 1,
+        "hardware_id": 1,
+        "mode_id": 6,
+        "mechanism_id": 3,
+        "load_id": 3,
+    }
+
+
+@pytest.fixture(scope="function")
 def test_datamanager(mock_program_dao):
     """Get a data manager instance for each test function."""
     # Create the device under test (dut) and connect to the database.
@@ -71,7 +82,7 @@ def test_datamanager(mock_program_dao):
     pub.unsubscribe(dut.do_select_all, "selected_mechanism")
     pub.unsubscribe(dut.do_get_tree, "request_get_opload_tree")
     pub.unsubscribe(dut.do_delete, "request_delete_opload")
-    pub.unsubscribe(dut._do_insert_opload, "request_insert_opload")
+    pub.unsubscribe(dut.do_insert, "request_insert_opload")
 
     # Delete the device under test.
     del dut
@@ -87,6 +98,8 @@ class TestCreateControllers:
         assert isinstance(test_datamanager, dmOpLoad)
         assert isinstance(test_datamanager.tree, Tree)
         assert isinstance(test_datamanager.dao, MockDAO)
+        assert test_datamanager._db_id_colname == "fld_load_id"
+        assert test_datamanager._db_tablename == "ramstk_opload"
         assert test_datamanager._tag == "opload"
         assert test_datamanager._root == 0
         assert test_datamanager._revision_id == 0
@@ -105,64 +118,53 @@ class TestCreateControllers:
         assert pub.isSubscribed(test_datamanager.do_update, "request_update_opload")
         assert pub.isSubscribed(test_datamanager.do_get_tree, "request_get_opload_tree")
         assert pub.isSubscribed(test_datamanager.do_delete, "request_delete_opload")
-        assert pub.isSubscribed(
-            test_datamanager._do_insert_opload, "request_insert_opload"
-        )
+        assert pub.isSubscribed(test_datamanager.do_insert, "request_insert_opload")
 
 
-@pytest.mark.usefixtures("test_datamanager")
+@pytest.mark.usefixtures("test_attributes", "test_datamanager")
 class TestSelectMethods:
     """Class for testing data manager select_all() and select() methods."""
 
     @pytest.mark.unit
-    def test_do_select_all(self, test_datamanager):
-        """do_select_all() should return a Tree() object populated with
-        RAMSTKOpLoad instances on success."""
-        test_datamanager.do_select_all(
-            {"revision_id": 1, "hardware_id": 1, "mode_id": 6, "mechanism_id": 2}
-        )
+    def test_do_select_all(self, test_attributes, test_datamanager):
+        """do_select_all() should return a Tree() object populated with RAMSTKOpLoad
+        instances on success."""
+        test_datamanager.do_select_all(attributes=test_attributes)
         assert isinstance(test_datamanager.tree, Tree)
         assert isinstance(
             test_datamanager.tree.get_node(1).data["opload"], MockRAMSTKOpLoad
         )
 
     @pytest.mark.unit
-    def test_do_select(self, test_datamanager):
-        """do_select() should return an instance of the RAMSTKOpLoad on
-        success."""
-        test_datamanager.do_select_all(
-            {"revision_id": 1, "hardware_id": 1, "mode_id": 6, "mechanism_id": 2}
-        )
+    def test_do_select(self, test_attributes, test_datamanager):
+        """do_select() should return an instance of the RAMSTKOpLoad on success."""
+        test_datamanager.do_select_all(attributes=test_attributes)
 
-        _opload = test_datamanager.do_select(2, table="opload")
+        _opload = test_datamanager.do_select(2)
 
         assert isinstance(_opload, MockRAMSTKOpLoad)
         assert _opload.description == "Test Operating Load #2"
         assert _opload.priority_id == 0
 
     @pytest.mark.unit
-    def test_do_select_non_existent_id(self, test_datamanager):
+    def test_do_select_non_existent_id(self, test_attributes, test_datamanager):
         """do_select() should return None when a non-existent opload ID is
         requested."""
-        test_datamanager.do_select_all(
-            {"revision_id": 1, "hardware_id": 1, "mode_id": 6, "mechanism_id": 2}
-        )
+        test_datamanager.do_select_all(attributes=test_attributes)
 
         assert test_datamanager.do_select(100, table="opload") is None
 
 
-@pytest.mark.usefixtures("test_datamanager")
+@pytest.mark.usefixtures("test_attributes", "test_datamanager")
 class TestInsertMethods:
     """Class for testing the data manager insert() method."""
 
     @pytest.mark.unit
-    def test_do_insert_sibling(self, test_datamanager):
-        """_do_insert_opload() should send the success message after
-        successfully inserting an operating load."""
-        test_datamanager.do_select_all(
-            {"revision_id": 1, "hardware_id": 1, "mode_id": 6, "mechanism_id": 2}
-        )
-        test_datamanager._do_insert_opload(2)
+    def test_do_insert_sibling(self, test_attributes, test_datamanager):
+        """_do_insert_opload() should send the success message after successfully
+        inserting an operating load."""
+        test_datamanager.do_select_all(attributes=test_attributes)
+        test_datamanager.do_insert(attributes=test_attributes)
 
         assert isinstance(test_datamanager.tree, Tree)
         assert isinstance(
@@ -170,14 +172,15 @@ class TestInsertMethods:
         )
 
 
-@pytest.mark.usefixtures("test_datamanager")
+@pytest.mark.usefixtures("test_attributes", "test_datamanager")
 class TestDeleteMethods:
     """Class for testing the data manager delete() method."""
 
     @pytest.mark.unit
-    def test_do_delete(self, test_datamanager):
-        """_do_delete() should send the success message with the treelib Tree
-        when successfully deleting a test method."""
+    def test_do_delete(self, test_attributes, test_datamanager):
+        """_do_delete() should send the success message with the treelib Tree when
+        successfully deleting a test method."""
+        test_datamanager.do_select_all(attributes=test_attributes)
         test_datamanager.do_delete(2)
 
         assert test_datamanager.tree.get_node(2) is None
