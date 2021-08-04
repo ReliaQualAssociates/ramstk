721b3a7d1961a7ca551cb09b046b5a02651ff94e
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-04 07:42:28 -0400

    refactor: remove do_select_all() from Hazards

--- src/ramstk/controllers/hazards/datamanager.py
+++ src/ramstk/controllers/hazards/datamanager.py
@@ -27,6 +27,7 @@ class DataManager(RAMSTKDataManager):
     # Define private scalar class attributes.
     _db_id_colname = "fld_hazard_id"
     _db_tablename = "ramstk_hazard_analysis"
+    _select_msg = "selected_function"
     _tag = "hazard"
 
     # Define public dictionary class attributes.
@@ -56,6 +57,7 @@ class DataManager(RAMSTKDataManager):
         # Initialize public list attributes.
 
         # Initialize public scalar attributes.
+        self.pkey = "hazard_id"
 
         # Subscribe to PyPubSub messages.
         pub.subscribe(super().do_get_attributes, "request_get_hazard_attributes")
@@ -63,7 +65,6 @@ class DataManager(RAMSTKDataManager):
         pub.subscribe(super().do_set_attributes, "wvw_editing_hazard")
         pub.subscribe(super().do_update, "request_update_hazard")
 
-        pub.subscribe(self.do_select_all, "selected_function")
         pub.subscribe(self.do_set_all_attributes, "request_set_all_hazard_attributes")
 
     def do_get_new_record(  # pylint: disable=method-hidden
@@ -87,40 +88,6 @@ class DataManager(RAMSTKDataManager):
         _new_record.set_attributes(attributes)
 
         return _new_record
-
-    def do_select_all(self, attributes: Dict[str, Any]) -> None:
-        """Retrieve all the Hazard data from the RAMSTK Program database.
-
-        :param attributes: the attributes for the selected Hazard.
-        :return: None
-        :rtype: None
-        """
-        self._fkey["revision_id"] = attributes["revision_id"]
-        self._fkey["function_id"] = attributes["function_id"]
-
-        for _node in self.tree.children(self.tree.root):
-            self.tree.remove_node(_node.identifier)
-
-        for _hazard in self.dao.do_select_all(
-            RAMSTKHazardAnalysis,
-            key=["revision_id", "function_id"],
-            value=[self._fkey["revision_id"], self._fkey["function_id"]],
-            order=RAMSTKHazardAnalysis.hazard_id,
-        ):
-
-            self.tree.create_node(
-                tag="hazard",
-                identifier=_hazard.hazard_id,
-                parent=self._parent_id,
-                data={"hazard": _hazard},
-            )
-
-        self.last_id = max(self.tree.nodes.keys())
-
-        pub.sendMessage(
-            "succeed_retrieve_hazards",
-            tree=self.tree,
-        )
 
     def do_set_all_attributes(self, attributes: Dict[str, Any]) -> None:
         """Set all the attributes of the selected hazard.
