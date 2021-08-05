6aed8ec3751e610eb696c7e128b2c7ff01108315
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-05 08:49:00 -0400

    refactor: remove do_select_all() from Stakeholder

--- src/ramstk/controllers/stakeholder/datamanager.py
+++ src/ramstk/controllers/stakeholder/datamanager.py
@@ -27,6 +27,7 @@ class DataManager(RAMSTKDataManager):
     # Define private scalar class attributes.
     _db_id_colname = "fld_stakeholder_id"
     _db_tablename = "ramstk_stakeholder"
+    _select_msg = "selected_revision"
     _tag = "stakeholder"
 
     # Define public dictionary class attributes.
@@ -55,14 +56,13 @@ class DataManager(RAMSTKDataManager):
         # Initialize public list attributes.
 
         # Initialize public scalar attributes.
+        self.pkey = "stakeholder_id"
 
         # Subscribe to PyPubSub messages.
         pub.subscribe(super().do_get_attributes, "request_get_stakeholder_attributes")
         pub.subscribe(super().do_set_attributes, "request_set_stakeholder_attributes")
         pub.subscribe(super().do_set_attributes, "lvw_editing_stakeholder")
         pub.subscribe(super().do_update, "request_update_stakeholder")
-
-        pub.subscribe(self.do_select_all, "selected_revision")
 
     def do_get_new_record(  # pylint: disable=method-hidden
         self, attributes: Dict[str, Any]
@@ -80,42 +80,8 @@ class DataManager(RAMSTKDataManager):
 
         for _key in self._fkey.items():
             attributes.pop(_key[0])
-        attributes.pop(self._db_id_colname.replace("fld_", ""))
+        attributes.pop(self.pkey)
 
         _new_record.set_attributes(attributes)
 
         return _new_record
-
-    def do_select_all(self, attributes: Dict[str, Any]) -> None:
-        """Retrieve all the Stakeholder data from the RAMSTK Program database.
-
-        :param attributes: the attributes dict for the selected Revision.
-        :return: None
-        :rtype: None
-        """
-        self._fkey["revision_id"] = attributes["revision_id"]
-
-        for _node in self.tree.children(self.tree.root):
-            self.tree.remove_node(_node.identifier)
-
-        for _stakeholder in self.dao.do_select_all(
-            RAMSTKStakeholder,
-            key=["revision_id"],
-            value=[self._fkey["revision_id"]],
-            order=RAMSTKStakeholder.stakeholder_id,
-        ):
-            _data_package = {"stakeholder": _stakeholder}
-
-            self.tree.create_node(
-                tag="stakeholder",
-                identifier=_stakeholder.stakeholder_id,
-                parent=self._parent_id,
-                data=_data_package,
-            )
-
-        self.last_id = max(self.tree.nodes.keys())
-
-        pub.sendMessage(
-            "succeed_retrieve_stakeholders",
-            tree=self.tree,
-        )
