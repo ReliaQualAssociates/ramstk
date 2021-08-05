038179620d44dc2d51095e89fa839e8d316c36c8
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-05 19:39:20 -0400

    refactor: use list of ID columns for Cause

--- src/ramstk/controllers/cause/datamanager.py
+++ src/ramstk/controllers/cause/datamanager.py
@@ -27,7 +27,7 @@ class DataManager(RAMSTKDataManager):
     # Define private scalar class attributes.
     _db_id_colname = "fld_cause_id"
     _db_tablename = "ramstk_cause"
-    _select_msg = "selected_mechanism"
+    _select_msg = "selected_revision"
     _tag = "cause"
 
     # Define public dictionary class attributes.
@@ -41,12 +41,6 @@ class DataManager(RAMSTKDataManager):
         super().__init__(**kwargs)
 
         # Initialize private dictionary attributes.
-        self._fkey = {
-            "revision_id": 0,
-            "hardware_id": 0,
-            "mode_id": 0,
-            "mechanism_id": 0,
-        }
         self._pkey = {
             "cause": [
                 "revision_id",
@@ -58,6 +52,13 @@ class DataManager(RAMSTKDataManager):
         }
 
         # Initialize private list attributes.
+        self._lst_id_columns = [
+            "revision_id",
+            "hardware_id",
+            "mode_id",
+            "mechanism_id",
+            "cause_id",
+        ]
 
         # Initialize private scalar attributes.
         self._record: Type[RAMSTKCause] = RAMSTKCause
@@ -85,17 +86,11 @@ class DataManager(RAMSTKDataManager):
         :rtype: None
         """
         _new_record = self._record()
-        _new_record.revision_id = self._fkey["revision_id"]
+        _new_record.revision_id = attributes["revision_id"]
         _new_record.hardware_id = attributes["hardware_id"]
         _new_record.mode_id = attributes["mode_id"]
         _new_record.mechanism_id = attributes["mechanism_id"]
         _new_record.cause_id = self.last_id + 1
         _new_record.parent_id = attributes["mechanism_id"]
-
-        for _key in self._fkey.items():
-            attributes.pop(_key[0])
-        attributes.pop(self.pkey)
-
-        _new_record.set_attributes(attributes)
 
         return _new_record
