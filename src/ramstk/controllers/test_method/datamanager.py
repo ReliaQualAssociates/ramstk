877f8afb79771e8609b5fab26e4b2ed8eba6cb70
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-05 22:54:40 -0400

    refactor: use list of ID columns for Test Method

--- src/ramstk/controllers/test_method/datamanager.py
+++ src/ramstk/controllers/test_method/datamanager.py
@@ -27,7 +27,7 @@ class DataManager(RAMSTKDataManager):
     # Define private scalar class attributes.
     _db_id_colname = "fld_test_id"
     _db_tablename = "ramstk_test_method"
-    _select_msg = "selected_load"
+    _select_msg = "selected_revision"
     _tag = "test_method"
 
     # Define public dictionary class attributes.
@@ -41,13 +41,6 @@ class DataManager(RAMSTKDataManager):
         super().__init__(**kwargs)
 
         # Initialize private dictionary attributes.
-        self._fkey = {
-            "revision_id": 0,
-            "hardware_id": 0,
-            "mode_id": 0,
-            "mechanism_id": 0,
-            "load_id": 0,
-        }
         self._pkey = {
             "test_method": [
                 "revision_id",
@@ -60,6 +53,14 @@ class DataManager(RAMSTKDataManager):
         }
 
         # Initialize private list attributes.
+        self._lst_id_columns = [
+            "revision_id",
+            "hardware_id",
+            "mode_id",
+            "mechanism_id",
+            "load_id",
+            "test_id",
+        ]
 
         # Initialize private scalar attributes.
         self._record: Type[RAMSTKTestMethod] = RAMSTKTestMethod
@@ -87,17 +88,11 @@ class DataManager(RAMSTKDataManager):
         :rtype: None
         """
         _new_record = self._record()
-        _new_record.revision_id = self._fkey["revision_id"]
-        _new_record.hardware_id = self._fkey["hardware_id"]
-        _new_record.mode_id = self._fkey["mode_id"]
-        _new_record.mechanism_id = self._fkey["mechanism_id"]
-        _new_record.load_id = self._fkey["load_id"]
+        _new_record.revision_id = attributes["revision_id"]
+        _new_record.hardware_id = attributes["hardware_id"]
+        _new_record.mode_id = attributes["mode_id"]
+        _new_record.mechanism_id = attributes["mechanism_id"]
+        _new_record.load_id = attributes["load_id"]
         _new_record.test_id = self.last_id + 1
-
-        for _key in self._fkey.items():
-            attributes.pop(_key[0])
-        attributes.pop(self.pkey)
-
-        _new_record.set_attributes(attributes)
 
         return _new_record
--- tests/controllers/test_method/method_unit_test.py
+++ tests/controllers/test_method/method_unit_test.py
@@ -82,7 +82,7 @@ def test_datamanager(mock_program_dao):
     pub.unsubscribe(dut.do_set_attributes, "request_set_test_method_attributes")
     pub.unsubscribe(dut.do_set_attributes, "wvw_editing_test_method")
     pub.unsubscribe(dut.do_update, "request_update_test_method")
-    pub.unsubscribe(dut.do_select_all, "selected_load")
+    pub.unsubscribe(dut.do_select_all, "selected_revision")
     pub.unsubscribe(dut.do_get_tree, "request_get_test_method_tree")
     pub.unsubscribe(dut.do_delete, "request_delete_test_method")
     pub.unsubscribe(dut.do_insert, "request_insert_test_method")
@@ -107,7 +107,7 @@ class TestCreateControllers:
         assert test_datamanager._revision_id == 0
         assert test_datamanager._parent_id == 0
         assert test_datamanager.last_id == 0
-        assert pub.isSubscribed(test_datamanager.do_select_all, "selected_load")
+        assert pub.isSubscribed(test_datamanager.do_select_all, "selected_revision")
         assert pub.isSubscribed(
             test_datamanager.do_get_attributes, "request_get_test_method_attributes"
         )
