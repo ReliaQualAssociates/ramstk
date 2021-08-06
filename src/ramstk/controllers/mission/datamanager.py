b7a9e0e2bfea2111a4f33796d37eacfbc7510476
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-05 22:03:21 -0400

    refactor: use list of ID columns for Mission

--- src/ramstk/controllers/mission/datamanager.py
+++ src/ramstk/controllers/mission/datamanager.py
@@ -41,14 +41,15 @@ class DataManager(RAMSTKDataManager):
         super().__init__(**kwargs)
 
         # Initialize private dictionary attributes.
-        self._fkey = {
-            "revision_id": 0,
-        }
         self._pkey = {
             "mission": ["revision_id", "mission_id"],
         }
 
         # Initialize private list attributes.
+        self._lst_id_columns = [
+            "revision_id",
+            "mission_id",
+        ]
 
         # Initialize private scalar attributes.
         self._record: Type[RAMSTKMission] = RAMSTKMission
@@ -76,13 +77,7 @@ class DataManager(RAMSTKDataManager):
         :rtype: None
         """
         _new_record = self._record()
-        _new_record.revision_id = self._fkey["revision_id"]
+        _new_record.revision_id = attributes["revision_id"]
         _new_record.mission_id = self.last_id + 1
-
-        for _key in self._fkey.items():
-            attributes.pop(_key[0])
-        attributes.pop(self.pkey)
-
-        _new_record.set_attributes(attributes)
 
         return _new_record
