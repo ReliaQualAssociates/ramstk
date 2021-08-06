7510e1f90b1afe7ee776fceef493907bbcd5cc92
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-06 00:35:24 -0400

    refactor: remove pubsub subscribers from Function

--- src/ramstk/controllers/function/datamanager.py
+++ src/ramstk/controllers/function/datamanager.py
@@ -9,9 +9,6 @@
 # Standard Library Imports
 from typing import Any, Dict, Type
 
-# Third Party Imports
-from pubsub import pub
-
 # RAMSTK Package Imports
 from ramstk.controllers import RAMSTKDataManager
 from ramstk.models.programdb import RAMSTKFunction
@@ -41,7 +38,6 @@ class DataManager(RAMSTKDataManager):
         super().__init__(**kwargs)
 
         # Initialize private dictionary attributes.
-        self._pkey = {"function": ["revision_id", "function_id"]}
 
         # Initialize private list attributes.
         self._lst_id_columns = [
@@ -60,10 +56,6 @@ class DataManager(RAMSTKDataManager):
         self.pkey = "function_id"
 
         # Subscribe to PyPubSub messages.
-        pub.subscribe(super().do_get_attributes, "request_get_function_attributes")
-        pub.subscribe(super().do_set_attributes, "request_set_function_attributes")
-        pub.subscribe(super().do_set_attributes, "wvw_editing_function")
-        pub.subscribe(super().do_update, "request_update_function")
 
     def do_get_new_record(  # pylint: disable=method-hidden
         self, attributes: Dict[str, Any]
