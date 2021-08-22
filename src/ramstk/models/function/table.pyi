648b387c357fad71b53bfdc234b42dfd75a211b4
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-22 11:38:07 -0400

    refactor: move Function datamanager to table.py

---
+++ src/ramstk/models/function/table.pyi
@@ -0,0 +1,18 @@
+# Standard Library Imports
+from typing import Any, Dict
+
+# RAMSTK Package Imports
+from ramstk.models import RAMSTKBaseTable as RAMSTKBaseTable
+from ramstk.models import RAMSTKFunctionRecord as RAMSTKFunctionRecord
+
+class RAMSTKFunctionTable(RAMSTKBaseTable):
+    _db_id_colname: str
+    _db_tablename: str
+    _select_msg: str
+    _tag: str
+    _lst_id_columns: Any
+    _record: Any
+    pkey: str
+    def __init__(self, **kwargs: Dict[Any, Any]) -> None: ...
+    _parent_id: Any
+    def do_get_new_record(self, attributes: Dict[str, Any]) -> object: ...
