9f2ccce35e6d6bc7d23dc31727d3acda5366e76f
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-24 07:22:18 -0400

    refactor: move Mechanism datamanager to table.py

--- src/ramstk/controllers/mechanism/__init__.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=unused-import
-# -*- coding: utf-8 -*-
-#
-#       ramstk.models.mechanism.__init__.py is part of The RAMSTK Project
-#
-# All rights reserved.
-# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""The RAMSTK failure Mechanism controllers package."""
