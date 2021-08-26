d9825da4324f93ed71ce29e12bc8260c7b4e0090
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-26 10:46:29 -0400

    refactor: move OpLoad datamanager to table.py

--- src/ramstk/controllers/opload/__init__.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=unused-import
-# -*- coding: utf-8 -*-
-#
-#       ramstk.models.opload.__init__.py is part of The RAMSTK Project
-#
-# All rights reserved.
-# Copyright 2007 - 2021 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""The Operating Load (OpLoad) controllers package."""
