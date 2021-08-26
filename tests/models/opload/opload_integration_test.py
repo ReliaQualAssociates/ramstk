bd1d323d96d5af4eb9abab4a75a92e236a4f07ba
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-08-26 10:47:27 -0400

    test: update OpLoad test suite for record.py and table.py

--- tests/models/opload/opload_integration_test.py
+++ tests/models/opload/opload_integration_test.py
@@ -2,8 +2,7 @@
 # type: ignore
 # -*- coding: utf-8 -*-
 #
-#       tests.controllers.opload.opload_integration_test.py is part of The RAMSTK
-#       Project
+#       tests.models.opload.opload_integration_test.py is part of The RAMSTK Project
 #
 # All rights reserved.
 # Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
@@ -15,26 +14,14 @@ from pubsub import pub
 from treelib import Tree
 
 # RAMSTK Package Imports
-from ramstk.controllers import dmOpLoad
-from ramstk.models.programdb import RAMSTKOpLoad
-
-
-@pytest.fixture(scope="function")
-def test_attributes():
-    yield {
-        "revision_id": 1,
-        "hardware_id": 1,
-        "mode_id": 6,
-        "mechanism_id": 3,
-        "load_id": 3,
-    }
+from ramstk.models import RAMSTKOpLoadRecord, RAMSTKOpLoadTable
 
 
 @pytest.fixture(scope="class")
-def test_datamanager(test_program_dao):
+def test_tablemodel(test_program_dao):
     """Get a data manager instance for each test class."""
     # Create the device under test (dut) and connect to the database.
-    dut = dmOpLoad()
+    dut = RAMSTKOpLoadTable()
     dut.do_connect(test_program_dao)
     dut.do_select_all(
         attributes={
@@ -61,19 +48,19 @@ def test_datamanager(test_program_dao):
     del dut
 
 
-@pytest.mark.usefixtures("test_attributes", "test_datamanager")
+@pytest.mark.usefixtures("test_attributes", "test_tablemodel")
 class TestSelectMethods:
     """Class for testing data manager select_all() and select() methods."""
 
     def on_succeed_select_all(self, tree):
         assert isinstance(tree, Tree)
-        assert isinstance(tree.get_node(1).data["opload"], RAMSTKOpLoad)
+        assert isinstance(tree.get_node(1).data["opload"], RAMSTKOpLoadRecord)
         print("\033[36m\nsucceed_retrieve_opload topic was broadcast.")
 
     @pytest.mark.integration
-    def test_do_select_all_populated_tree(self, test_attributes, test_datamanager):
-        """do_select_all() should return a Tree() object populated with RAMSTKOpLoad
-        instances on success."""
+    def test_do_select_all_populated_tree(self, test_attributes, test_tablemodel):
+        """do_select_all() should return a Tree() object populated with
+        RAMSTKOpLoadRecord instances on success."""
         pub.subscribe(self.on_succeed_select_all, "succeed_retrieve_opload")
 
         pub.sendMessage("selected_revision", attributes=test_attributes)
@@ -81,14 +68,14 @@ class TestSelectMethods:
         pub.unsubscribe(self.on_succeed_select_all, "succeed_retrieve_opload")
 
 
-@pytest.mark.usefixtures("test_attributes", "test_datamanager")
+@pytest.mark.usefixtures("test_attributes", "test_tablemodel")
 class TestInsertMethods:
     """Class for testing the data manager insert() method."""
 
     def on_succeed_insert_sibling(self, node_id, tree):
         assert node_id == 5
         assert isinstance(tree, Tree)
-        assert isinstance(tree.get_node(5).data["opload"], RAMSTKOpLoad)
+        assert isinstance(tree.get_node(5).data["opload"], RAMSTKOpLoadRecord)
         print("\033[36m\nsucceed_insert_opload topic was broadcast.")
 
     def on_fail_insert_no_parent(self, error_message):
@@ -100,39 +87,39 @@ class TestInsertMethods:
         print("\033[35m\nfail_insert_opload topic was broadcast.")
 
     @pytest.mark.integration
-    def test_do_insert_sibling(self, test_attributes, test_datamanager):
+    def test_do_insert_sibling(self, test_attributes, test_tablemodel):
         """do_insert() should send the success message with the ID of the newly
         inserted node and the data manager's tree after successfully inserting a new
         opload."""
         pub.subscribe(self.on_succeed_insert_sibling, "succeed_insert_opload")
 
-        assert test_datamanager.tree.get_node(5) is None
+        assert test_tablemodel.tree.get_node(5) is None
 
         pub.sendMessage("request_insert_opload", attributes=test_attributes)
 
         assert isinstance(
-            test_datamanager.tree.get_node(5).data["opload"], RAMSTKOpLoad
+            test_tablemodel.tree.get_node(5).data["opload"], RAMSTKOpLoadRecord
         )
 
         pub.unsubscribe(self.on_succeed_insert_sibling, "succeed_insert_opload")
 
     @pytest.mark.integration
-    def test_do_insert_no_parent(self, test_attributes, test_datamanager):
+    def test_do_insert_no_parent(self, test_attributes, test_tablemodel):
         """_do_insert_opload() should send the fail message if attempting to add an
         operating load to a non-existent opload ID."""
         pub.subscribe(self.on_fail_insert_no_parent, "fail_insert_opload")
 
-        assert test_datamanager.tree.get_node(6) is None
+        assert test_tablemodel.tree.get_node(6) is None
 
         test_attributes["mechanism_id"] = 30
         pub.sendMessage("request_insert_opload", attributes=test_attributes)
 
-        assert test_datamanager.tree.get_node(6) is None
+        assert test_tablemodel.tree.get_node(6) is None
 
         pub.unsubscribe(self.on_fail_insert_no_parent, "fail_insert_opload")
 
 
-@pytest.mark.usefixtures("test_datamanager")
+@pytest.mark.usefixtures("test_tablemodel")
 class TestDeleteMethods:
     """Class for testing the data manager delete() method."""
 
@@ -152,7 +139,7 @@ class TestDeleteMethods:
         print("\033[35m\nfail_delete_opload topic was broadcast.")
 
     @pytest.mark.integration
-    def test_do_delete(self, test_datamanager):
+    def test_do_delete(self, test_tablemodel):
         """_do_delete() should send the success message with the treelib Tree when
         successfully deleting a test method."""
         pub.subscribe(self.on_succeed_delete, "succeed_delete_opload")
@@ -162,7 +149,7 @@ class TestDeleteMethods:
         pub.unsubscribe(self.on_succeed_delete, "succeed_delete_opload")
 
     @pytest.mark.integration
-    def test_do_delete_non_existent_id(self, test_datamanager):
+    def test_do_delete_non_existent_id(self, test_tablemodel):
         """_do_delete() should send the fail message when attempting to delete a node
         ID that doesn't exist in the tree."""
         pub.subscribe(self.on_fail_delete_non_existent_id, "fail_delete_opload")
@@ -172,18 +159,18 @@ class TestDeleteMethods:
         pub.unsubscribe(self.on_fail_delete_non_existent_id, "fail_delete_opload")
 
     @pytest.mark.integration
-    def test_do_delete_not_in_tree(self, test_datamanager):
+    def test_do_delete_not_in_tree(self, test_tablemodel):
         """_do_delete() should send the fail message when attempting to remove a node
         that doesn't exist from the tree even if it exists in the database."""
         pub.subscribe(self.on_fail_delete_not_in_tree, "fail_delete_opload")
 
-        test_datamanager.tree.remove_node(4)
+        test_tablemodel.tree.remove_node(4)
         pub.sendMessage("request_delete_opload", node_id=4)
 
         pub.unsubscribe(self.on_fail_delete_not_in_tree, "fail_delete_opload")
 
 
-@pytest.mark.usefixtures("test_datamanager")
+@pytest.mark.usefixtures("test_tablemodel")
 class TestUpdateMethods:
     """Class for testing update() and update_all() methods."""
 
@@ -218,27 +205,27 @@ class TestUpdateMethods:
         print("\033[35m\nfail_update_opload topic was broadcast")
 
     @pytest.mark.integration
-    def test_do_update(self, test_datamanager):
+    def test_do_update(self, test_tablemodel):
         """do_update() should return a zero error code on success."""
         pub.subscribe(self.on_succeed_update, "succeed_update_opload")
 
-        test_datamanager.tree.get_node(4).data[
+        test_tablemodel.tree.get_node(4).data[
             "opload"
         ].description = "Test failure opload"
-        test_datamanager.tree.get_node(4).data["opload"].priority_id = 4
+        test_tablemodel.tree.get_node(4).data["opload"].priority_id = 4
 
         pub.sendMessage("request_update_opload", node_id=4, table="opload")
 
         assert (
-            test_datamanager.tree.get_node(4).data["opload"].description
+            test_tablemodel.tree.get_node(4).data["opload"].description
             == "Test failure opload"
         )
-        assert test_datamanager.tree.get_node(4).data["opload"].priority_id == 4
+        assert test_tablemodel.tree.get_node(4).data["opload"].priority_id == 4
 
         pub.unsubscribe(self.on_succeed_update, "succeed_update_opload")
 
     @pytest.mark.integration
-    def test_do_update_all(self, test_datamanager):
+    def test_do_update_all(self, test_tablemodel):
         """do_update_all() should broadcast the succeed message on success."""
         pub.subscribe(self.on_succeed_update_all, "succeed_update_all")
 
@@ -247,12 +234,12 @@ class TestUpdateMethods:
         pub.unsubscribe(self.on_succeed_update_all, "succeed_update_all")
 
     @pytest.mark.integration
-    def test_do_update_wrong_data_type(self, test_datamanager):
+    def test_do_update_wrong_data_type(self, test_tablemodel):
         """do_update() should return a non-zero error code when passed a Requirement ID
         that doesn't exist."""
         pub.subscribe(self.on_fail_update_wrong_data_type, "fail_update_opload")
 
-        _opload = test_datamanager.do_select(4)
+        _opload = test_tablemodel.do_select(4)
         _opload.priority_id = {1: 2}
 
         pub.sendMessage("request_update_opload", node_id=4, table="opload")
@@ -260,14 +247,14 @@ class TestUpdateMethods:
         pub.unsubscribe(self.on_fail_update_wrong_data_type, "fail_update_opload")
 
     @pytest.mark.integration
-    def test_do_update_root_node_wrong_data_type(self, test_datamanager):
+    def test_do_update_root_node_wrong_data_type(self, test_tablemodel):
         """do_update() should return a non-zero error code when passed a Requirement ID
         that doesn't exist."""
         pub.subscribe(
             self.on_fail_update_root_node_wrong_data_type, "fail_update_opload"
         )
 
-        _opload = test_datamanager.do_select(4)
+        _opload = test_tablemodel.do_select(4)
         _opload.priority_id = {1: 2}
 
         pub.sendMessage("request_update_opload", node_id=0, table="opload")
@@ -277,7 +264,7 @@ class TestUpdateMethods:
         )
 
     @pytest.mark.integration
-    def test_do_update_non_existent_id(self, test_datamanager):
+    def test_do_update_non_existent_id(self, test_tablemodel):
         """do_update() should return a non-zero error code when passed an OpLoad ID
         that doesn't exist."""
         pub.subscribe(self.on_fail_update_non_existent_id, "fail_update_opload")
@@ -287,18 +274,18 @@ class TestUpdateMethods:
         pub.unsubscribe(self.on_fail_update_non_existent_id, "fail_update_opload")
 
     @pytest.mark.integration
-    def test_do_update_no_data_package(self, test_datamanager):
+    def test_do_update_no_data_package(self, test_tablemodel):
         """do_update() should return a non-zero error code when passed a FMEA ID that
         has no data package."""
         pub.subscribe(self.on_fail_update_no_data_package, "fail_update_opload")
 
-        test_datamanager.tree.get_node(4).data.pop("opload")
+        test_tablemodel.tree.get_node(4).data.pop("opload")
         pub.sendMessage("request_update_opload", node_id=4, table="opload")
 
         pub.unsubscribe(self.on_fail_update_no_data_package, "fail_update_opload")
 
 
-@pytest.mark.usefixtures("test_datamanager")
+@pytest.mark.usefixtures("test_tablemodel")
 class TestGetterSetter:
     """Class for testing methods that get or set."""
 
@@ -310,7 +297,7 @@ class TestGetterSetter:
 
     def on_succeed_get_data_manager_tree(self, tree):
         assert isinstance(tree, Tree)
-        assert isinstance(tree.get_node(4).data["opload"], RAMSTKOpLoad)
+        assert isinstance(tree.get_node(4).data["opload"], RAMSTKOpLoadRecord)
         print("\033[36m\nsucceed_get_opload_tree topic was broadcast")
 
     def on_succeed_set_attributes(self, tree):
@@ -319,7 +306,7 @@ class TestGetterSetter:
         print("\033[36m\nsucceed_get_opload_tree topic was broadcast")
 
     @pytest.mark.integration
-    def test_do_get_attributes(self, test_datamanager):
+    def test_do_get_attributes(self, test_tablemodel):
         """do_get_attributes() should return a dict of mode attributes on success."""
         pub.subscribe(self.on_succeed_get_attributes, "succeed_get_mode_attributes")
 
@@ -328,7 +315,7 @@ class TestGetterSetter:
         pub.unsubscribe(self.on_succeed_get_attributes, "succeed_get_mode_attributes")
 
     @pytest.mark.integration
-    def test_on_get_tree_data_manager(self, test_datamanager):
+    def test_on_get_tree_data_manager(self, test_tablemodel):
         """on_get_tree() should return the PoF treelib Tree."""
         pub.subscribe(self.on_succeed_get_data_manager_tree, "succeed_get_opload_tree")
 
@@ -339,7 +326,7 @@ class TestGetterSetter:
         )
 
     @pytest.mark.integration
-    def test_do_set_attributes(self, test_datamanager):
+    def test_do_set_attributes(self, test_tablemodel):
         """do_set_attributes() should return None when successfully setting operating
         load attributes."""
         pub.subscribe(self.on_succeed_get_data_manager_tree, "succeed_get_opload_tree")
