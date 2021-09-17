06ecde5502e02b1551962d4ae4a5088dafc0474b
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-09-17 12:17:11 -0400

    refactor: move Hardware panel methods to RAMSTKPanel (#774)
    * refactor: move Hardware panels to panel.py
    * refactor: move Hardware views to view.py
    * refactor: move Design Electric panels to panel.py
    * refactor: move MILHDBK217 panels to panel.py
    * refactor: move component panels to Design Electric/MIL-HDBK-217 packages
    * refactor: move Reliability panels to panel.py
    * refactor: move stress input panel to panel.py
    * refactor: move IC Design Electric panel to panels.py
    * refactor: move Capacitor Design Electric panel to panel.py
    * refactor: move Connection Design Electric panel to panel.py
    * refactor: move Inductor Design Electric panel to panel.py
    * refactor: move Meter Design Electric panel to panel.py
    * refactor: move Miscellaneous Design Electric panel to panel.py
    * refactor: move Relay input panel to panel.py
    * refactor: move Resistor input panel to panel.py
    * refactor: move Semiconductor input panel to panel.py
    * refactor: move Switch input panel to panel.py
    * refactor: remove old Design Electric panels.py
    * refactor: move Reliability results panel to panel.py
    * refactor: move Availability results panel to panel.py
    * refactor: move Stress results panel to panel.py
    * refactor: move Capacitor results panel to panel.py
    * refactor: move Connection results panel to panel.py
    * refactor: move Inductor results panel to panel.py
    * refactor: move Integrated Circuit results panel to panel.py
    * refactor: move Meter results panel to panel.py
    * refactor: move Miscellaneous results panel to panel.py
    * refactor: move Relay results panel to panel.py
    * refactor: move Resistor results panel to panel.py
    * refactor: move Semiconductor results panel to panel.py
    * refactor: move Switch results panel to panel.py

---
+++ src/ramstk/views/gtk3/design_electric/components/capacitor.pyi
@@ -0,0 +1,37 @@
+# Standard Library Imports
+from typing import Any, Dict, List
+
+# RAMSTK Package Imports
+from ramstk.views.gtk3 import _ as _
+from ramstk.views.gtk3.widgets import RAMSTKComboBox as RAMSTKComboBox
+from ramstk.views.gtk3.widgets import RAMSTKEntry as RAMSTKEntry
+from ramstk.views.gtk3.widgets import RAMSTKFixedPanel as RAMSTKFixedPanel
+
+class CapacitorDesignElectricInputPanel(RAMSTKFixedPanel):
+    _dic_quality: Dict[int, List[Any]]
+    _dic_specifications: Dict[int, List[Any]]
+    _dic_styles: Dict[int, List[Any]]
+    _record_field: str
+    _select_msg: str
+    _tag: str
+    _title: str
+    cmbConfiguration: RAMSTKComboBox
+    cmbConstruction: RAMSTKComboBox
+    cmbQuality: RAMSTKComboBox
+    cmbSpecification: RAMSTKComboBox
+    cmbStyle: RAMSTKComboBox
+    txtCapacitance: RAMSTKEntry
+    txtESR: RAMSTKEntry
+    _hazard_rate_method_id: int
+    _quality_id: int
+    dic_attribute_index_map: Dict[int, List[str]]
+    dic_attribute_widget_map: Dict[str, List[Any]]
+    category_id: int
+    subcategory_id: int
+    def __init__(self) -> None: ...
+    def do_load_comboboxes(self, subcategory_id: int) -> None: ...
+    def _do_set_reliability_attributes(self, attributes: Dict[str, Any]) -> None: ...
+    def _do_load_styles(self, combo: RAMSTKComboBox) -> None: ...
+    def _do_set_sensitive(self, attributes: Dict[str, Any]) -> None: ...
+    def __do_set_parts_count_sensitive(self, attributes: Dict[str, Any]) -> None: ...
+    def __do_set_part_stress_sensitive(self, attributes: Dict[str, Any]) -> None: ...
---
+++ src/ramstk/views/gtk3/milhdbk217f/components/inductor.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+#
+#       ramstk.views.gtk3.hardware.components.inductor.py is part of the RAMSTK
+#       Project.
+#
+# All rights reserved.
+# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
+"""Inductor Results Panel."""
+
+# Standard Library Imports
+from typing import Any, Dict, List
+
+# RAMSTK Package Imports
+from ramstk.views.gtk3 import _
+from ramstk.views.gtk3.milhdbk217f import MilHdbk217FResultPanel
+from ramstk.views.gtk3.widgets import RAMSTKEntry
+
+
+class InductorMilHdbk217FResultPanel(MilHdbk217FResultPanel):
+    """Displays Inductor assessment results attribute data.
+
+    The Inductor assessment result view displays all the assessment results
+    for the selected inductor.  This includes, currently, results for
+    MIL-HDBK-217FN2 parts count and part stress methods.  The attributes of an
+    Inductor assessment result view are:
+
+    :cvar dict _dic_part_stress: dictionary of MIL-HDBK-217F part stress
+        models.  The key is the subcategory ID attribute of the component.
+
+    :ivar list _lst_labels: list of label text to display for the capacitor
+        MIL-HDBK-217 input parameters.
+
+    :ivar self.txtLambdaB: displays the base hazard rate for the Hardware
+        item.
+    :ivar txtPiC: displays the construction factor for the Hardware item.
+    """
+
+    # Define private dict class attributes.
+    _dic_part_stress: Dict[int, str] = {
+        1: '<span foreground="blue">\u03BB<sub>p</sub> = '
+        "\u03BB<sub>b</sub>\u03C0<sub>C</sub>\u03C0<sub>Q</sub>\u03C0"
+        "<sub>E</sub></span>",
+        2: '<span foreground="blue">\u03BB<sub>p</sub> = '
+        "\u03BB<sub>b</sub>\u03C0<sub>Q</sub>\u03C0<sub>E</sub></span>",
+    }
+
+    # Define private list attributes.
+
+    # Define private scalar class attributes.
+    _title: str = _("Inductive Device MIL-HDBK-217F Results")
+
+    # Define public dictionary class attributes.
+
+    # Define public list class attributes.
+
+    # Define public scalar class attributes.
+
+    def __init__(self) -> None:
+        """Initialize an instance of the Inductor assessment result view."""
+        super().__init__()
+
+        # Initialize widgets.
+        self.txtPiC: RAMSTKEntry = RAMSTKEntry()
+
+        # Initialize private dict attributes.
+
+        # Initialize private list attributes.
+
+        # Initialize private scalar attributes.
+
+        # Initialize public dict attributes.
+        self.dic_attribute_widget_map: Dict[str, List[Any]] = {
+            "hazard_rate_model": [
+                13,
+                self.lblModel,
+                "",
+                None,
+                "",
+                "",
+                {
+                    "tooltip": _(
+                        "The assessment model used to calculate the inductive device's "
+                        "failure rate."
+                    ),
+                },
+                "",
+            ],
+            "lambda_b": [
+                23,
+                self.txtLambdaB,
+                "",
+                None,
+                "",
+                0.0,
+                {
+                    "tooltip": _("The base hazard rate for the inductive device."),
+                },
+                "\u03BB<sub>b</sub>:",
+            ],
+            "pi_q": [
+                30,
+                self.txtPiQ,
+                "",
+                None,
+                "",
+                1.0,
+                {
+                    "tooltip": _("The quality factor for the inductive device."),
+                },
+                "\u03C0<sub>Q</sub>:",
+            ],
+            "pi_e": [
+                19,
+                self.txtPiE,
+                "",
+                None,
+                "",
+                1.0,
+                {
+                    "tooltip": _("The environment factor for the inductive device."),
+                },
+                "\u03C0<sub>E</sub>:",
+            ],
+            "pi_c": [
+                13,
+                self.txtPiC,
+                "",
+                None,
+                "",
+                1.0,
+                {
+                    "tooltip": _("The construction factor for the inductive device."),
+                },
+                "\u03C0<sub>C</sub>:",
+            ],
+        }
+
+        # Initialize public list attributes.
+
+        # Initialize public scalar attributes.
+
+        super().do_set_properties()
+        super().do_make_panel()
+
+        # Subscribe to PyPubSub messages.
+
+    def _do_load_entries(self, attributes: Dict[str, Any]) -> None:
+        """Set widget sensitivity as needed for the selected capacitor.
+
+        :return: None
+        :rtype: None
+        """
+        super().do_load_entries(attributes)
+
+        self.txtPiC.set_sensitive(False)
+
+        # MIL-HDBK-217F, Parts Stress
+        if self.category_id == 5 and self._hazard_rate_method_id == 2:
+            self.lblModel.do_update(self._dic_part_stress[self.subcategory_id])
+
+            self.txtPiC.do_update(
+                str(self.fmt.format(attributes["piC"])),
+                signal="changed",
+            )
