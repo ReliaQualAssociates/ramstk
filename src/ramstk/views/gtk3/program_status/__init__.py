82a3206c9838a00b3804f3759972909902a5d1a6
Doyle Rowland <doyle.rowland@reliaqual.com>
2021-10-05 09:08:47 -0400

    refactor: move Program Status panel methods to panel.py

---
+++ src/ramstk/views/gtk3/program_status/__init__.py
@@ -0,0 +1,12 @@
+# pylint: disable=unused-import
+# -*- coding: utf-8 -*-
+#
+#       ramstk.views.gtk3.program_status.__init__.py is part of The RAMSTK Project
+#
+# All rights reserved.
+# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
+"""GTK3 Program Status Views."""
+
+# RAMSTK Local Imports
+from .panel import ProgramStatusPlotPanel
+from .view import ProgramStatusWorkView
