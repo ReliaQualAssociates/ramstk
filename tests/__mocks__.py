1e99ea53fd39202c83401ff04b52cf8208e4efa8
Doyle Rowland <doyle.rowland@reliaqual.com>
2022-01-22 20:35:31 -0500

    fix: PoF changes can now be saved (#882)
    * refactor: update database column types
    
    * fix: PoF changes can be saved to database
    
    * refactor: move duplicate code to baseview

--- tests/__mocks__.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=protected-access, no-self-use, missing-docstring
-# -*- coding: utf-8 -*-
-#
-#       tests.__mocks__.py is part of The RAMSTK Project
-#
-# All rights reserved.
-# Copyright 2020 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""File for organizing mock structures."""
