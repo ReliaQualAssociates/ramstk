86892e652d81776e4386bdf98f1f83c8c26d3da8
Doyle Rowland <doyle.rowland@reliaqual.com>
2022-01-25 21:20:57 -0500

    fix: verification task list now loads

--- src/ramstk/models/programdb/validation/table.py
+++ src/ramstk/models/programdb/validation/table.py
@@ -28,6 +28,7 @@ class RAMSTKValidationTable(RAMSTKBaseTable):
     # Define private scalar class attributes.
     _db_id_colname = "fld_validation_id"
     _db_tablename = "ramstk_validation"
+    _deprecated = False
     _select_msg = "selected_revision"
     _tag = "validation"
 
