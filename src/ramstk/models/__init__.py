0d2e9bacff5198689bd164ddfa6be199d3510145
Doyle Rowland <doyle.rowland@reliaqual.com>
2022-02-22 20:45:11 -0500

    refactor: move ramstk_model to subpackage

--- src/ramstk/models/__init__.py
+++ src/ramstk/models/__init__.py
@@ -26,6 +26,7 @@ from .commondb.load_history.record import RAMSTKLoadHistoryRecord  # isort:skip
 from .commondb.manufacturer.record import RAMSTKManufacturerRecord  # isort:skip
 from .commondb.measurement.record import RAMSTKMeasurementRecord  # isort:skip
 from .commondb.method.record import RAMSTKMethodRecord  # isort:skip
+from .commondb.model.record import RAMSTKModelRecord  # isort:skip
 from .commondb.site_info.record import RAMSTKSiteInfoRecord  # isort:skip
 from .commondb.subcategory.record import RAMSTKSubCategoryRecord  # isort:skip
 from .commondb.category.table import RAMSTKCategoryTable  # isort:skip
@@ -40,6 +41,7 @@ from .commondb.load_history.table import RAMSTKLoadHistoryTable
 from .commondb.manufacturer.table import RAMSTKManufacturerTable
 from .commondb.measurement.table import RAMSTKMeasurementTable
 from .commondb.method.table import RAMSTKMethodTable
+from .commondb.model.table import RAMSTKModelTable
 from .commondb.site_info.table import RAMSTKSiteInfoTable
 from .commondb.subcategory.table import RAMSTKSubCategoryTable
 from .programdb.action.record import RAMSTKActionRecord
