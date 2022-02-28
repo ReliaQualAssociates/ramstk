68aac99c06ad7a72d484c531497fb49bf1b417cc
Doyle Rowland <doyle.rowland@reliaqual.com>
2022-02-28 14:15:07 -0500

    refactor: create models.db package; move common DB to package

---
+++ src/ramstk/models/db/__init__.py
@@ -0,0 +1,11 @@
+# pylint: disable=unused-import
+# -*- coding: utf-8 -*-
+#
+#       ramstk.models.db.__init__.py is part of The RAMSTK Project
+#
+# All rights reserved.
+# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
+"""The RAMSTK database models package."""
+
+# RAMSTK Local Imports
+from .common_database import RAMSTKCommonDB
