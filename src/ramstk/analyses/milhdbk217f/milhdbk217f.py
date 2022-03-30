1c4c18d7366c43cb430bc6abf554d52f836a9c85
Doyle Rowland <doyle.rowland@reliaqual.com>
2022-03-30 09:43:57 -0400

    test: fix test that no longer covered after setting defaults

--- src/ramstk/analyses/milhdbk217f/milhdbk217f.py
+++ src/ramstk/analyses/milhdbk217f/milhdbk217f.py
@@ -57,7 +57,7 @@ def do_predict_active_hazard_rate(**attributes: Dict[str, Any]) -> float:
         pub.sendMessage("succeed_predict_reliability", attributes=attributes)
         pub.sendMessage("request_set_all_milhdbk217f_attributes", attributes=attributes)
         pub.sendMessage("request_set_all_reliability_attributes", attributes=attributes)
-    except ValueError:
+    except (TypeError, ValueError):
         pub.sendMessage(
             "fail_predict_reliability",
             error_message=(
