6becb5ff0d9b3e0c2a62f7a44bba2cb2f3214ff9
Doyle Rowland <doyle.rowland@reliaqual.com>
2022-07-03 09:25:36 -0400

    style: update stub file

--- src/ramstk/views/gtk3/widgets/matrix.pyi
+++ src/ramstk/views/gtk3/widgets/matrix.pyi
@@ -11,7 +11,9 @@ from .combo import RAMSTKComboBox as RAMSTKComboBox
 from .label import RAMSTKLabel as RAMSTKLabel
 
 class RAMSTKMatrixView(Gtk.Grid):
+    column_id_dic: Dict[str, int]
     icons_dic: Dict[str, str]
+    row_id_dic: Dict[str, int]
     n_columns: int
     n_rows: int
     def __init__(self) -> None: ...
@@ -20,9 +22,9 @@ class RAMSTKMatrixView(Gtk.Grid):
     def do_remove_column(self, position_idx: int) -> None: ...
     def do_remove_row(self, position_idx: int) -> None: ...
     def do_set_column_headings(
-        self, column_name_lst: List[Tuple[str, str]]
+        self, column_name_lst: List[Tuple[str, str, int]]
     ) -> None: ...
-    def do_set_row_headings(self, row_name_lst: List[Tuple[str, str]]) -> None: ...
+    def do_set_row_headings(self, row_name_lst: List[Tuple[str, str, int]]) -> None: ...
     def _do_add_widgets(
         self,
         position_tpl: Tuple[int, int],
