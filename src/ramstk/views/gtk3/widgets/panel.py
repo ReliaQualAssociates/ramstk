d4ccf549b8a2098bec6aa88b019f26411178a1c2
Doyle Rowland <doyle.rowland@reliaqual.com>
2023-03-18 11:51:19 -0400

    fix: do update message passes signal instead of signal_str

--- src/ramstk/views/gtk3/widgets/panel.py
+++ src/ramstk/views/gtk3/widgets/panel.py
@@ -196,7 +196,7 @@ class RAMSTKFixedPanel(RAMSTKPanel):
             _idx,  # pylint: disable=unused-variable
             _value_obj,
         ) in self.dic_attribute_widget_map.items():
-            _value_obj[1].do_update(_value_obj[4], signal_str=_value_obj[2])
+            _value_obj[1].do_update(_value_obj[4], signal=_value_obj[2])
 
     def do_load_panel(
         self,
@@ -213,7 +213,7 @@ class RAMSTKFixedPanel(RAMSTKPanel):
             _new_text_str = attributes.get(_key_str, _value_obj[5])
             _value_obj[1].do_update(
                 _new_text_str,
-                signal_str=_value_obj[2],
+                signal=_value_obj[2],
             )
 
         pub.sendMessage("request_set_cursor_active")
