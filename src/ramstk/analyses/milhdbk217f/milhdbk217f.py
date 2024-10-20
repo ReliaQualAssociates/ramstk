1645ade7d56eaa58a6bed501e15abb0d41a130f6
Doyle Rowland <doyle.rowland@reliaqual.com>
2024-10-20 12:03:44 -0400

    refactor: mil-hdbk-217fn2 top level functions

--- src/ramstk/analyses/milhdbk217f/milhdbk217f.py
+++ src/ramstk/analyses/milhdbk217f/milhdbk217f.py
@@ -8,7 +8,7 @@
 """milhdbk217f Calculations Class."""
 
 # Standard Library Imports
-from typing import Any, Dict
+from typing import Any, Callable, Dict, Optional
 
 # Third Party Imports
 from pubsub import pub
@@ -58,34 +58,16 @@ def do_predict_active_hazard_rate(**attributes: Dict[str, Any]) -> float:
         pub.sendMessage("request_set_all_milhdbk217f_attributes", attributes=attributes)
         pub.sendMessage("request_set_all_reliability_attributes", attributes=attributes)
     except (TypeError, ValueError):
-        pub.sendMessage(
-            "fail_predict_reliability",
-            error_message=(
-                f"Failed to predict MIL-HDBK-217F hazard rate for hardware ID "
-                f'{attributes["hardware_id"]}; one or more inputs has a negative or '
-                f"missing value. Hardware item category "
-                f'ID={attributes["category_id"]}, subcategory '
-                f'ID={attributes["subcategory_id"]}, rated '
-                f'power={attributes["power_rated"]}, number of '
-                f'elements={attributes["n_elements"]}.'
-            ),
+        _handle_prediction_failure(
+            "reliability",
+            attributes,
+            "negative or missing values.",
         )
     except ZeroDivisionError:
-        pub.sendMessage(
-            "fail_predict_reliability",
-            error_message=(
-                f"Failed to predict MIL-HDBK-217F hazard rate for hardware ID "
-                f'{attributes["hardware_id"]}; one or more inputs has a value of 0.0.  '
-                f'Hardware item category ID={attributes["category_id"]}, subcategory '
-                f'ID={attributes["subcategory_id"]}, operating ac '
-                f'voltage={attributes["voltage_ac_operating"]}, operating DC '
-                f'voltage={attributes["voltage_dc_operating"]}, operating '
-                f'temperature={attributes["temperature_active"]}, temperature '
-                f'rise={attributes["temperature_rise"]}, rated maximum '
-                f'temperature={attributes["temperature_rated_max"]}, feature '
-                f'size={attributes["feature_size"]}, surface '
-                f'area={attributes["area"]}, and item weight={attributes["weight"]}.'
-            ),
+        _handle_prediction_failure(
+            "reliability",
+            attributes,
+            "input value of 0.0.",
         )
 
     return attributes["hazard_rate_active"]
@@ -130,30 +112,29 @@ def _do_calculate_part_count(**attributes: Dict[str, Any]) -> Dict[str, Any]:
     :rtype: dict :raise: IndexError if there is no entry for the active environment ID.
         :raise: KeyError if there is no entry for category ID or subcategory ID.
     """
-    _part_count = {
-        1: integratedcircuit.calculate_part_count,
-        2: semiconductor.calculate_part_count,
-        3: resistor.calculate_part_count,
-        4: capacitor.calculate_part_count,
-        5: inductor.calculate_part_count,
-        6: relay.calculate_part_count,
-        7: switch.calculate_part_count,
-        8: connection.calculate_part_count,
-        9: meter.calculate_part_count,
-        10: {
-            1: crystal.calculate_part_count,
-            2: efilter.calculate_part_count,
-            3: fuse.calculate_part_count,
-            4: lamp.calculate_part_count,
+    _part_count = _get_function_or_list(
+        {
+            1: integratedcircuit.calculate_part_count,
+            2: semiconductor.calculate_part_count,
+            3: resistor.calculate_part_count,
+            4: capacitor.calculate_part_count,
+            5: inductor.calculate_part_count,
+            6: relay.calculate_part_count,
+            7: switch.calculate_part_count,
+            8: connection.calculate_part_count,
+            9: meter.calculate_part_count,
+            10: {
+                1: crystal.calculate_part_count,
+                2: efilter.calculate_part_count,
+                3: fuse.calculate_part_count,
+                4: lamp.calculate_part_count,
+            },
         },
-    }
+        attributes["category_id"],
+        attributes["subcategory_id"],
+    )
 
-    if attributes["category_id"] == 10:
-        attributes["lambda_b"] = _part_count[attributes["category_id"]][
-            attributes["subcategory_id"]
-        ](**attributes)
-    else:
-        attributes["lambda_b"] = _part_count[attributes["category_id"]](**attributes)
+    attributes["lambda_b"] = _part_count(**attributes)
 
     if attributes["category_id"] != 2:
         attributes["piQ"] = _get_part_count_quality_factor(
@@ -182,23 +163,27 @@ def _do_calculate_part_stress(**attributes: Dict[str, Any]) -> Dict[str, Any]:
     :rtype: dict :raise: IndexError if there is no entry for the active environment ID.
         :raise: KeyError if there is no entry for category ID or subcategory ID.
     """
-    _functions = {
-        1: integratedcircuit.calculate_part_stress,
-        2: semiconductor.calculate_part_stress,
-        3: resistor.calculate_part_stress,
-        4: capacitor.calculate_part_stress,
-        5: inductor.calculate_part_stress,
-        6: relay.calculate_part_stress,
-        7: switch.calculate_part_stress,
-        8: connection.calculate_part_stress,
-        9: meter.calculate_part_stress,
-        10: {
-            1: crystal.calculate_part_stress,
-            2: efilter.calculate_part_stress,
-            3: fuse.calculate_part_stress,
-            4: lamp.calculate_part_stress,
+    _part_stress = _get_function_or_list(
+        {
+            1: integratedcircuit.calculate_part_stress,
+            2: semiconductor.calculate_part_stress,
+            3: resistor.calculate_part_stress,
+            4: capacitor.calculate_part_stress,
+            5: inductor.calculate_part_stress,
+            6: relay.calculate_part_stress,
+            7: switch.calculate_part_stress,
+            8: connection.calculate_part_stress,
+            9: meter.calculate_part_stress,
+            10: {
+                1: crystal.calculate_part_stress,
+                2: efilter.calculate_part_stress,
+                3: fuse.calculate_part_stress,
+                4: lamp.calculate_part_stress,
+            },
         },
-    }
+        attributes["category_id"],
+        attributes["subcategory_id"],
+    )
 
     if attributes["category_id"] != 6:
         attributes["piE"] = _get_environment_factor(
@@ -214,13 +199,6 @@ def _do_calculate_part_stress(**attributes: Dict[str, Any]) -> Dict[str, Any]:
             attributes["subcategory_id"],
             attributes["quality_id"],
         )
-
-    if attributes["category_id"] == 10:
-        _part_stress = _functions[attributes["category_id"]][
-            attributes["subcategory_id"]
-        ]
-    else:
-        _part_stress = _functions[attributes["category_id"]]
 
     return _part_stress(**attributes)
 
@@ -245,26 +223,31 @@ def _get_environment_factor(
         environment ID. :raise: KeyError if there is no piE list for the passed category
         ID (or subcategory ID, quality ID when appllicable).
     """
-    _pi_e_lists = {
-        1: integratedcircuit.PI_E,
-        2: semiconductor.PI_E,
-        3: resistor.PI_E,
-        4: capacitor.PI_E,
-        5: inductor.PI_E,
-        7: switch.PI_E,
-        8: connection.PI_E,
-        9: meter.PI_E,
-        10: {1: crystal.PI_E, 2: efilter.PI_E, 3: fuse.PI_E, 4: lamp.PI_E},
-    }
+    _pi_e_list = _get_function_or_list(
+        {
+            1: integratedcircuit.PI_E,
+            2: semiconductor.PI_E,
+            3: resistor.PI_E,
+            4: capacitor.PI_E,
+            5: inductor.PI_E,
+            7: switch.PI_E,
+            8: connection.PI_E,
+            9: meter.PI_E,
+            10: {
+                1: crystal.PI_E,
+                2: efilter.PI_E,
+                3: fuse.PI_E,
+                4: lamp.PI_E,
+            },
+        },
+        category_id,
+        subcategory_id,
+    )
 
     if category_id == 8 and subcategory_id in {1, 2}:
-        return _pi_e_lists[category_id][subcategory_id][quality_id][
-            environment_active_id - 1
-        ]
-    if category_id in {2, 3, 5, 7, 9, 10, 8}:
-        return _pi_e_lists[category_id][subcategory_id][environment_active_id - 1]
-
-    return _pi_e_lists[category_id][environment_active_id - 1]
+        return _pi_e_list[quality_id][environment_active_id - 1]
+    else:
+        return _pi_e_list[environment_active_id - 1]
 
 
 def _get_part_count_quality_factor(
@@ -287,28 +270,29 @@ def _get_part_count_quality_factor(
         environment ID.
     :raise: KeyError if there is no piQ list for the passed category ID.
     """
-    _pi_q_lists = {
-        1: integratedcircuit.PI_Q,
-        3: resistor.PART_COUNT_PI_Q,
-        4: capacitor.PART_COUNT_PI_Q,
-        5: inductor.PART_COUNT_PI_Q,
-        6: relay.PART_COUNT_PI_Q,
-        7: switch.PART_COUNT_PI_Q,
-        8: connection.PART_COUNT_PI_Q,
-        9: meter.PART_COUNT_PI_Q,
-        10: {1: crystal.PART_COUNT_PI_Q, 2: efilter.PI_Q},
-    }
-
-    if category_id in {6, 7, 9}:
-        return _pi_q_lists[category_id][subcategory_id][quality_id - 1]
-
-    if category_id == 10 and subcategory_id in {1, 2}:
-        return _pi_q_lists[category_id][subcategory_id][quality_id - 1]
+    _pi_q_list = _get_function_or_list(
+        {
+            1: integratedcircuit.PI_Q,
+            3: resistor.PART_COUNT_PI_Q,
+            4: capacitor.PART_COUNT_PI_Q,
+            5: inductor.PART_COUNT_PI_Q,
+            6: relay.PART_COUNT_PI_Q,
+            7: switch.PART_COUNT_PI_Q,
+            8: connection.PART_COUNT_PI_Q,
+            9: meter.PART_COUNT_PI_Q,
+            10: {
+                1: crystal.PART_COUNT_PI_Q,
+                2: efilter.PI_Q,
+            },
+        },
+        category_id,
+        subcategory_id,
+    )
 
     if category_id == 10 and subcategory_id in {3, 4}:
         return 1.0
 
-    return _pi_q_lists[category_id][quality_id - 1]
+    return _pi_q_list[quality_id - 1]
 
 
 # pylint: disable=too-many-return-statements
@@ -327,37 +311,51 @@ def _get_part_stress_quality_factor(
     :raise: IndexError if there is no list entry for the passed quality ID.
     :raise: KeyError if there is no piQ list for the passed category ID.
     """
-    _pi_q_lists = {
-        1: integratedcircuit.PI_Q,
-        3: resistor.PART_STRESS_PI_Q,
-        4: capacitor.PART_STRESS_PI_Q,
-        6: relay.PART_STRESS_PI_Q,
-        7: switch.PART_STRESS_PI_Q,
-        8: connection.PART_STRESS_PI_Q,
-        9: meter.PART_STRESS_PI_Q,
-        10: {1: crystal.PART_STRESS_PI_Q, 2: efilter.PI_Q},
-    }
-
-    if category_id == 1:
-        return _pi_q_lists[category_id][quality_id - 1]
+    _pi_q_list = _get_function_or_list(
+        {
+            1: integratedcircuit.PI_Q,
+            3: resistor.PART_STRESS_PI_Q,
+            4: capacitor.PART_STRESS_PI_Q,
+            6: relay.PART_STRESS_PI_Q,
+            7: switch.PART_STRESS_PI_Q,
+            8: connection.PART_STRESS_PI_Q,
+            9: meter.PART_STRESS_PI_Q,
+            10: {
+                1: crystal.PART_STRESS_PI_Q,
+                2: efilter.PI_Q,
+            },
+        },
+        category_id,
+        subcategory_id,
+    )
 
     if (
-        category_id == 8
-        and subcategory_id in {4, 5}
-        or (category_id == 7 and subcategory_id == 5)
+        category_id in {7, 8}
+        or (category_id == 9 and subcategory_id == 1)
+        or (category_id == 10 and subcategory_id in {3, 4})
     ):
-        return _pi_q_lists[category_id][subcategory_id][quality_id - 1]
-
-    if category_id == 7:
         return 0.0
+    else:
+        return _pi_q_list[quality_id - 1]
 
-    if category_id == 8:
-        return 0.0
 
-    if category_id == 9 and subcategory_id == 1:
-        return 0.0
+def _get_function_or_list(
+    func_dict: Dict[int, Any], category_id: int, subcategory_id: Optional[int] = None
+) -> Callable:
+    """Retrieve the appropriate function based on category and subcategory IDs."""
+    if category_id == 10 and subcategory_id is not None:
+        return func_dict[category_id][subcategory_id]
+    return func_dict[category_id]
 
-    if category_id == 10 and subcategory_id in {3, 4}:
-        return 0.0
 
-    return _pi_q_lists[category_id][subcategory_id][quality_id - 1]
+def _handle_prediction_failure(
+    error_type: str, attributes: Dict[str, Any], additional_info: str = ""
+):
+    """Handle the failure of a hazard rate prediction and publish an error message."""
+    error_message = (
+        f"Failed to predict MIL-HDBK-217F hazard rate for hardware ID {attributes['hardware_id']}; "
+        f"category ID={attributes['category_id']}, subcategory ID={attributes['subcategory_id']}."
+    )
+    if additional_info:
+        error_message += f" {additional_info}"
+    pub.sendMessage(f"fail_predict_{error_type}", error_message=error_message)
