9b3c2b709233e7ec73cf083743519cc1e37227e2
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-02-08 17:04:22 -0500

    refactor: update crystal stub file for new changes

--- src/ramstk/analyses/milhdbk217f/models/crystal.pyi
+++ src/ramstk/analyses/milhdbk217f/models/crystal.pyi
@@ -1,16 +1,20 @@
 # Standard Library Imports
-from typing import Dict, List, Union
+from typing import Dict, Union
 
-PART_COUNT_LAMBDA_B: List[float]
-PART_COUNT_PI_Q: List[float]
-PART_STRESS_PI_Q: List[float]
-PI_E: List[float]
-
-def calculate_part_count(**attributes: Dict[str, Union[float, int, str]]) -> float: ...
 def calculate_part_stress(
-    **attributes: Dict[str, Union[float, int, str]]
+    attributes: Dict[str, Union[float, int, str]]
 ) -> Dict[str, Union[float, int, str]]: ...
-def get_part_count_lambda_b(environment_active_id: int) -> float: ...
+def calculate_part_stress_lambda_b(
+    attributes: Dict[str, Union[float, int, str]]
+) -> float: ...
+def get_environment_factor(attributes: Dict[str, Union[float, int, str]]) -> float: ...
+def get_part_count_lambda_b(attributes: Dict[str, Union[float, int, str]]) -> float: ...
+def get_part_count_quality_factor(
+    attributes: Dict[str, Union[float, int, str]]
+) -> float: ...
+def get_part_stress_quality_factor(
+    attributes: Dict[str, Union[float, int, str]]
+) -> float: ...
 def set_default_values(
     **attributes: Dict[str, Union[float, int, str]]
 ) -> Dict[str, Union[float, int, str]]: ...
