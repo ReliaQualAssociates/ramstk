9d12f572c7dd0c7f91d5a99b41ae1b94a662af17
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-02-08 19:55:37 -0500

    test: updated tests for refactored code

--- tests/analyses/milhdbk217f/models/filter_unit_test.py
+++ tests/analyses/milhdbk217f/models/filter_unit_test.py
@@ -15,57 +15,6 @@ import pytest
 # RAMSTK Package Imports
 from ramstk.analyses.milhdbk217f import efilter
 
-PART_COUNT_LAMBDA_B = {
-    1: [
-        0.022,
-        0.044,
-        0.13,
-        0.088,
-        0.20,
-        0.15,
-        0.20,
-        0.24,
-        0.29,
-        0.24,
-        0.018,
-        0.15,
-        0.33,
-        2.6,
-    ],
-    2: [
-        0.12,
-        0.24,
-        0.72,
-        0.48,
-        1.1,
-        0.84,
-        1.1,
-        1.3,
-        1.6,
-        1.3,
-        0.096,
-        0.84,
-        1.8,
-        1.4,
-    ],
-    3: [
-        0.27,
-        0.54,
-        1.6,
-        1.1,
-        2.4,
-        1.9,
-        2.4,
-        3.0,
-        3.5,
-        3.0,
-        0.22,
-        1.9,
-        4.1,
-        32.0,
-    ],
-}
-
 
 @pytest.mark.unit
 @pytest.mark.usefixtures("test_attributes_filter")
