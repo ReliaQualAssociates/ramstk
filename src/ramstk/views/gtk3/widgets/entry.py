b70dd5dfc6b8e51a00d77d6dd6da0a975313282b
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-04 15:14:06 -0400

    refactor: re-write RAMSTKEntry and RAMSTKTextView to use new RAMSTKBaseWidget

--- src/ramstk/views/gtk3/widgets/entry.py
+++ src/ramstk/views/gtk3/widgets/entry.py
@@ -1,202 +1,213 @@
 # pylint: disable=non-parent-init-called
 # -*- coding: utf-8 -*-
 #
-#       ramstk.gui.gtk.ramstk.Entry.py is part of the RAMSTK Project
+#       ramstk.views.gtk3.widgets.entry.py is part of the RAMSTK Project
 #
 # All rights reserved.
-# Copyright 2007 - 2017 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""RAMSTK Entry Module."""
+# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
+"""The RAMSTKEntry module."""
 
 # Standard Library Imports
-from datetime import datetime
-from typing import Any, Callable
+from datetime import date, datetime
+from typing import Dict, Union
+
+# Third Party Imports
+from pubsub import pub
 
 # RAMSTK Package Imports
 from ramstk.views.gtk3 import Gtk, Pango
 
 # RAMSTK Local Imports
-from .widget import RAMSTKWidget
+from .widget import RAMSTKBaseWidget, WidgetProperties
 
 
-class RAMSTKEntry(Gtk.Entry, RAMSTKWidget):
-    """The RAMSTK Entry class."""
+class RAMSTKEntry(Gtk.Entry, RAMSTKBaseWidget):
+    """The RAMSTKEntry class."""
 
     # Define private scalar class attributes.
     _default_height = 25
     _default_width = 200
+    _edit_signal = "changed"
 
     def __init__(self) -> None:
-        """Create a RAMSTK Entry widget."""
-        RAMSTKWidget.__init__(self)
+        """Initialize an instance of the RAMSTKEntry widget."""
+        RAMSTKBaseWidget.__init__(self)
 
         self.show()
 
-    def do_get_text(self):
-        """Wrap the Gtk.Entry().get_text() method for consistent calls.
+    # ----- ----- Standard widget methods. ----- ----- #
+    def do_set_properties(self, properties: WidgetProperties) -> None:
+        """Set the properties of the RAMSTKEntry.
 
-        :return: the text in the RAMSTKEntry().
-        :rtype: str
+        :param properties: the WidgetProperties dict with the property values to set for
+            the RAMSTKEntry.
         """
-        return self.get_text()
+        super().do_set_properties(properties)
 
-    def do_set_properties(self, **kwargs: Any) -> None:
-        """Set the properties of the RAMSTK Entry.
+        self.dic_properties["bold"] = properties.get("bold", False)
+        self.dic_properties["editable"] = properties.get("editable", True)
+        self.dic_properties["input_purpose"] = properties.get(
+            "input_purpose",
+            Gtk.InputPurpose.FREE_FORM,
+        )
+        self.dic_properties["invisible_char"] = properties.get("invisible_char", "*")
+        self.dic_properties["xalign"] = properties.get("xalign", True)
 
-        Accepts the following keyword arguments:
+        self.set_property("editable", self.dic_properties["editable"])
 
-            * *width* -- width of the Gtk.Entry() widget.
-                Default is 200.
-            * *height* -- height of the Gtk.Entry() widget.
-                Default is 25.
-            * *editable* -- boolean indicating whether Gtk.Entry()
-                should be editable.  Defaults to True.
-            * *bold* -- boolean indicating whether text should be bold.
-                Defaults to False.
-            * *color* -- the hexidecimal color to set the background when
-                the Gtk.Entry() is not editable.  Default is #BBDDFF (light
-                blue).
-            * *tooltip* -- the tooltip, if any, for the entry.
-                Default is an empty string.
-        :return: None
-        :rtype: None
-        """
-        super().do_set_properties(**kwargs)
-
-        _bold = kwargs.get("bold", False)
-        _editable = kwargs.get("editable", True)
-
-        self.set_property("editable", _editable)
-
-        if _bold:
+        if self.dic_properties["bold"]:
             self.modify_font(Pango.FontDescription("bold"))
 
-    def do_update(self, value: Any, signal: str = "") -> None:
-        """Update the RAMSTK Entry with a new value.
+        self.set_input_purpose(self.dic_properties["input_purpose"])
+        self.set_invisible_char(self.dic_properties["invisible_char"])
 
-        :param value: the information to update the RAMSTKEntry() to display. :keyword
-            signal: the name of the signal whose handler ID the RAMSTKEntry() needs to
-            block.
-        :return: None
-        :rtype: None
+    def do_update(self, package: Dict[str, Union[bool, date, float, int, str]]) -> None:
+        """Update the RAMSTKEntry with a new value.
+
+        :param package: the date package to use to update the RAMSTKEntry.
         """
-        try:
-            _value = datetime.strftime(value, "%Y-%m-%d")
-        except TypeError:
-            _value = str(value)
+        _field, _value = next(iter(package.items()))
 
-        # Sometimes there is no handler ID for the RAMSTKEntry().  This
-        # usually happens when the widget is being used to display results
-        # and has no associated callback method.
-        try:
-            _handler_id = self.dic_handler_id[signal]
+        if _field != self.field:
+            return
 
-            self.handler_block(_handler_id)
+        if isinstance(_value, date):
+            _value = datetime.strftime(_value, "%Y-%m-%d")
+
+        try:
+            self.handler_block(self.handler_id)
             self.set_text(_value)
-            self.handler_unblock(_handler_id)
+            self.handler_unblock(self.handler_id)
         except KeyError:
             self.set_text(_value)
 
+    def on_changed(
+        self, __entry: RAMSTKBaseWidget  # pylint: disable=unused-argument
+    ) -> None:
+        """Retrieve the data package for the RAMSTKEntry on value changes.
 
-class RAMSTKTextView(Gtk.TextView, RAMSTKWidget):
-    """The RAMSTK TextView class."""
+        This method also sends a PyPubSub message along with the data package for
+        listeners to update with the new value.
+
+        :param __entry: the RAMSTKEntry whose edited signal called this method.
+        """
+        try:
+            self.handler_block(self.handler_id)
+            _package = {self.field: self.do_get_text()}
+            self.handler_unblock(self.handler_id)
+        except KeyError:
+            _package = {self.field: self.do_get_text()}
+
+        pub.sendMessage(self.send_topic, node_id=self.record_id, package=_package)
+
+    # ----- ----- RAMSTKEntry specific methods. ----- ----- #
+    def do_get_text(self) -> Union[float, int, str, None]:
+        """Retrieve the text displayed in the RAMSTKEntry.
+
+        This method will return the correct datatype (float, int, str) associated with
+        the database field associated with the RAMSTKEntry.
+
+        :return: the text in the RAMSTKEntry.
+        """
+        _value: Union[float, int, str, None] = None
+
+        if self.datatype == "gfloat":
+            _value = float(self.get_text())
+        elif self.datatype == "gint":
+            _value = int(self.get_text())
+        elif self.datatype == "gchararray":
+            _value = str(self.get_text())
+
+        return _value
+
+
+class RAMSTKTextView(Gtk.TextView, RAMSTKBaseWidget):
+    """The RAMSTKTextView class."""
 
     # Define private class scalar attributes.
     _default_height = 100
     _default_width = 200
+    _edit_signal = "changed"
 
-    def __init__(self, txvbuffer: Gtk.TextBuffer) -> None:
-        """Create RAMSTK TextView() widgets.
+    def __init__(self, txtbuffer: Gtk.TextBuffer) -> None:
+        """Initialize an instance of the RAMSTKTextView widget.
 
-        Returns a Gtk.TextView() embedded in a Gtk.ScrolledWindow().
-
-        :param txvbuffer: the Gtk.TextBuffer() to associate with the RAMSTKTextView().
-        :return: None
-        :rtype: None
+        :param txtbuffer: the Gtk.TextBuffer to associate with the RAMSTKTextView.
         """
-        RAMSTKWidget.__init__(self)
+        RAMSTKBaseWidget.__init__(self)
 
-        self.set_buffer(txvbuffer)
-        self.set_wrap_mode(Gtk.WrapMode.WORD)
+        self.dic_properties["buffer"] = txtbuffer
+        self.tag_bold = self.dic_properties["buffer"].create_tag(  # type: ignore[union-attr] # noqa
+            "bold", weight=Pango.Weight.BOLD
+        )
+        self.set_buffer(self.dic_properties["buffer"])
 
         self.scrollwindow = Gtk.ScrolledWindow()
         self.scrollwindow.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
         self.scrollwindow.add_with_viewport(self)
 
-        self.tag_bold = txvbuffer.create_tag("bold", weight=Pango.Weight.BOLD)
+    # ----- ----- Standard widget methods. ----- ----- #
+    def do_set_properties(self, properties: WidgetProperties) -> None:
+        """Set the properties of the RAMSTKTextView.
 
-    # pylint: disable=arguments-differ
-    def connect(
-        self,
-        signal: str,
-        callback: Callable,
-        index: int,
-        message: str,
-    ) -> int:
-        """Connect textview's buffer to a callback method or function.
-
-        :param signal:
-        :param callback:
-        :param index:
-        :param message:
+        :param properties: the WidgetProperties dict with the property values to set for
+            the RAMSTKTextView.
         """
-        _buffer = self.do_get_buffer()
-        return _buffer.connect(signal, callback, index, message, self)
+        super().do_set_properties(properties)
 
-    def do_get_buffer(self) -> Gtk.TextBuffer:
-        """Return the Gtk.TextBuffer() emedded in the RAMSTK TextView.
+        self.dic_properties["buffer"] = properties.get("buffer", Gtk.TextBuffer())
+        self.dic_properties["editable"] = properties.get("editable", True)
+        self.dic_properties["justify"] = properties.get(
+            "justify",
+            Gtk.Justification.RIGHT,
+        )
+        self.dic_properties["wrap_mode"] = properties.get(
+            "wrap_mode", Gtk.WrapMode.WORD
+        )
 
-        :return: buffer; the embedded Gtk.TextBuffer()
-        :rtype: :class:`Gtk.TextBuffer`
+        self.set_editable(self.dic_properties["editable"])
+        self.set_justification(self.dic_properties["justify"])
+        self.set_wrap_mode(self.dic_properties["wrap_mode"])
+
+    def do_update(self, package: Dict[str, Union[bool, date, float, int, str]]) -> None:
+        """Update the RAMSTKTextView with a new value.
+
+        :param package: the date package to use to update the RAMSTKTextView.
         """
-        return self.get_buffer()
+        _field, _value = next(iter(package.items()))
 
-    def do_get_text(self) -> Any:
+        if _field != self.field:
+            return
+
+        try:
+            self.dic_properties["buffer"].handler_block(self.handler_id)  # type: ignore[union-attr] # noqa
+            self.dic_properties["buffer"].set_text(str(_value))  # type: ignore[union-attr] # noqa
+            self.dic_properties["buffer"].handler_unblock(self.handler_id)  # type: ignore[union-attr] # noqa
+        except KeyError:
+            self.dic_properties["buffer"].set_text(str(_value))  # type: ignore[union-attr] # noqa
+
+    def on_changed(self) -> None:
+        """Retrieve the data package for the RAMSTKTextView on value changes.
+
+        This method also sends a PyPubSub message along with the data package for
+        listeners to update with the new value.
+        """
+        try:
+            self.dic_properties["buffer"].handler_block(self.handler_id)
+            _package = {self.field: self.do_get_text()}
+            self.dic_properties["buffer"].handler_unblock(self.handler_id)
+        except KeyError:
+            _package = {self.field: self.do_get_text()}
+
+        pub.sendMessage(self.topic, node_id=self.record_id, package=_package)
+
+    # ----- ----- RAMSTKTextView specific methods. ----- ----- #
+    def do_get_text(self) -> str:
         """Retrieve the text from the embedded Gtk.TextBuffer().
 
         :return: text; the text in the Gtk.TextBuffer().
         :rtype: str
         """
-        _buffer = self.do_get_buffer()
-
-        return _buffer.get_text(*_buffer.get_bounds(), True)
-
-    def do_set_properties(self, **kwargs: Any) -> None:
-        """Set the properties of the RAMSTK TextView.
-
-        Accepts the following keyword arguments:
-
-            * *height* -- height of the Gtk.TextView() widget.  Default is 25.
-            * *tooltip* -- the tooltip, if any, for the entry.  Default is an
-                empty string.
-            * *width* -- width of the Gtk.TextView() widget.  Default is 200.
-        :return: None
-        :rtype: None
-        """
-        super().do_set_properties(**kwargs)
-
-        self.scrollwindow.set_property("height-request", self.height)
-        self.scrollwindow.set_property("width-request", self.width)
-        self.scrollwindow.set_shadow_type(Gtk.ShadowType.ETCHED_OUT)
-
-    def do_update(self, value: str, signal: str = "") -> None:
-        """Update the RAMSTK TextView with a new value.
-
-        :param value: the information to update the RAMSTKTextView() to display.
-            :keyword str signal: the name of the signal whose handler ID the
-            RAMSTKTextView() needs to block.
-        :return: None
-        :rtype: None
-        """
-        _buffer = self.do_get_buffer()
-
-        # Sometimes there is no handler ID for the RAMSTKTextView().  This
-        # usually happens when the widget is being used to display results
-        # and has no associated callback method.
-        try:
-            _handler_id = self.dic_handler_id[signal]
-
-            _buffer.handler_block(_handler_id)
-            _buffer.set_text(str(value))
-            _buffer.handler_unblock(_handler_id)
-        except KeyError:
-            _buffer.set_text(str(value))
+        return self.dic_properties["buffer"].get_text(  # type: ignore[union-attr] # noqa
+            *self.dic_properties["buffer"].get_bounds(), True  # type: ignore[union-attr] # noqa
+        )
