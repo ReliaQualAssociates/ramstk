58ab9367b43b137e2b540149bdd321e39a2e7836
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-04 17:55:39 -0400

    refactor: re-write RAMSTKPlot to use new RAMSTKBaseWidget
    Also creates a plot properties class for typing.

--- src/ramstk/views/gtk3/widgets/plot.py
+++ src/ramstk/views/gtk3/widgets/plot.py
@@ -3,15 +3,16 @@
 #       ramstk.views.gtk3.widgets.plot.py is part of the RAMSTK Project
 #
 # All rights reserved.
-# Copyright 2007 - 2020 Doyle Rowland doyle.rowland <AT> reliaqual <DOT> com
+# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
 # isort:skip_file
-"""RAMSTK GTK3 Plot Module."""
+"""The RAMSTKPlot module."""
 
 # Standard Library Imports
-from typing import Any, Dict, List, Tuple
+from typing import List, Optional, Tuple, TypedDict, Union
 
 # RAMSTK Package Imports
 from ramstk.views.gtk3 import Gdk, Gtk, _
+from .widget import RAMSTKBaseWidget
 
 # type: ignore
 try:
@@ -34,69 +35,90 @@ except RuntimeError:
     pass
 
 
-class RAMSTKPlot:
-    """The RAMSTKPlot class.
+class PlotProperties(TypedDict, total=False):
+    """Type for plot properties dict."""
 
-    This module contains RAMSTK plot class.  This class is derived from the applicable
-    pyGTK widgets and matplotlib plots, but are provided with RAMSTK specific property
-    values and methods.  This ensures a consistent look and feel to widgets in the
-    RAMSTK application.
-    """
+    font_size: Union[int, str]
+    """font_size options are:
+        - xx-small
+        - x-small
+        - small
+        - medium
+        - large
+        - x-large
+        - xx-large
+        - an actual integer value"""
+    font_weight: str
+    frame_on: bool
+    horizontal_alignment: str
+    line_width: float
+    location: str
+    """location options are:
+        - best
+        - upper right
+        - upper left
+        - lower left
+        - lower right
+        - right
+        - center left
+        - center right
+        - lower center
+        - upper center
+        - center"""
+    n_columns: int
+    rotation: str
+    shadow: bool
+    title: str
+    vertical_alignment: str
+    x_pos: int
+    y_pos: int
+
+
+class RAMSTKPlot(RAMSTKBaseWidget):
+    """The RAMSTKPlot class."""
 
     def __init__(self) -> None:
-        """Initialize an instance of the RAMSTKPlot."""
-        # Initialize private dictionary attributes.
+        """Initialize an instance of the RAMSTKPlot widget."""
+        RAMSTKBaseWidget.__init__(  # type: ignore[call-arg] # pylint: disable=too-many-function-args # noqa
+            self
+        )
 
-        # Initialize private list attributes.
+        # Initialize private instance attributes.
         self._lst_max: List[float] = []
         self._lst_min: List[float] = [0.0]
 
-        # Initialize private scalar attributes.
-
-        # Initialize public dictionary attributes.
-
-        # Initialize public list attributes.
-
-        # Initialize public scalar attributes.
+        # Initialize public instance attributes.
         self.figure: matplotlib.figure.Figure = Figure()
         self.canvas = FigureCanvas(self.figure)
         self.axis = self.figure.add_subplot(111)
 
+    # ----- ----- RAMSTKPlot specific methods. ----- ----- #
     def do_load_plot(
         self,
         x_values: List[float],
         y_values: List[float],
-        **kwargs: Dict[str, str],
+        plot_type: str = "scatter",
     ) -> None:
         """Load the RAMSTKPlot.
 
-        Plots can be one of the following types:
-
-            * 'date'
-            * 'histogram'
-            * 'scatter' (default)
-            * 'step'
-
-        Pass the keyword plot_type to select.  Markers can be set using the
-        marker keyword.  The default marker is 'g-' or a solid green line.  See
-        matplotlib documentation for other options.
+        Pass the keyword plot_type to select.  Markers can be set using the marker
+        keyword.  The default marker is 'g-' or a solid green line.  See matplotlib
+        documentation for other options.
 
         :param x_values: list of the x-values to plot.
-        :param y_values: list of the y-values to plot or list of bin edges if
-            plotting a histogram.
-        :return: None
-        :rtype: None
+        :param y_values: list of the y-values to plot or list of bin edges if plotting a
+            histogram.
+        :param plot_type: the type of plot to produce. Options are 'date', 'histogram',
+            'scatter' (default), and 'step'.
         """
-        _plot_type = kwargs.get("plot_type", "scatter")
-
-        if _plot_type == "step":
-            self._do_make_step_plot(x_values, y_values, **kwargs)
-        elif _plot_type == "scatter":
-            self._do_make_scatter_plot(x_values, y_values, **kwargs)
-        elif _plot_type == "histogram":
-            self._do_make_histogram(x_values, y_values, **kwargs)
-        elif _plot_type == "date":
-            self._do_make_date_plot(x_values, y_values, **kwargs)
+        if plot_type == "step":
+            self._do_make_step_plot(x_values, y_values)
+        elif plot_type == "scatter":
+            self._do_make_scatter_plot(x_values, y_values)
+        elif plot_type == "histogram":
+            self._do_make_histogram(x_values, y_values)
+        elif plot_type == "date":
+            self._do_make_date_plot(x_values, y_values)
 
         _min, _max = self._get_minimax_ordinates()
 
@@ -107,7 +129,7 @@ class RAMSTKPlot:
     def do_add_line(
         self,
         x_values: List[float],
-        y_values: List[float] = None,
+        y_values: Optional[List[float]] = None,
         color: str = "k",
         marker: str = "^",
     ) -> None:
@@ -120,8 +142,6 @@ class RAMSTKPlot:
             See matplotlib documentation for options.
         :param marker: the marker to use on the plot. Default is '^' or an upward
             pointing triangle. See matplotlib documentation for other options.
-        :return: None
-        :rtype: None
         """
         _line = Line2D(
             x_values,
@@ -144,20 +164,16 @@ class RAMSTKPlot:
         :type __event: :class:`Gdk.Event`
         :param parent: the original parent Gtk.Widget() for the plot.
         :type parent: :class:`Gtk.Widget`
-        :return: None
-        :rtype: None
         """
         self.canvas.reparent(parent)
 
     # noinspection PyUnresolvedReferences
     def do_expand_plot(self, event: matplotlib.backend_bases.MouseEvent) -> None:
-        """Display a plot in it's own window.
+        """Display a plot in its own window.
 
         :param event: the matplotlib.backend_bases.MouseEvent() that called
             this method.
         :type event: :class:`matplotlib.backend_bases.MouseEvent`
-        :return: None
-        :rtype: None
         """
         self.canvas = event.canvas
         _parent = self.canvas.get_parent()
@@ -178,132 +194,92 @@ class RAMSTKPlot:
             _window.show_all()
 
     # noinspection PyUnresolvedReferences
-    def do_make_labels(self, label: str, **kwargs: Any) -> matplotlib.text.Text:
+    def do_make_labels(
+        self,
+        label: str,
+        properties: PlotProperties,
+        set_x: bool = True,
+        x_pos: int = 0,
+        y_pos: int = 0,
+    ) -> matplotlib.text.Text:
         """Make the abscissa or ordinate label.
-
-        Accepts keyword arguments:
-            * *fontsize* -- the size of the font to use for the axis label.
-            * *fontweight* -- the weight of the font to use for the axis label.
-            * *set_x* -- whether to set the abscissa (default) or ordinate
-                label.
-            * *x_pos* -- the position along the abscissa to place the label.
-            * *y_pos* -- the position along the ordinate to place the label.
 
         :param label: the text to display as the abscissa or ordinate
             label.
+        :param properties: the PlotProperties for the plot label.
+        :param set_x: whether to set the x-axis label (default) or the y-axis label.
+        :param x_pos:
+        :param y_pos:
         :return: matplotlib text instance representing the label.
         :rtype: :class:`matplotlib.text.Text`
         """
-        _fontsize = kwargs.get("fontsize", 14)
-        _fontweight = kwargs.get("fontweight", "bold")
-        _set_x = kwargs.get("set_x", True)
-        _x_pos = kwargs.get("x_pos", 0)
-        _y_pos = kwargs.get("y_pos", 0)
-
-        if _set_x:
+        if set_x:
             return self.axis.set_xlabel(
                 label,
                 {
-                    "fontsize": _fontsize,
-                    "fontweight": _fontweight,
-                    "verticalalignment": "center",
-                    "horizontalalignment": "center",
-                    "x": _x_pos,
-                    "y": _y_pos,
+                    "fontsize": properties.get("font_size", 14),
+                    "fontweight": properties.get("font_weight", "bold"),
+                    "horizontalalignment": properties.get(
+                        "horizontal_alignment", "center"
+                    ),
+                    "verticalalignment": properties.get("vertical_alignment", "center"),
+                    "x": x_pos,
+                    "y": y_pos,
                 },
             )
         return self.axis.set_ylabel(
             label,
             {
-                "fontsize": _fontsize,
-                "fontweight": _fontweight,
-                "verticalalignment": "center",
-                "horizontalalignment": "center",
-                "rotation": "vertical",
+                "fontsize": properties.get("font_size", 14),
+                "fontweight": properties.get("font_weight", "bold"),
+                "verticalalignment": properties.get("horizontal_alignment", "center"),
+                "horizontalalignment": properties.get("vertical_alignment", "center"),
+                "rotation": properties.get("rotation", "vertical"),
             },
         )
 
     # pylint: disable=too-many-arguments
-    def do_make_legend(self, text: List[str], **kwargs: Any) -> None:
+    def do_make_legend(
+        self, text: List[str], title: str, properties: PlotProperties
+    ) -> None:
         """Make a legend on the RAMSTKPlot.
 
-        Accepts keyword arguments:
-            * *fontsize* (str) -- the size of the font to use for the legend.
-                Options are:
-                    - xx-small
-                    - x-small
-                    - small (default)
-                    - medium
-                    - large
-                    - x-large
-                    - xx-large
-            * *frameon* (bool) -- whether or not there is a frame around the
-                legend.
-            * *location* (str) -- the location of the legend on the plot.
-                Options are:
-                    - best
-                    - upper right (default)
-                    - upper left
-                    - lower left
-                    - lower right
-                    - right
-                    - center left
-                    - center right
-                    - lower center
-                    - upper center
-                    - center
-            * *ncol* (int) -- the number columns in the legend.  Default is 1.
-            * *shadow* (bool) -- whether or not to display a shadow behind the
-                legend block.  Default is True.
-            * *title* (str) -- the title of the legend.  Default is an empty
-                string.
-            * *lwd* (float) -- the linewidth of the box around the legend.
-
         :param text: the text to display in the legend.
-        :return: None
-        :rtype: None
+        :param title: the title for the legend block.
+        :param properties: the PlotProperties for the plot legend.
         """
-        _fontsize = kwargs.get("fontsize", "small")
-        _frameon = kwargs.get("frameon", False)
-        _location = kwargs.get("location", "upper right")
-        _lwd = kwargs.get("lwd", 0.5)
-        _ncol = kwargs.get("ncol", 1)
-        _shadow = kwargs.get("shadow", True)
-        _title = kwargs.get("title", "")
-
         _legend = self.axis.legend(
             text,
-            frameon=_frameon,
-            loc=_location,
-            ncol=_ncol,
-            shadow=_shadow,
-            title=_title,
+            frameon=properties.get("frame_on", False),
+            loc=properties.get("location", "upper right"),
+            ncol=properties.get("n_columns", 1),
+            shadow=properties.get("shadow", True),
+            title=title,
         )
 
         for _text in _legend.get_texts():
-            _text.set_fontsize(_fontsize)
+            _text.set_fontsize(properties.get("font_size", "small"))
         for _line in _legend.get_lines():
-            _line.set_linewidth(_lwd)
+            _line.set_linewidth(properties.get("line_width", 0.5))
 
     # noinspection PyUnresolvedReferences
     def do_make_title(
-        self, title: str, fontsize: int = 16, fontweight: str = "bold"
+        self, title: str, properties: PlotProperties
     ) -> matplotlib.text.Text:
         """Make the plot title.
 
         :param title: the text to display as the title.
-        :param fontsize: the size of the font to use for the title.
-        :param fontweight: the weight of the font to use for the title.
+        :param properties: the PlotProperties for the plot title.
         :return: matplotlib text instance representing the title.
         :rtype: :class:`matplotlib.text.Text`
         """
         return self.axis.set_title(
             title,
             {
-                "fontsize": fontsize,
-                "fontweight": fontweight,
-                "verticalalignment": "baseline",
-                "horizontalalignment": "center",
+                "fontsize": properties.get("font_size", 16),
+                "fontweight": properties.get("font_weight", "bold"),
+                "horizontalalignment": properties.get("horizontal_alignment", "center"),
+                "verticalalignment": properties.get("vertical_alignment", "baseline"),
             },
         )
 
@@ -311,22 +287,17 @@ class RAMSTKPlot:
         self,
         x_values: List[float],
         y_values: List[float],
-        **kwargs: Dict[str, str],
+        marker: str = "g-",
     ) -> None:
         """Make a date plot.
 
-        Use the keyword marker to set the type and color of marker to use for the plot.
-        Default is 'g-' or a solid green line.
-
         :param x_values: the list of x-values (dates) for the plot.
         :param y_values: the list of y-values for the plot.
-        :return: None
-        :rtype: None
+        :param marker: type and color of marker to use for the plot. Default is 'go' or
+            a solid green line.
         """
-        _marker = kwargs.get("marker", "g-")
-
         if y_values is not None:
-            self.axis.plot_date(x_values, y_values, _marker, xdate=True, linewidth=2)
+            self.axis.plot_date(x_values, y_values, marker, xdate=True, linewidth=2)
             self._lst_min.append(min(y_values))
             self._lst_max.append(max(y_values))
 
@@ -334,25 +305,19 @@ class RAMSTKPlot:
         self,
         x_values: List[float],
         y_values: List[float],
-        **kwargs: Dict[str, str],
+        marker: str = "g",
     ) -> None:
         """Make a histogram.
 
-        Use the keyword marker to set the color of the bars used for the plot. Default
-        is 'g' or green.
-
         :param list x_values: the list of x-values for the plot.
-        :param list y_values: the list of bin edges for the plot. :keyword str marker:
-            the color of bars to use for the histogram. Default is green.
-        :return: None
-        :rtype: None
+        :param list y_values: the list of bin edges for the plot.
+        :param marker: type and color of marker to use for the plot. Default is 'g' or
+            solid green.
         """
-        _marker = kwargs.get("marker", "g")
-
         if y_values is not None:
             self.axis.grid(False, which="both")
             # pylint: disable=unused-variable
-            _values, _edges, __ = self.axis.hist(x_values, bins=y_values, color=_marker)
+            _values, _edges, __ = self.axis.hist(x_values, bins=y_values, color=marker)
             self._lst_min.append(min(_values))
             self._lst_max.append(max(_values) + 1)
 
@@ -360,22 +325,17 @@ class RAMSTKPlot:
         self,
         x_values: List[float],
         y_values: List[float],
-        **kwargs: Dict[str, str],
+        marker: str = "go",
     ) -> None:
         """Make a scatter plot.
 
-        Use the keyword marker to set the type and color of marker to use for the plot.
-        Default is 'go' or open green circles.
-
         :param x_values: the list of x-values for the plot.
         :param y_values: the list of y-values for the plot.
-        :return: None
-        :rtype: None
+        :param marker: type and color of marker to use for the plot. Default is 'go' or
+            open green circles.
         """
-        _marker = kwargs.get("marker", "go")
-
         if y_values is not None:
-            (_line,) = self.axis.plot(x_values, y_values, _marker, linewidth=2)
+            (_line,) = self.axis.plot(x_values, y_values, marker, linewidth=2)
             _line.set_ydata(y_values)
             self._lst_min.append(min(y_values))
             self._lst_max.append(max(y_values))
@@ -384,22 +344,17 @@ class RAMSTKPlot:
         self,
         x_values: List[float],
         y_values: List[float],
-        **kwargs: Dict[str, str],
+        marker: str = "g-",
     ) -> None:
         """Make a step plot.
 
-        Use the keyword marker to set the type and color of marker to use for the plot.
-        Default is 'g-' or a solid green line.
-
         :param list x_values: the list of x-values for the plot.
         :param list y_values: the list of y-values for the plot.
-        :return: None
-        :rtype: None
+        :param marker: type and color of marker to use for the plot. Default is 'g-' or
+            a solid green line.
         """
-        _marker = kwargs.get("marker", "g-")
-
         if y_values is not None:
-            (_line,) = self.axis.step(x_values, y_values, _marker, where="mid")
+            (_line,) = self.axis.step(x_values, y_values, marker, where="mid")
             _line.set_ydata(y_values)
             self._lst_min.append(min(y_values))
             self._lst_max.append(max(y_values))
