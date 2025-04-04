8d7b29967a8bd72790712d1f404e6405363fe864
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-04 15:31:12 -0400

    refactor: update combobox stub file

--- src/ramstk/views/gtk3/widgets/combo.pyi
+++ src/ramstk/views/gtk3/widgets/combo.pyi
@@ -1,5 +1,6 @@
 # Standard Library Imports
-from typing import Any, Dict, List, Union
+from datetime import date
+from typing import Any
 
 # RAMSTK Package Imports
 from ramstk.utilities import none_to_default as none_to_default
@@ -7,24 +8,20 @@ from ramstk.views.gtk3 import GObject as GObject
 from ramstk.views.gtk3 import Gtk as Gtk
 
 # RAMSTK Local Imports
-from .widget import RAMSTKWidget as RAMSTKWidget
+from .widget import RAMSTKBaseWidget as RAMSTKBaseWidget
+from .widget import WidgetProperties as WidgetProperties
 
-class RAMSTKComboBox(Gtk.ComboBox, RAMSTKWidget):
-    _default_height: int = ...
-    _default_width: int = ...
-    _index: Any = ...
+class RAMSTKComboBox(Gtk.ComboBox, RAMSTKBaseWidget):
     def __init__(
-        self,
-        index: int = ...,
-        simple: bool = ...,
-        n_items: int = ...,
+        self, index: int = 0, simple: bool = True, n_items: int = 2
     ) -> None: ...
-    def do_get_options(self) -> Dict[int, Any]: ...
+    def do_set_properties(self, properties: WidgetProperties) -> None: ...
+    def do_update(
+        self, package: dict[str, bool | date | float | int | str | None]
+    ) -> None: ...
+    def on_changed(self) -> None: ...
+    def do_get_options(self) -> dict[int, Any]: ...
     def do_load_combo(
-        self,
-        entries: List[List[Union[str, int]]],
-        signal: str = ...,
-        simple: bool = ...,
+        self, entries: list[list[str | int]], simple: bool = True
     ) -> None: ...
-    def do_update(self, value: int, signal: str = ...) -> None: ...
-    def get_value(self, index: int = ...) -> str: ...
+    def get_value(self, index: int = 0) -> str: ...
