a3c8edea0dc682addb1197627f6a3951ae996d70
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-04 15:13:02 -0400

    refactor: update widget stub file

--- src/ramstk/views/gtk3/widgets/widget.pyi
+++ src/ramstk/views/gtk3/widgets/widget.pyi
@@ -1,15 +1,125 @@
 # Standard Library Imports
-from typing import Any
+from datetime import date
+from typing import TypedDict
+
+# Third Party Imports
+from _typeshed import Incomplete
 
 # RAMSTK Package Imports
+from ramstk.views.gtk3 import Gdk as Gdk
 from ramstk.views.gtk3 import GObject as GObject
+from ramstk.views.gtk3 import Gtk as Gtk
+from ramstk.views.gtk3 import Pango as Pango
 from ramstk.views.gtk3 import _ as _
 
-class RAMSTKWidget:
-    _default_height: int = ...
-    _default_width: int = ...
-    dic_handler_id: Any = ...
-    height: int = ...
-    width: int = ...
+class WidgetAttributes(TypedDict, total=False):
+    datatype: bool | date | float | int | str | None
+    default: bool | date | float | int | str | None
+    field: str
+    format: str
+    index: int
+    label_text: str | None
+    listen_topic: str
+    record_id: int
+    send_topic: str
+    subscribe: str
+    x_pos: int
+    y_pos: int
+
+class WidgetProperties(TypedDict, total=False):
+    action: Gtk.FileChooserAction | None
+    activatable: bool
+    adjustment: Gtk.Adjustment | None
+    alignment: Pango.Alignment | None
+    always_show_image: bool
+    angle: float
+    background_rgba: Gdk.RGBA | None
+    bg_color: str
+    bold: bool
+    buffer: Gtk.TextBuffer | None
+    can_focus: bool
+    cell_background_rgba: Gdk.RGBA | None
+    cell_foreground_rgba: Gdk.RGBA | None
+    climb_rate: float
+    destroy_with_parent: bool
+    digits: int
+    editable: bool
+    ellipsize: Pango.EllipsizeMode | None
+    enable_grid_lines: Gtk.TreeViewGridLines | None
+    enable_tree_lines: bool
+    fg_color: str
+    foreground_rgba: Gdk.RGBA | None
+    group: Gtk.RadioButton | None
+    has_entry: bool
+    height_request: int
+    icon: str
+    input_purpose: Gtk.InputPurpose | None
+    invisible_char: str
+    justify: Gtk.Justification | None
+    label: str
+    level_indentation: int
+    lines: int
+    lower: float
+    modal: bool
+    model: Gtk.ListStore | None
+    name: str
+    numeric: bool
+    opacity: int
+    page_increment: float
+    page_size: float
+    parent: Gtk.Widget | None
+    rubber_banding: bool
+    sensitive: bool
+    shadow_type: Gtk.ShadowType | None
+    snap_to_ticks: bool
+    step_increment: float
+    text_column: int
+    tooltip: str
+    tooltip_column: int
+    upper: float
+    use_markup: bool
+    use_underline: bool
+    visible: bool
+    weight: int
+    weight_set: bool
+    width_request: int
+    wrap: bool
+    wrap_mode: Gtk.WrapMode.WORD | None
+    wrap_width: int
+    xalign: float
+    yalign: float
+
+class RAMSTKBaseWidget(Gtk.Widget):
+    dic_handler_id: dict[str, int]
+    dic_properties: Incomplete
+    datatype: bool | date | float | int | str | None
+    default: bool | date | float | int | str | None
+    field: str
+    format: str
+    handler_id: int
+    height: int
+    index: int
+    label_text: str
+    listen_topic: str
+    record_id: int
+    send_topic: str
+    width: int
+    x_pos: int
+    y_pos: int
     def __init__(self) -> None: ...
-    def do_set_properties(self, **kwargs: Any) -> None: ...
+    def do_get_attribute(
+        self, attribute: str
+    ) -> bool | date | float | int | str | None: ...
+    def do_set_attributes(self, attributes: WidgetAttributes) -> None: ...
+    def do_set_callbacks(self) -> None: ...
+    def do_set_properties(self, properties: WidgetProperties) -> None: ...
+    def do_subscribe_to_messages(self) -> None: ...
+
+class WidgetConfig(TypedDict):
+    widget: RAMSTKBaseWidget
+    attributes: WidgetAttributes
+    properties: WidgetProperties
+
+def set_widget_sensitivity(
+    widgets: list[RAMSTKBaseWidget | Gtk.Widget], sensitive: bool = True
+) -> None: ...
