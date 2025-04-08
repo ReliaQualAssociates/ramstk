31aa89b26cbd206bd48639d7c7391791e78f3f75
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-08 18:44:36 -0400

    refactor: update options stub files

--- src/ramstk/views/gtk3/options/view.pyi
+++ src/ramstk/views/gtk3/options/view.pyi
@@ -1,14 +1,11 @@
 # RAMSTK Package Imports
 from ramstk.views.gtk3 import Gtk as Gtk
 from ramstk.views.gtk3 import _ as _
-from ramstk.views.gtk3.widgets import RAMSTKDialog as RAMSTKDialog
-from ramstk.views.gtk3.widgets import RAMSTKPanel as RAMSTKPanel
+from ramstk.views.gtk3.widgets import RAMSTKBasePanel as RAMSTKBasePanel
+from ramstk.views.gtk3.widgets.dialogs import RAMSTKBaseDialog as RAMSTKBaseDialog
 
 # RAMSTK Local Imports
 from . import OptionsPanel as OptionsPanel
 
-class OptionsDialog(RAMSTKDialog):
-    _pnlPanel: OptionsPanel
-    def __init__(self, parent: object = ...) -> None: ...
-    def _cancel(self, __button: Gtk.Button): ...
-    def __make_ui(self) -> None: ...
+class OptionsDialog(RAMSTKBaseDialog):
+    def __init__(self, parent: object = None) -> None: ...
