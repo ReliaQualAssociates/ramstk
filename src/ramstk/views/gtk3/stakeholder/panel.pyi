d16a0e9b60c9e7c8147f0c0fc356ed0b51068934
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-09 08:26:57 -0400

    refactor: update stakeholder stub file

--- src/ramstk/views/gtk3/stakeholder/panel.pyi
+++ src/ramstk/views/gtk3/stakeholder/panel.pyi
@@ -1,29 +1,31 @@
-# Standard Library Imports
-from typing import Any, Callable, Dict, List, Tuple
-
 # Third Party Imports
 import treelib
+from _typeshed import Incomplete
 
 # RAMSTK Package Imports
+from ramstk.utilities import do_subscribe_to_messages as do_subscribe_to_messages
 from ramstk.views.gtk3 import Gtk as Gtk
 from ramstk.views.gtk3 import _ as _
+from ramstk.views.gtk3.widgets import RAMSTKCellRendererCombo as RAMSTKCellRendererCombo
+from ramstk.views.gtk3.widgets import RAMSTKCellRendererSpin as RAMSTKCellRendererSpin
+from ramstk.views.gtk3.widgets import RAMSTKCellRendererText as RAMSTKCellRendererText
 from ramstk.views.gtk3.widgets import RAMSTKTreePanel as RAMSTKTreePanel
+from ramstk.views.gtk3.widgets import WidgetConfig as WidgetConfig
 
 class StakeholderTreePanel(RAMSTKTreePanel):
     _select_msg: str
     _tag: str
-    _title: str
-    _dic_row_loader: Dict[str, Callable]
+    _title: Incomplete
+    _lst_widget_configuration: list[WidgetConfig]
     _on_edit_message: str
-    dic_attribute_widget_map: Dict[str, List[Any]]
     def __init__(self) -> None: ...
     def do_load_affinity_groups(
-        self, affinities: Dict[int, Tuple[str, str]]
+        self, affinities: dict[int, tuple[str, str]]
     ) -> None: ...
-    def do_load_stakeholders(self, stakeholders: Dict[int, str]) -> None: ...
+    def do_load_stakeholders(self, stakeholders: dict[int, str]) -> None: ...
     def _do_load_requirements(self, tree: treelib.Tree) -> None: ...
     def _on_insert(self, tree: treelib.Tree) -> None: ...
-    def _on_module_switch(self, module: str = ...) -> None: ...
-    _record_id: int
-    _parent_id: int
+    def _on_module_switch(self, module: str = "") -> None: ...
+    _record_id: Incomplete
+    _parent_id: Incomplete
     def _on_row_change(self, selection: Gtk.TreeSelection) -> None: ...
