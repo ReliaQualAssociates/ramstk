5f06dbf91285a15bcac6c240eded62d7125113e5
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-09 08:13:36 -0400

    refactor: update failure definition stub file

--- src/ramstk/views/gtk3/failure_definition/panel.pyi
+++ src/ramstk/views/gtk3/failure_definition/panel.pyi
@@ -1,24 +1,16 @@
 # Standard Library Imports
-from typing import Any, Callable, Dict, List
-
-# Third Party Imports
-import treelib
+from typing import Any
 
 # RAMSTK Package Imports
+from ramstk.utilities import do_subscribe_to_messages as do_subscribe_to_messages
 from ramstk.views.gtk3 import Gtk as Gtk
 from ramstk.views.gtk3 import _ as _
+from ramstk.views.gtk3.widgets import RAMSTKCellRendererText as RAMSTKCellRendererText
 from ramstk.views.gtk3.widgets import RAMSTKTreePanel as RAMSTKTreePanel
+from ramstk.views.gtk3.widgets import WidgetConfig as WidgetConfig
 
 class FailureDefinitionTreePanel(RAMSTKTreePanel):
-    _select_msg: str
-    _tag: str
-    _title: str
-    _dic_row_loader: Dict[str, Callable]
-    dic_attribute_widget_map: Dict[str, List[Any]]
     def __init__(self) -> None: ...
-    def _on_module_switch(self, module: str = ...) -> None: ...
-    _record_id: int
-    def _on_row_change(self, selection: Gtk.TreeSelection) -> None: ...
-    def __do_load_failure_definition(
-        self, node: treelib.Node, row: Gtk.TreeIter
-    ) -> Gtk.TreeIter: ...
+    def do_filter_tree(
+        self, model: Gtk.TreeModel, row: Gtk.TreeIter, data: Any
+    ) -> bool: ...
