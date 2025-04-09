aef52980872bb16e30ba12a01d2543efac411b46
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-09 08:07:47 -0400

    refactor: update usage profile stub files

--- src/ramstk/views/gtk3/usage_profile/panel.pyi
+++ src/ramstk/views/gtk3/usage_profile/panel.pyi
@@ -1,34 +1,21 @@
 # Standard Library Imports
-from typing import Any, Callable, Dict, List, Tuple
-
-# Third Party Imports
-import treelib
+from typing import Any
 
 # RAMSTK Package Imports
 from ramstk.views.gtk3 import GdkPixbuf as GdkPixbuf
 from ramstk.views.gtk3 import Gtk as Gtk
 from ramstk.views.gtk3 import _ as _
+from ramstk.views.gtk3.widgets import RAMSTKCellRendererCombo as RAMSTKCellRendererCombo
+from ramstk.views.gtk3.widgets import RAMSTKCellRendererText as RAMSTKCellRendererText
 from ramstk.views.gtk3.widgets import RAMSTKTreePanel as RAMSTKTreePanel
+from ramstk.views.gtk3.widgets import WidgetConfig as WidgetConfig
 
 class UsageProfileTreePanel(RAMSTKTreePanel):
-    _select_msg: str
-    _tag: str
-    _title: str
-    _dic_row_loader: Dict[str, Callable]
-    _dic_visible_mask: Dict[str, Dict[str, bool]]
-    _on_edit_message: str
-    dic_attribute_widget_map: Dict[str, List[Any]]
-    dic_icons: Dict[str, Any]
-    dic_units: Dict[str, Tuple[str, str, str]]
+    dic_icons: dict[str, Any]
+    dic_units: dict[str, tuple[str, str, str]]
+    level: str
     def __init__(self) -> None: ...
-    def do_load_combobox(self) -> None: ...
-    def _on_row_change(self, selection: Gtk.TreeSelection) -> None: ...
-    def __do_load_environment(
-        self, node: treelib.Node, row: Gtk.TreeIter
-    ) -> Gtk.TreeIter: ...
-    def __do_load_mission(
-        self, node: treelib.Node, row: Gtk.TreeIter
-    ) -> Gtk.TreeIter: ...
-    def __do_load_phase(
-        self, node: treelib.Node, row: Gtk.TreeIter
-    ) -> Gtk.TreeIter: ...
+    def do_get_usage_profile_level(
+        self, model: Gtk.TreeModel, row: Gtk.TreeIter
+    ) -> None: ...
+    def do_load_units(self, measurement_units: dict[int, tuple[str, str]]) -> None: ...
--- src/ramstk/views/gtk3/usage_profile/view.pyi
+++ src/ramstk/views/gtk3/usage_profile/view.pyi
@@ -1,29 +1,15 @@
-# Standard Library Imports
-from typing import List
-
 # RAMSTK Package Imports
 from ramstk.configuration import RAMSTKUserConfiguration as RAMSTKUserConfiguration
 from ramstk.logger import RAMSTKLogManager as RAMSTKLogManager
+from ramstk.utilities import do_subscribe_to_messages as do_subscribe_to_messages
 from ramstk.views.gtk3 import Gtk as Gtk
 from ramstk.views.gtk3 import _ as _
-from ramstk.views.gtk3.widgets import RAMSTKPanel
 from ramstk.views.gtk3.widgets import RAMSTKWorkView as RAMSTKWorkView
 
 # RAMSTK Local Imports
 from . import UsageProfileTreePanel as UsageProfileTreePanel
 
 class UsageProfileWorkView(RAMSTKWorkView):
-    _tag: str
-    _tablabel: str
-    _tabtooltip: str
-    _lst_col_order: List[int]
-    _lst_mnu_labels: List[str]
-    _lst_tooltips: List[str]
-    _pnlPanel: RAMSTKPanel
     def __init__(
         self, configuration: RAMSTKUserConfiguration, logger: RAMSTKLogManager
     ) -> None: ...
-    def _do_request_delete(self, __button: Gtk.ToolButton) -> None: ...
-    def _do_request_insert_child(self, __button: Gtk.ToolButton) -> None: ...
-    def _do_request_insert_sibling(self, __button: Gtk.ToolButton) -> None: ...
-    def __make_ui(self) -> None: ...
