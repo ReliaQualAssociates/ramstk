7a2c17573be20beb183e1d249eeccb8336ba4e0f
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-11 11:09:56 -0400

    refactor: update stub files

--- src/ramstk/views/gtk3/widgets/panels/base_panel.pyi
+++ src/ramstk/views/gtk3/widgets/panels/base_panel.pyi
@@ -1,5 +1,6 @@
 # RAMSTK Local Imports
 from ..frame import RAMSTKFrame as RAMSTKFrame
+from ..widget import RAMSTKBaseWidget as RAMSTKBaseWidget
 from ..widget import WidgetConfig as WidgetConfig
 
 def do_log_message(
@@ -7,7 +8,16 @@ def do_log_message(
 ) -> None: ...
 
 class RAMSTKBasePanel(RAMSTKFrame):
+    _record_field: str
+    _select_msg: str
+    _tag: str
+    _title: str
+    _lst_widget_configuration: list[WidgetConfig]
+    _parent_id: int
+    _record_id: int
     def __init__(self) -> None: ...
     def do_set_widget_attributes(self) -> None: ...
     def do_set_widget_callbacks(self) -> None: ...
     def do_set_widget_properties(self) -> None: ...
+    @staticmethod
+    def do_set_widget_sensitivity(widgets: list[RAMSTKBaseWidget]) -> None: ...
