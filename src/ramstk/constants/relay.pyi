68c37da09a28833ccb05eaf637d72e109e36419c
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-12 13:16:55 -0400

    refactor: update stub files

---
+++ src/ramstk/constants/relay.pyi
@@ -0,0 +1,19 @@
+# Third Party Imports
+from _typeshed import Incomplete
+
+# RAMSTK Package Imports
+from ramstk.views.gtk3 import _ as _
+
+PART_COUNT_LAMBDA_B: dict[int, dict[int, list[float]]]
+PART_COUNT_PI_Q: dict[int, list[float]]
+PART_STRESS_PI_Q: dict[int, list[float]]
+PI_C: dict[int, list[float]]
+PI_E: dict[int, dict[int, list[float]] | list[float]]
+PI_F: dict[int, dict[int, dict[int, list[float]]]]
+RELAY_APPLICATION_DICT: Incomplete
+RELAY_CONSTRUCTION_DICT: Incomplete
+RELAY_QUALITY_DICT: Incomplete
+RELAY_TYPE_DICT: Incomplete
+RELAY_CONTACT_FORM_LIST: Incomplete
+RELAY_CONTACT_RATING_LIST: Incomplete
+RELAY_TECHNOLOGY_LIST: Incomplete
