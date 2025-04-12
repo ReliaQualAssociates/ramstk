950224dfff927757d9f3c7b717668c51e201378d
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-12 12:49:41 -0400

    refactor: update stub files

---
+++ src/ramstk/constants/inductor.pyi
@@ -0,0 +1,11 @@
+# RAMSTK Package Imports
+from ramstk.views.gtk3 import _ as _
+
+PART_COUNT_LAMBDA_B: dict[int, dict[int, list[float]]]
+PART_COUNT_PI_Q: list[float]
+PART_STRESS_PI_Q: dict[int, dict[int, list[float]] | list[float]]
+PI_E: dict[int, list[float]]
+REF_TEMPS: dict[int, dict[int, float]]
+INDUCTOR_INSULATION_DICT: dict[int, list[list[str]]]
+INDUCTOR_QUALITY_DICT: dict[int, list[list[str]]]
+INDUCTOR_SPECIFICATION_DICT: dict[int, list[list[str]]]
--- src/ramstk/views/gtk3/design_electric/components/inductor.pyi
+++ src/ramstk/views/gtk3/design_electric/components/inductor.pyi
@@ -1,16 +1,25 @@
 # Standard Library Imports
-from typing import Any, Dict, List
+from typing import Any
 
 # RAMSTK Package Imports
+from ramstk.constants.inductor import (
+    INDUCTOR_INSULATION_DICT as INDUCTOR_INSULATION_DICT,
+)
+from ramstk.constants.inductor import INDUCTOR_QUALITY_DICT as INDUCTOR_QUALITY_DICT
+from ramstk.constants.inductor import (
+    INDUCTOR_SPECIFICATION_DICT as INDUCTOR_SPECIFICATION_DICT,
+)
+from ramstk.utilities import do_subscribe_to_messages as do_subscribe_to_messages
 from ramstk.views.gtk3 import _ as _
 from ramstk.views.gtk3.widgets import RAMSTKComboBox as RAMSTKComboBox
 from ramstk.views.gtk3.widgets import RAMSTKEntry as RAMSTKEntry
 from ramstk.views.gtk3.widgets import RAMSTKFixedPanel as RAMSTKFixedPanel
+from ramstk.views.gtk3.widgets import WidgetConfig as WidgetConfig
+
+PART_COUNT: int
+PART_STRESS: int
 
 class InductorDesignElectricInputPanel(RAMSTKFixedPanel):
-    _dic_insulation: Dict[int, List[List[str]]]
-    _dic_quality: Dict[int, List[List[str]]]
-    _dic_specifications: Dict[int, List[List[str]]]
     _record_field: str
     _select_msg: str
     _tag: str
@@ -18,20 +27,20 @@ class InductorDesignElectricInputPanel(RAMSTKFixedPanel):
     cmbConstruction: RAMSTKComboBox
     cmbFamily: RAMSTKComboBox
     cmbInsulation: RAMSTKComboBox
+    cmbQuality: RAMSTKComboBox
     cmbSpecification: RAMSTKComboBox
     txtArea: RAMSTKEntry
     txtWeight: RAMSTKEntry
+    _lst_widget_configuration: list[WidgetConfig]
     _hazard_rate_method_id: int
     _quality_id: int
-    dic_attribute_widget_map: Dict[str, List[Any]]
     category_id: int
     subcategory_id: int
     def __init__(self) -> None: ...
-    def do_load_comboboxes(self, subcategory_id: int) -> None: ...
-    def _do_load_panel(self, attributes: Dict[str, Any]) -> None: ...
-    def _do_set_reliability_attributes(self, attributes: Dict[str, Any]) -> None: ...
-    def _do_set_sensitive(self) -> None: ...
-    def __do_load_family_combobox(self) -> None: ...
-    def __do_load_insulation_combobox(self) -> None: ...
-    def __do_load_quality_combobox(self) -> None: ...
-    def __do_load_specification_combobox(self) -> None: ...
+    def on_subcategory_change(self, subcategory_id: int) -> None: ...
+    def _do_load_construction(self) -> None: ...
+    def _do_load_panel(self, attributes: dict[str, Any]) -> None: ...
+    def _get_family_list(self) -> list[list[str]]: ...
+    def _get_quality_list(self) -> list[list[str]]: ...
+    def _set_reliability_attributes(self, attributes: dict[str, Any]) -> None: ...
+    def _set_sensitive(self) -> None: ...
