185627401a5dec4c9c56ff4340c52a1e88a518c5
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-12 12:04:01 -0400

    refactor: update stub files

---
+++ src/ramstk/constants/resistor.pyi
@@ -0,0 +1,17 @@
+# RAMSTK Package Imports
+from ramstk.views.gtk3 import _ as _
+
+PART_COUNT_LAMBDA_B: dict[int, list[float]]
+PART_COUNT_PI_Q: list[float]
+PART_STRESS_PI_Q: dict[int, list[float]]
+PI_C: dict[int, list[float]]
+PI_E: dict[int, list[float]]
+PI_R: dict[int, list[float] | list[list[float]]]
+PI_V: dict[int, list[float]]
+REF_TEMPS: dict[int, float]
+REF_TEMPS_FILM: dict[int, float]
+RESISTOR_CONSTRUCTION_DICT: dict[int, list[list[str]]]
+RESISTOR_QUALITY_DICT: dict[int, list[list[str]]]
+RESISTOR_SPECIFICATION_DICT: dict[int, list[list[str]]]
+RESISTOR_STYLE_DICT: dict[int, dict[int, list[list[str]]]]
+RESISTOR_TYPE_DICT: dict[int, list[list[str]]]
