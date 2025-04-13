2a3737797b5798c8e26e7be30fd4033d62c7ef3d
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-13 18:01:07 -0400

    refactor: update stub file

--- src/ramstk/views/gtk3/milhdbk217f/components/integrated_circuit.pyi
+++ src/ramstk/views/gtk3/milhdbk217f/components/integrated_circuit.pyi
@@ -1,12 +1,26 @@
 # Standard Library Imports
-from typing import Any, Dict, List
+from typing import Any
+
+# Third Party Imports
+from _typeshed import Incomplete
 
 # RAMSTK Package Imports
-from ramstk.views.gtk3.milhdbk217f import MilHdbk217FResultPanel
-from ramstk.views.gtk3.widgets import RAMSTKEntry
+from ramstk.views.gtk3 import _ as _
+from ramstk.views.gtk3.milhdbk217f import (
+    MilHdbk217FResultPanel as MilHdbk217FResultPanel,
+)
+from ramstk.views.gtk3.widgets import RAMSTKEntry as RAMSTKEntry
+from ramstk.views.gtk3.widgets import WidgetConfig as WidgetConfig
+from ramstk.views.gtk3.widgets import make_widget_config as make_widget_config
 
 class ICMilHdbk217FResultPanel(MilHdbk217FResultPanel):
-    _dic_part_stress: Dict[int, str]
+    _lambda_p: str
+    _function_1: str
+    _function_2: str
+    _function_3: Incomplete
+    _dic_part_stress: dict[int, str]
+    _record_field: str
+    _tag: str
     _title: str
     txtC1: RAMSTKEntry
     txtC2: RAMSTKEntry
@@ -20,6 +34,6 @@ class ICMilHdbk217FResultPanel(MilHdbk217FResultPanel):
     txtPiMFG: RAMSTKEntry
     txtPiPT: RAMSTKEntry
     txtPiT: RAMSTKEntry
-    dic_attribute_widget_map: Dict[str, List[Any]]
+    _lst_widget_configuration: list[WidgetConfig]
     def __init__(self) -> None: ...
-    def _do_load_entries(self, attributes: Dict[str, Any]) -> None: ...
+    def _do_load_entries(self, attributes: dict[str, Any]) -> None: ...
