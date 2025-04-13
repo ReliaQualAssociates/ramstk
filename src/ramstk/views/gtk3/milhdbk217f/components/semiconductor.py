8840f3d4c3d80f318a1413704aa5c84b481b2460
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-13 18:09:39 -0400

    refactor: update semiconductor MIL-HDBK217F panel to use new RAMSTKWidgets

--- src/ramstk/views/gtk3/milhdbk217f/components/semiconductor.py
+++ src/ramstk/views/gtk3/milhdbk217f/components/semiconductor.py
@@ -13,7 +13,7 @@ from typing import Any, Dict, List
 # RAMSTK Package Imports
 from ramstk.views.gtk3 import _
 from ramstk.views.gtk3.milhdbk217f import MilHdbk217FResultPanel
-from ramstk.views.gtk3.widgets import RAMSTKEntry
+from ramstk.views.gtk3.widgets import RAMSTKEntry, WidgetConfig, make_widget_config
 
 
 class SemiconductorMilHdbk217FResultPanel(MilHdbk217FResultPanel):
@@ -34,13 +34,12 @@ class SemiconductorMilHdbk217FResultPanel(MilHdbk217FResultPanel):
     :ivar txtPiS: displays the electrical stress factor for the semiconductor.
     """
 
+    # Define private class attributes.
     _lambda_p = '<span foreground="blue">\u03bb<sub>p</sub> = '
     _function_1 = (
         "\u03bb<sub>b</sub>\u03c0<sub>T</sub>\u03c0<sub>Q</sub>"
         "\u03c0<sub>E</sub></span>"
     )
-
-    # Define private dict class attributes.
     _dic_part_stress: Dict[int, str] = {
         1: _lambda_p
         + "\u03bb<sub>b</sub>\u03c0<sub>T</sub>\u03c0<sub>S</sub>\u03c0<sub>Q"
@@ -74,19 +73,9 @@ class SemiconductorMilHdbk217FResultPanel(MilHdbk217FResultPanel):
         + "\u03bb<sub>b</sub>\u03c0<sub>T</sub>\u03c0<sub>Q</sub>\u03c0<sub>I"
         "</sub>\u03c0<sub>A</sub>\u03c0<sub>P</sub>\u03c0<sub>E</sub></span> ",
     }
-
-    # Define private list class attributes.
-
-    # Define private scalar class attributes.
     _record_field: str = "hardware_id"
     _tag: str = "milhdbk217f"
     _title: str = _("Semiconductor MIL-HDBK-217F Results")
-
-    # Define public dictionary class attributes.
-
-    # Define public list class attributes.
-
-    # Define public scalar class attributes.
 
     def __init__(self) -> None:
         """Initialize instance of the Semiconductor assessment result view."""
@@ -102,198 +91,250 @@ class SemiconductorMilHdbk217FResultPanel(MilHdbk217FResultPanel):
         self.txtPiS: RAMSTKEntry = RAMSTKEntry()
         self.txtPiT: RAMSTKEntry = RAMSTKEntry()
 
-        # Initialize private dictionary attributes.
-
-        # Initialize private list attributes.
-
-        # Initialize private scalar attributes.
-
-        # Initialize public dictionary attributes.
-        self.dic_attribute_widget_map: Dict[str, List[Any]] = {
-            "hazard_rate_model": [
-                13,
+        # Initialize private instance attributes.
+        self._lst_widget_configuration: List[WidgetConfig] = [
+            make_widget_config(
                 self.lblModel,
-                "",
-                None,
-                "",
-                "",
+                {
+                    "datatype": "",
+                    "default": "",
+                    "field": "hazard_rate_model",
+                    "index": 13,
+                    "label_text": "",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _(
                         "The assessment model used to calculate the semiconductor "
                         "hazard rate."
                     ),
+                    "editable": True,
+                    "visible": True,
                 },
-                "",
-            ],
-            "lambda_b": [
-                23,
+            ),
+            make_widget_config(
                 self.txtLambdaB,
-                "",
-                None,
-                "",
-                0.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 0.0,
+                    "field": "lambda_b",
+                    "index": 23,
+                    "label_text": "\u03bb<sub>b</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The base hazard rate for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03bb<sub>b</sub>:",
-            ],
-            "pi_q": [
-                30,
+            ),
+            make_widget_config(
                 self.txtPiQ,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_q",
+                    "index": 30,
+                    "label_text": "\u03c0<sub>Q</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The quality factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>Q</sub>:",
-            ],
-            "pi_e": [
-                19,
+            ),
+            make_widget_config(
                 self.txtPiE,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_e",
+                    "index": 19,
+                    "label_text": "\u03c0<sub>E</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The environment factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>E</sub>:",
-            ],
-            "pi_t": [
-                33,
+            ),
+            make_widget_config(
                 self.txtPiT,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_t",
+                    "index": 33,
+                    "label_text": "\u03c0<sub>T</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The temperature factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>T</sub>:",
-            ],
-            "pi_a": [
-                12,
+            ),
+            make_widget_config(
                 self.txtPiA,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_a",
+                    "index": 12,
+                    "label_text": "\u03c0<sub>A</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The application factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>A</sub>:",
-            ],
-            "pi_c": [
-                13,
+            ),
+            make_widget_config(
                 self.txtPiC,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_c",
+                    "index": 13,
+                    "label_text": "\u03c0<sub>C</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _(
                         "The contact construction factor for the semiconductor."
                     ),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>C</sub>:",
-            ],
-            "pi_r": [
-                31,
+            ),
+            make_widget_config(
                 self.txtPiR,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_r",
+                    "index": 31,
+                    "label_text": "\u03c0<sub>R</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The power rating factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>R</sub>:",
-            ],
-            "pi_m": [
-                24,
+            ),
+            make_widget_config(
                 self.txtPiM,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_m",
+                    "index": 24,
+                    "label_text": "\u03c0<sub>M</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The matching network factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>M</sub>:",
-            ],
-            "pi_i": [
-                21,
+            ),
+            make_widget_config(
                 self.txtPiI,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_i",
+                    "index": 21,
+                    "label_text": "\u03c0<sub>I</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The forward current factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>I</sub>:",
-            ],
-            "pi_p": [
-                28,
+            ),
+            make_widget_config(
                 self.txtPiP,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_p",
+                    "index": 28,
+                    "label_text": "\u03c0<sub>P</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The power degradation factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>P</sub>:",
-            ],
-            "pi_s": [
-                31,
+            ),
+            make_widget_config(
                 self.txtPiS,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_s",
+                    "index": 31,
+                    "label_text": "\u03c0<sub>S</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The electrical stress factor for the semiconductor."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>S</sub>:",
-            ],
-        }
+            ),
+        ]
 
-        # Initialize public list attributes.
-
-        # Initialize public scalar attributes.
-
-        super().do_set_properties()
+        super().do_set_widget_attributes()
+        super().do_set_widget_properties()
         super().do_make_panel()
-
-        # Subscribe to PyPubSub messages.
 
     def _do_load_entries(self, attributes: Dict[str, Any]) -> None:
         """Load the semiconductor assessment results page.
 
         :param attributes: the attributes dictionary for the selected Semiconductor.
-        :return: None
-        :rtype: None
         """
         super().do_load_entries(attributes)
 
-        self.txtPiA.set_sensitive(False)
-        self.txtPiC.set_sensitive(False)
-        self.txtPiI.set_sensitive(False)
-        self.txtPiM.set_sensitive(False)
-        self.txtPiP.set_sensitive(False)
-        self.txtPiR.set_sensitive(False)
-        self.txtPiS.set_sensitive(False)
-        self.txtPiT.set_sensitive(False)
+        super().do_set_widget_sensitivity(
+            [
+                self.txtPiA,
+                self.txtPiC,
+                self.txtPiI,
+                self.txtPiM,
+                self.txtPiP,
+                self.txtPiR,
+                self.txtPiS,
+                self.txtPiT,
+            ],
+            False,
+        )
 
         if self.category_id == 2 and self._hazard_rate_method_id == 2:
-            self.txtPiA.do_update(str(self.fmt.format(attributes["piA"])))
-            self.txtPiC.do_update(str(self.fmt.format(attributes["piC"])))
-            self.txtPiI.do_update(str(self.fmt.format(attributes["piI"])))
-            self.txtPiM.do_update(str(self.fmt.format(attributes["piM"])))
-            self.txtPiP.do_update(str(self.fmt.format(attributes["piP"])))
-            self.txtPiR.do_update(str(self.fmt.format(attributes["piR"])))
-            self.txtPiS.do_update(str(self.fmt.format(attributes["piS"])))
-            self.txtPiT.do_update(str(self.fmt.format(attributes["piT"])))
+            self.txtPiA.do_update({"piA": str(self.fmt.format(attributes["piA"]))})
+            self.txtPiC.do_update({"piC": str(self.fmt.format(attributes["piC"]))})
+            self.txtPiI.do_update({"piI": str(self.fmt.format(attributes["piI"]))})
+            self.txtPiM.do_update({"piM": str(self.fmt.format(attributes["piM"]))})
+            self.txtPiP.do_update({"piP": str(self.fmt.format(attributes["piP"]))})
+            self.txtPiR.do_update({"piR": str(self.fmt.format(attributes["piR"]))})
+            self.txtPiS.do_update({"piS": str(self.fmt.format(attributes["piS"]))})
+            self.txtPiT.do_update({"piT": str(self.fmt.format(attributes["piT"]))})
