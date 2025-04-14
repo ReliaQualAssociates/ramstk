3f4c5fd44159bc7040baea20a0009aa0d84fa506
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-14 11:43:44 -0400

    refactor: update meter MIL-HDBK217F panel to use new RAMSTKWidgets

--- src/ramstk/views/gtk3/milhdbk217f/components/meter.py
+++ src/ramstk/views/gtk3/milhdbk217f/components/meter.py
@@ -4,7 +4,7 @@
 #
 # All rights reserved.
 # Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""Meter Results Panel."""
+"""Meter Results panel module."""
 
 # Standard Library Imports
 from typing import Any, Dict, List
@@ -12,7 +12,7 @@ from typing import Any, Dict, List
 # RAMSTK Package Imports
 from ramstk.views.gtk3 import _
 from ramstk.views.gtk3.milhdbk217f import MilHdbk217FResultPanel
-from ramstk.views.gtk3.widgets import RAMSTKEntry
+from ramstk.views.gtk3.widgets import RAMSTKEntry, WidgetConfig, make_widget_config
 
 
 class MeterMilHdbk217FResultPanel(MilHdbk217FResultPanel):
@@ -29,7 +29,7 @@ class MeterMilHdbk217FResultPanel(MilHdbk217FResultPanel):
         meter.
     """
 
-    # Define private class dict class attributes.
+    # Define private class attributes.
     _dic_part_stress: Dict[int, str] = {
         1: '<span foreground="blue">\u03bb<sub>p</sub> = '
         "\u03bb<sub>b</sub>\u03c0<sub>A</sub>\u03c0<sub>F</sub>\u03c0<sub>Q"
@@ -37,19 +37,9 @@ class MeterMilHdbk217FResultPanel(MilHdbk217FResultPanel):
         2: '<span foreground="blue">\u03bb<sub>p</sub> = '
         "\u03bb<sub>b</sub>\u03c0<sub>T</sub>\u03c0<sub>E</sub></span> ",
     }
-
-    # Define private class list class attributes.
-
-    # Define private scalar class attributes.
     _record_field: str = "hardware_id"
     _tag: str = "milhdbk217f"
     _title: str = _("Meter MIL-HDBK-217F Results")
-
-    # Define public dictionary class attributes.
-
-    # Define public list class attributes.
-
-    # Define public scalar class attributes.
 
     def __init__(self) -> None:
         """Initialize an instance of the Meter assessment result view."""
@@ -60,127 +50,156 @@ class MeterMilHdbk217FResultPanel(MilHdbk217FResultPanel):
         self.txtPiF: RAMSTKEntry = RAMSTKEntry()
         self.txtPiT: RAMSTKEntry = RAMSTKEntry()
 
-        # Initialize private dictionary attributes.
-
-        # Initialize private list attributes.
-
-        # Initialize private scalar attributes.
-
-        # Initialize public dictionary attributes.
-        self.dic_attribute_widget_map: Dict[str, List[Any]] = {
-            "hazard_rate_model": [
-                13,
+        # Initialize private instance attributes.
+        self._lst_widget_configuration: List[WidgetConfig] = [
+            make_widget_config(
                 self.lblModel,
-                "",
-                None,
-                "",
-                "",
+                {
+                    "datatype": "",
+                    "default": "",
+                    "field": "hazard_rate_model",
+                    "index": 13,
+                    "label_text": "",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _(
                         "The assessment model used to calculate the meter hazard rate."
                     ),
+                    "editable": True,
+                    "visible": True,
                 },
-                "",
-            ],
-            "lambda_b": [
-                23,
+            ),
+            make_widget_config(
                 self.txtLambdaB,
-                "",
-                None,
-                "",
-                0.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 0.0,
+                    "field": "lambda_b",
+                    "index": 23,
+                    "label_text": "\u03bb<sub>b</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The base hazard rate for the meter."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03bb<sub>b</sub>:",
-            ],
-            "pi_q": [
-                30,
+            ),
+            make_widget_config(
                 self.txtPiQ,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_q",
+                    "index": 30,
+                    "label_text": "\u03c0<sub>Q</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The quality factor for the meter."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>Q</sub>:",
-            ],
-            "pi_e": [
-                19,
+            ),
+            make_widget_config(
                 self.txtPiE,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_e",
+                    "index": 19,
+                    "label_text": "\u03c0<sub>E</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The environment factor for the meter."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>E</sub>:",
-            ],
-            "pi_a": [
-                12,
+            ),
+            make_widget_config(
                 self.txtPiA,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_a",
+                    "index": 12,
+                    "label_text": "\u03c0<sub>A</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The application factor for the meter."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>A</sub>:",
-            ],
-            "pi_f": [
-                20,
+            ),
+            make_widget_config(
                 self.txtPiF,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_f",
+                    "index": 20,
+                    "label_text": "\u03c0<sub>F</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The function factor for the meter."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>F</sub>:",
-            ],
-            "pi_t": [
-                33,
+            ),
+            make_widget_config(
                 self.txtPiT,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_t",
+                    "index": 33,
+                    "label_text": "\u03c0<sub>T</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _(
                         "The temperature stress factor for the elapsed time meter."
                     ),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>T</sub>:",
-            ],
-        }
+            ),
+        ]
 
-        # Initialize public list attributes.
-
-        # Initialize public scalar attributes.
-
-        super().do_set_properties()
+        super().do_set_widget_attributes()
+        super().do_set_widget_properties()
         super().do_make_panel()
-
-        # Subscribe to PyPubSub messages.
 
     def _do_load_entries(self, attributes: Dict[str, Any]) -> None:
         """Load the meter assessment results page.
 
         :param attributes: the attributes dictionary for the selected Meter.
-        :return: None
-        :rtype: None
         """
         super().do_load_entries(attributes)
-
-        self.txtPiA.set_sensitive(False)
-        self.txtPiF.set_sensitive(False)
-        self.txtPiT.set_sensitive(False)
+        super().do_set_widget_sensitivity(
+            [
+                self.txtPiA,
+                self.txtPiF,
+                self.txtPiT,
+            ],
+            False,
+        )
 
         if self.category_id == 9 and self._hazard_rate_method_id == 2:
-            self.txtPiA.do_update(str(self.fmt.format(attributes["piA"])))
-            self.txtPiF.do_update(str(self.fmt.format(attributes["piF"])))
-            self.txtPiT.do_update(str(self.fmt.format(attributes["piT"])))
+            self.lblModel.do_update(
+                {"hazard_rate_model": self._dic_part_stress[self.subcategory_id]}
+            )
+            self.txtPiA.do_update({"piA": str(self.fmt.format(attributes["piA"]))})
+            self.txtPiF.do_update({"piF": str(self.fmt.format(attributes["piF"]))})
+            self.txtPiT.do_update({"piT": str(self.fmt.format(attributes["piT"]))})
