19e94621f5c201aac6180c836aeec0838ca00565
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-14 11:40:29 -0400

    refactor: update connection MIL-HDBK217F panel to use new RAMSTKWidgets

--- src/ramstk/views/gtk3/milhdbk217f/components/connection.py
+++ src/ramstk/views/gtk3/milhdbk217f/components/connection.py
@@ -5,7 +5,7 @@
 #
 # All rights reserved.
 # Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""Connection Results Panel."""
+"""Connection Results panel module."""
 
 # Standard Library Imports
 from typing import Any, Dict, List
@@ -13,7 +13,7 @@ from typing import Any, Dict, List
 # RAMSTK Package Imports
 from ramstk.views.gtk3 import _
 from ramstk.views.gtk3.milhdbk217f import MilHdbk217FResultPanel
-from ramstk.views.gtk3.widgets import RAMSTKEntry
+from ramstk.views.gtk3.widgets import RAMSTKEntry, WidgetConfig, make_widget_config
 
 
 class ConnectionMilHdbk217FResultPanel(MilHdbk217FResultPanel):
@@ -46,7 +46,7 @@ class ConnectionMilHdbk217FResultPanel(MilHdbk217FResultPanel):
     :ivar txtPiQ: displays the quality factor for the Hardware item.
     """
 
-    # Define private dict class attributes.
+    # Define private class attributes.
     _dic_part_stress = {
         1: '<span foreground="blue">\u03bb<sub>p</sub> = '
         "\u03bb<sub>b</sub>\u03c0<sub>K</sub>\u03c0<sub>P</sub>\u03c0"
@@ -63,19 +63,9 @@ class ConnectionMilHdbk217FResultPanel(MilHdbk217FResultPanel):
         5: '<span foreground="blue">\u03bb<sub>p</sub> = '
         "\u03bb<sub>b</sub>\u03c0<sub>Q</sub>\u03c0<sub>E</sub></span>",
     }
-
-    # Define private list class attributes.
-
-    # Define private scalar class attributes.
     _record_field: str = "hardware_id"
     _tag: str = "milhdbk217f"
     _title: str = _("Connection MIL-HDBK-217F Results")
-
-    # Define public dictionary class attributes.
-
-    # Define public list class attributes.
-
-    # Define public scalar class attributes.
 
     def __init__(self) -> None:
         """Initialize an instance of the Connection assessment result view."""
@@ -86,137 +76,156 @@ class ConnectionMilHdbk217FResultPanel(MilHdbk217FResultPanel):
         self.txtPiK: RAMSTKEntry = RAMSTKEntry()
         self.txtPiP: RAMSTKEntry = RAMSTKEntry()
 
-        # Initialize private dict attributes.
-
-        # Initialize private list attributes.
-
-        # Initialize private scalar attributes.
-
-        # Initialize public dict attributes.
-        self.dic_attribute_widget_map: Dict[str, List[Any]] = {
-            "hazard_rate_model": [
-                13,
+        # Initialize private instance attributes.
+        self._lst_widget_configuration: List[WidgetConfig] = [
+            make_widget_config(
                 self.lblModel,
-                "",
-                None,
-                "",
-                "",
+                {
+                    "datatype": "",
+                    "default": "",
+                    "field": "hazard_rate_model",
+                    "index": 13,
+                    "label_text": "",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _(
-                        "The assessment model used to calculate the connection "
-                        "hazard rate."
+                        "The assessment model used to calculate the connection hazard "
+                        "rate."
                     ),
+                    "editable": True,
+                    "visible": True,
                 },
-                "",
-            ],
-            "lambda_b": [
-                23,
+            ),
+            make_widget_config(
                 self.txtLambdaB,
-                "",
-                None,
-                "",
-                0.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 0.0,
+                    "field": "lambda_b",
+                    "index": 23,
+                    "label_text": "\u03bb<sub>b</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The base hazard rate for the connection."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03bb<sub>b</sub>:",
-            ],
-            "pi_q": [
-                30,
+            ),
+            make_widget_config(
                 self.txtPiQ,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_q",
+                    "index": 30,
+                    "label_text": "\u03c0<sub>Q</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The quality factor for the connection."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>Q</sub>:",
-            ],
-            "pi_e": [
-                19,
+            ),
+            make_widget_config(
                 self.txtPiE,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_e",
+                    "index": 19,
+                    "label_text": "\u03c0<sub>E</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The environment factor for the connection."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>E</sub>:",
-            ],
-            "pi_c": [
-                13,
+            ),
+            make_widget_config(
                 self.txtPiC,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_c",
+                    "index": 13,
+                    "label_text": "\u03c0<sub>C</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The mating/unmating factor for the connection."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>C</sub>:",
-            ],
-            "pi_k": [
-                22,
+            ),
+            make_widget_config(
                 self.txtPiK,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_k",
+                    "index": 22,
+                    "label_text": "\u03c0<sub>K</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The active pins factor for the connection."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>K</sub>:",
-            ],
-            "pi_p": [
-                28,
+            ),
+            make_widget_config(
                 self.txtPiP,
-                "",
-                None,
-                "",
-                1.0,
+                {
+                    "datatype": "gfloat",
+                    "default": 1.0,
+                    "field": "pi_p",
+                    "index": 28,
+                    "label_text": "\u03c0<sub>P</sub>:",
+                    "listen_topic": None,
+                    "send_topic": None,
+                },
                 {
                     "tooltip": _("The complexity factor for the connection."),
+                    "editable": True,
+                    "visible": True,
                 },
-                "\u03c0<sub>P</sub>:",
-            ],
-        }
+            ),
+        ]
 
-        # Initialize public list attributes.
-
-        # Initialize public scalar attributes.
-
-        super().do_set_properties()
+        super().do_set_widget_attributes()
+        super().do_set_widget_properties()
         super().do_make_panel()
-
-        # Subscribe to PyPubSub messages.
 
     def _do_load_entries(self, attributes: Dict[str, Any]) -> None:
         """Set widget sensitivity as needed for the selected capacitor.
 
-        :return: None
-        :rtype: None
+        :param attributes: the dict of attributes to load into the widgets.
         """
         super().do_load_entries(attributes)
-
-        self.txtPiK.set_sensitive(False)
-        self.txtPiP.set_sensitive(False)
-        self.txtPiC.set_sensitive(False)
+        super().do_set_widget_sensitivity(
+            [
+                self.txtPiK,
+                self.txtPiP,
+                self.txtPiC,
+            ],
+            False,
+        )
 
         # MIL-HDBK-217F, Parts Stress
         if self.category_id == 8 and self._hazard_rate_method_id == 2:
-            self.lblModel.do_update(self._dic_part_stress[self.subcategory_id])
-
-            self.txtPiC.do_update(
-                str(self.fmt.format(attributes["piC"])),
-                signal="changed",
+            self.lblModel.do_update(
+                {"hazard_rate_model": self._dic_part_stress[self.subcategory_id]}
             )
-            self.txtPiK.do_update(
-                str(self.fmt.format(attributes["piK"])),
-                signal="changed",
-            )
-            self.txtPiP.do_update(
-                str(self.fmt.format(attributes["piP"])),
-                signal="changed",
-            )
+            self.txtPiC.do_update({"piC": str(self.fmt.format(attributes["piC"]))})
+            self.txtPiK.do_update({"piK": str(self.fmt.format(attributes["piK"]))})
+            self.txtPiP.do_update({"piP": str(self.fmt.format(attributes["piP"]))})
