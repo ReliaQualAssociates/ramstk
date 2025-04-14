ef34e50cdd1ae1a19b263b81a2eebb8430e11fb0
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-14 11:43:55 -0400

    refactor: update stub file

--- src/ramstk/views/gtk3/milhdbk217f/components/meter.pyi
+++ src/ramstk/views/gtk3/milhdbk217f/components/meter.pyi
@@ -1,16 +1,23 @@
 # Standard Library Imports
-from typing import Any, Dict, List
+from typing import Any
 
 # RAMSTK Package Imports
-from ramstk.views.gtk3.milhdbk217f import MilHdbk217FResultPanel
-from ramstk.views.gtk3.widgets import RAMSTKEntry
+from ramstk.views.gtk3 import _ as _
+from ramstk.views.gtk3.milhdbk217f import (
+    MilHdbk217FResultPanel as MilHdbk217FResultPanel,
+)
+from ramstk.views.gtk3.widgets import RAMSTKEntry as RAMSTKEntry
+from ramstk.views.gtk3.widgets import WidgetConfig as WidgetConfig
+from ramstk.views.gtk3.widgets import make_widget_config as make_widget_config
 
 class MeterMilHdbk217FResultPanel(MilHdbk217FResultPanel):
-    _dic_part_stress: Dict[int, str]
+    _dic_part_stress: dict[int, str]
+    _record_field: str
+    _tag: str
     _title: str
     txtPiA: RAMSTKEntry
     txtPiF: RAMSTKEntry
     txtPiT: RAMSTKEntry
-    dic_attribute_widget_map: Dict[str, List[Any]]
+    _lst_widget_configuration: list[WidgetConfig]
     def __init__(self) -> None: ...
-    def _do_load_entries(self, attributes: Dict[str, Any]) -> None: ...
+    def _do_load_entries(self, attributes: dict[str, Any]) -> None: ...
