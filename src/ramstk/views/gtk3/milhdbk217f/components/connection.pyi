c118284aa8c97a00b953a6cf2d3907d896795c82
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-14 11:40:40 -0400

    refactor: update stub file

--- src/ramstk/views/gtk3/milhdbk217f/components/connection.pyi
+++ src/ramstk/views/gtk3/milhdbk217f/components/connection.pyi
@@ -1,16 +1,26 @@
 # Standard Library Imports
-from typing import Any, Dict, List
+from typing import Any
+
+# Third Party Imports
+from _typeshed import Incomplete
 
 # RAMSTK Package Imports
-from ramstk.views.gtk3.milhdbk217f import MilHdbk217FResultPanel
-from ramstk.views.gtk3.widgets import RAMSTKEntry
+from ramstk.views.gtk3 import _ as _
+from ramstk.views.gtk3.milhdbk217f import (
+    MilHdbk217FResultPanel as MilHdbk217FResultPanel,
+)
+from ramstk.views.gtk3.widgets import RAMSTKEntry as RAMSTKEntry
+from ramstk.views.gtk3.widgets import WidgetConfig as WidgetConfig
+from ramstk.views.gtk3.widgets import make_widget_config as make_widget_config
 
 class ConnectionMilHdbk217FResultPanel(MilHdbk217FResultPanel):
-    _dic_part_stress: Dict[int, str]
+    _dic_part_stress: Incomplete
+    _record_field: str
+    _tag: str
     _title: str
     txtPiC: RAMSTKEntry
     txtPiK: RAMSTKEntry
     txtPiP: RAMSTKEntry
-    dic_attribute_widget_map: Dict[str, List[Any]]
+    _lst_widget_configuration: list[WidgetConfig]
     def __init__(self) -> None: ...
-    def _do_load_entries(self, attributes: Dict[str, Any]) -> None: ...
+    def _do_load_entries(self, attributes: dict[str, Any]) -> None: ...
