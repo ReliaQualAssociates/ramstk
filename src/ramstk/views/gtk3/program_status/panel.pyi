d294c6b7654f197e5fd0eb99894e0ebec2ff35d5
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-17 09:08:22 -0400

    refactor: update stub file

--- src/ramstk/views/gtk3/program_status/panel.pyi
+++ src/ramstk/views/gtk3/program_status/panel.pyi
@@ -1,10 +1,9 @@
-# Standard Library Imports
-from typing import Dict, List
-
 # Third Party Imports
 import pandas as pd
+from _typeshed import Incomplete
 
 # RAMSTK Package Imports
+from ramstk.utilities import do_subscribe_to_messages as do_subscribe_to_messages
 from ramstk.views.gtk3 import _ as _
 from ramstk.views.gtk3.widgets import RAMSTKPlotPanel as RAMSTKPlotPanel
 
@@ -12,12 +11,13 @@ class ProgramStatusPlotPanel(RAMSTKPlotPanel):
     _record_field: str
     _select_msg: str
     _tag: str
-    _title: str
-    lst_axis_labels: List[str]
-    lst_legend: List[str]
-    plot_title: str
+    _title: Incomplete
+    lst_axis_labels: Incomplete
+    lst_legend: Incomplete
+    plot_title: Incomplete
     def __init__(self) -> None: ...
-    def _do_load_panel(self, attributes: Dict[str, pd.DataFrame]) -> None: ...
+    def _do_load_panel(self, attributes: dict[str, pd.DataFrame]) -> None: ...
+    def _do_load_actuals(self, status: pd.DataFrame) -> None: ...
     def _do_load_assessment_milestones(
         self, assessed: pd.DataFrame, y_max: float
     ) -> None: ...
