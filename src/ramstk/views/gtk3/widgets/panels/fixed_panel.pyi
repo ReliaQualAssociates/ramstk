fa5a1a450dee366c34bdc42b5b55dadc7c8fa65e
Doyle Rowland <doyle.rowland@reliaqual.com>
2025-04-18 11:05:12 -0400

    refactor: update stub files

--- src/ramstk/views/gtk3/widgets/panels/fixed_panel.pyi
+++ src/ramstk/views/gtk3/widgets/panels/fixed_panel.pyi
@@ -5,6 +5,7 @@ from datetime import date
 from _typeshed import Incomplete
 
 # RAMSTK Package Imports
+from ramstk.utilities import do_subscribe_to_messages as do_subscribe_to_messages
 from ramstk.views.gtk3 import Gtk as Gtk
 from ramstk.views.gtk3.widgets.buttons import RAMSTKCheckButton as RAMSTKCheckButton
 
@@ -21,13 +22,23 @@ MARGIN: int
 LABEL_SPACING: int
 
 class RAMSTKFixedPanel(RAMSTKBasePanel):
+    _dic_widget_field_map: dict[str, RAMSTKBaseWidget]
     fixed: Incomplete
     def __init__(self) -> None: ...
-    def do_clear_panel(self) -> None: ...
-    def do_load_panel(
+    def do_clear_fixed_panel(self) -> None: ...
+    _record_id: Incomplete
+    def do_load_fixed_panel(
         self, attributes: dict[str, bool | date | float | int | str]
     ) -> None: ...
-    def do_make_panel(
+    def do_make_fixed_panel(
         self, justify: Gtk.Justification = ..., n_columns: int = 1
     ) -> None: ...
-    def do_subscribe_to_messages(self) -> None: ...
+    def _do_create_widgets_for_column(
+        self, /, column: int, __justify: Gtk.Justification
+    ): ...
+    def _do_place_widgets(
+        self,
+        x_pos: list[int],
+        labels: list[RAMSTKLabel],
+        widgets: list[RAMSTKBaseWidget],
+    ) -> None: ...
